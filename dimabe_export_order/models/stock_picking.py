c34bf2c6cebdd98b2edcd8560c0b0df47ac8fdc9
dimabe-odoo <desarrolloti@dimabe.cl>
2020-02-25 14:31:23 -0300

    test

--- dimabe_export_order/models/stock_picking.py
+++ dimabe_export_order/models/stock_picking.py
@@ -210,9 +210,7 @@ class StockPicking(models.Model):
         compute='_compute_elapsed_time'
     )
 
-    sale_order = fields.Many2one(rel='sale.order')
-
-
+    sale_order = fields.Many2one('sale.order')
 
     @api.multi
     def generate_report(self):
