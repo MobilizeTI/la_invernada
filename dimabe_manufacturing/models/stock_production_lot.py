267b5c2ea9e87705b2273d6f73bdedc5e17cae6d
dimabe-odoo <desarrolloti@dimabe.cl>
2020-02-28 14:12:16 -0300

    test

--- dimabe_manufacturing/models/stock_production_lot.py
+++ dimabe_manufacturing/models/stock_production_lot.py
@@ -77,7 +77,7 @@ class StockProductionLot(models.Model):
                     move_line = s.move_line_ids.filtered(
                         lambda a : a.lot_id.id == item.id
                     )
-                    models._logger.error(move_line)
+                    models._logger.error(len(move_line))
 
     @api.multi
     def write(self, values):
