4ba93bc09f10b225c53ffc0b65735e8063d6a48d
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-02-28 14:54:01 -0300

    debug

--- dimabe_manufacturing/models/unpelled_dried.py
+++ dimabe_manufacturing/models/unpelled_dried.py
@@ -4,6 +4,7 @@ from odoo import fields, models, api
 class UnpelledDried(models.Model):
     _name = 'unpelled.dried'
     _description = 'clase que para producción de secado y despelonado'
+    _order = 'state'
 
     state = fields.Selection([
         ('draft', 'Borrador'),
