dd1696a68faef8576043695fddd5b17f59279b64
dimabe-odoo <desarrolloti@dimabe.cl>
2020-02-28 10:36:31 -0300

    debug

--- dimabe_manufacturing/models/stock_production_lot.py
+++ dimabe_manufacturing/models/stock_production_lot.py
@@ -39,7 +39,7 @@ class StockProductionLot(models.Model):
                     stock_picking = self.env['stock.picking'].search([('id', '=', stock_picking_id)])
                     if stock_picking:
                         for stock in stock_picking.move_ids_without_package:
-                            models._logger.error(stock.reserved_availability)
+                            models._logger.error(item.product_uom_qty)
 
 
 
