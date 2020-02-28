d64247de9eb3572f5b7b35a2bb88b98f25a62cb9
dimabe-odoo <desarrolloti@dimabe.cl>
2020-02-28 17:31:18 -0300

    test

--- dimabe_manufacturing/models/stock_production_lot.py
+++ dimabe_manufacturing/models/stock_production_lot.py
@@ -50,9 +50,12 @@ class StockProductionLot(models.Model):
                             'reserved_quantity': stock_quant.reserved_quantity + item.qty_to_reserve
                         })
                         item.update({
-                            'qty_to_reserve': reserve,
-                            'is_reserved': True
+                            'qty_to_reserve': reserve
                         })
+                        if reserve >= 0:
+                            item.update({
+                                'is_reserve': True
+                            })
                         move_line = self.env['stock.move.line'].create({
                             'product_id': item.product_id.id,
                             'lot_id': item.id,
@@ -82,61 +85,66 @@ class StockProductionLot(models.Model):
                     lambda a: a.lot_id.id == item.id and a.product_uom_qty == stock_move.reserved_availability
                 )
                 item.update({
-                    'qty_to_reserve': 0,
-                    'is_reserved': False
+                    'qty_to_reserve': 0
                 })
-                models._logger.error(item.is_reserved)
-                stock_quant = item.get_stock_quant()
-                stock_quant.sudo().update({
-                    'reserved_quantity': stock_quant.reserved_quantity - stock_move.product_uom_qty
-                })
-
-                for ml in move_line:
-                    ml.write({'move_id': None, 'reserved_availability': 0})
-
-    @api.multi
-    def write(self, values):
-        for item in self:
-            res = super(StockProductionLot, self).write(values)
-            counter = 0
-            if not item.is_standard_weight:
-                for serial in item.stock_production_lot_serial_ids:
-                    counter += 1
-                    tmp = '00{}'.format(counter)
-                    serial.serial_number = item.name + tmp[-3:]
-            return res
-
-    @api.multi
-    def generate_standard_serial(self):
-        for item in self:
-            serial_ids = []
-            for counter in range(item.qty_standard_serial):
-                tmp = '00{}'.format(counter + 1)
-                serial = item.stock_production_lot_serial_ids.filtered(
-                    lambda a: a.serial_number == item.name + tmp[-3:]
-                )
-                if serial:
-                    if not serial.consumed:
-                        serial.update({
-                            'display_weight': item.standard_weight
-                        })
-                        serial_ids.append(serial.id)
-                else:
-                    new_serial = item.env['stock.production.lot.serial'].create({
-                        'stock_production_lot_id': item.id,
-                        'display_weight': item.standard_weight,
-                        'serial_number': item.name + tmp[-3:],
-                        'belong_to_prd_lot': True
+                if item.qty_to_reserve <= 0:
+                    item.update({
+                        'is_reserve': False
                     })
-                    serial_ids.append(new_serial.id)
-            serial_ids += list(item.stock_production_lot_serial_ids.filtered(
-                lambda a: a.consumed
-            ).mapped('id'))
+            stock_quant = item.get_stock_quant()
+            stock_quant.sudo().update({
+                'reserved_quantity': stock_quant.reserved_quantity - stock_move.product_uom_qty
+            })
 
-            item.stock_production_lot_serial_ids = [(6, 0, serial_ids)]
+            for ml in move_line:
+                ml.write({'move_id': None, 'reserved_availability': 0})
 
-    @api.model
-    def get_stock_quant(self):
-        return self.quant_ids.filtered(
-            lambda a: a.location_id.name == 'Stock'
-        )
+
+@api.multi
+def write(self, values):
+    for item in self:
+        res = super(StockProductionLot, self).write(values)
+        counter = 0
+        if not item.is_standard_weight:
+            for serial in item.stock_production_lot_serial_ids:
+                counter += 1
+                tmp = '00{}'.format(counter)
+                serial.serial_number = item.name + tmp[-3:]
+        return res
+
+
+@api.multi
+def generate_standard_serial(self):
+    for item in self:
+        serial_ids = []
+        for counter in range(item.qty_standard_serial):
+            tmp = '00{}'.format(counter + 1)
+            serial = item.stock_production_lot_serial_ids.filtered(
+                lambda a: a.serial_number == item.name + tmp[-3:]
+            )
+            if serial:
+                if not serial.consumed:
+                    serial.update({
+                        'display_weight': item.standard_weight
+                    })
+                    serial_ids.append(serial.id)
+            else:
+                new_serial = item.env['stock.production.lot.serial'].create({
+                    'stock_production_lot_id': item.id,
+                    'display_weight': item.standard_weight,
+                    'serial_number': item.name + tmp[-3:],
+                    'belong_to_prd_lot': True
+                })
+                serial_ids.append(new_serial.id)
+        serial_ids += list(item.stock_production_lot_serial_ids.filtered(
+            lambda a: a.consumed
+        ).mapped('id'))
+
+        item.stock_production_lot_serial_ids = [(6, 0, serial_ids)]
+
+
+@api.model
+def get_stock_quant(self):
+    return self.quant_ids.filtered(
+        lambda a: a.location_id.name == 'Stock'
+    )
