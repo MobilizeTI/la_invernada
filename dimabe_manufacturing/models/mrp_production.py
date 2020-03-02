c04b9d3a5fdb4a503f2d99cdabe4ab9141f6eaf2
dimabe-odoo <desarrolloti@dimabe.cl>
2020-03-02 16:01:52 -0300

    test

--- dimabe_manufacturing/models/mrp_production.py
+++ dimabe_manufacturing/models/mrp_production.py
@@ -94,8 +94,8 @@ class MrpProduction(models.Model):
             list_product = []
             for item in self:
                 for i in item.bom_id.bom_line_ids:
-                    list_product.append(i.product_id)
-                result = self.env['product.product'].search([('product_id', 'in', list_product)])
+                    list_product.append(i.product_id.id)
+                result = self.env['product.product'].search([('id', 'in', list_product)])
                 models._logger.error(result)
             production.update({
                 'potential_lot_ids': [
