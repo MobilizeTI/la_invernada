52cf307e7a7189542126bf593a19655b07bfea4c
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-03-02 17:25:18 -0300

    debug

--- dimabe_manufacturing/models/unpelled_dried.py
+++ dimabe_manufacturing/models/unpelled_dried.py
@@ -194,21 +194,10 @@ class UnpelledDried(models.Model):
                 'state': 'done',
             })
 
-            prd_move_line = self.env['stock.move.line'].create({
-                'reference': item.out_lot_id.name,
-                'product_id': item.out_product_id.id,
-                'location_id': item.origin_location_id.id,
-                'location_dest_id': item.dest_location_id.id,
-                'qty_done': item.total_out_weight,
-                'product_uom_qty': 0,
-                'product_uom_id': item.out_product_id.uom_id.id,
-                'lot_id': item.out_lot_id.id,
-                'state': 'done',
-                'move_id': stock_move.id
-            })
+            consumed = []
 
             for used_lot_id in oven_use_to_close_ids.mapped('used_lot_ids'):
-                self.env['stock.move.line'].create({
+                tmp = self.env['stock.move.line'].create({
                     'reference': used_lot_id.name,
                     'product_id': used_lot_id.product_id.id,
                     'location_id': used_lot_id.get_stock_quant().location_id.id,
@@ -220,6 +209,22 @@ class UnpelledDried(models.Model):
                     'state': 'done',
                     'move_id': stock_move.id
                 })
+
+                consumed.append([4, tmp.id])
+
+            prd_move_line = self.env['stock.move.line'].create({
+                'consume_line_ids': consumed,
+                'reference': item.out_lot_id.name,
+                'product_id': item.out_product_id.id,
+                'location_id': item.origin_location_id.id,
+                'location_dest_id': item.dest_location_id.id,
+                'qty_done': item.total_out_weight,
+                'product_uom_qty': 0,
+                'product_uom_id': item.out_product_id.uom_id.id,
+                'lot_id': item.out_lot_id.id,
+                'state': 'done',
+                'move_id': stock_move.id
+            })
 
             oven_use_to_close_ids.mapped('dried_oven_id').set_is_in_use(False)
 
