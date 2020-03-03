f63de33707ffd3a063ed4a8c49dd629e4c17465c
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-03-03 12:26:46 -0300

    debug

--- dimabe_manufacturing/models/stock_traceability.py
+++ dimabe_manufacturing/models/stock_traceability.py
@@ -60,5 +60,5 @@ class StockTraceability(models.TransientModel):
     @api.model
     def _get_linked_move_lines(self, move_line):
         move_lines, is_used = super(StockTraceability, self)._get_linked_move_lines(move_line)
-        models._logger.error('{} {}'.format(move_lines, is_used))
+        models._logger.error('{} {} {}'.format(move_line, move_lines, is_used))
         return move_lines, is_used
