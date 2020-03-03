f85a7a7a0ad3e0d7fad2a7f4ec7d8899d820c015
dimabe-odoo <desarrolloti@dimabe.cl>
2020-03-03 08:30:54 -0300

    test

--- dimabe_manufacturing/models/stock_move_line.py
+++ dimabe_manufacturing/models/stock_move_line.py
@@ -13,6 +13,16 @@ class StockMoveLine(models.Model):
 
     tmp_qty_done = fields.Float('Realizado')
 
+    lot_serial = fields.Many2many('stock_production_lot_serial',compute='get_serial')
+    
+    @api.multi
+    def get_serial(self):
+        for item in self:
+            if item.lot_id:
+                for serial in item.stock_production_lot_serial_ids:
+                    models._logger.error(serial.id)
+
+
     @api.multi
     def _compute_count_stock_production_lot_serial(self):
         for item in self:
