34e0665d82d3353e4dad05705adbb0618d0c7a07
dimabe-odoo <desarrolloti@dimabe.cl>
2020-03-05 17:14:59 -0300

    test

--- dimabe_manufacturing/models/stock_picking.py
+++ dimabe_manufacturing/models/stock_picking.py
@@ -127,6 +127,7 @@ class StockPicking(models.Model):
         for item in self:
             custom_serial = item.validate_barcode(barcode)
             res = super(StockPicking, item).on_barcode_scanned(barcode)
+            move_id = fields.Integer()
             for move in item.move_lines:
                 if move.product_id.id == custom_serial.stock_production_lot_id.product_id.id:
                     raise models.ValidationError(move.id)
