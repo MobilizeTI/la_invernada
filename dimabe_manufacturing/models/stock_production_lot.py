006ebe99e6a09d568a569b71b44e5588b0595580
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-03-05 10:11:03 -0300

    debug

--- dimabe_manufacturing/models/stock_production_lot.py
+++ dimabe_manufacturing/models/stock_production_lot.py
@@ -64,7 +64,7 @@ class StockProductionLot(models.Model):
             ('partner_id', operator, value),
             ('picking_type_code', '=', 'incoming')
         ])
-        models._logger.error(stock_picking_ids)
+        models._logger.error(stock_picking_ids.mapped('name'))
         return [('name', 'in', stock_picking_ids.mapped('name'))]
 
     @api.multi
