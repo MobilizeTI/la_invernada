132b29a4207758fd6a68b647b92f19eb10455273
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-03-06 16:06:33 -0300

    debug

--- dimabe_manufacturing/models/stock_picking.py
+++ dimabe_manufacturing/models/stock_picking.py
@@ -2,7 +2,7 @@ from odoo import models, api, fields
 
 
 class StockPicking(models.Model):
-    _inherit = 'stock.picking'
+    _inherit = ['stock.picking', 'barcodes.barcode_events_mixin']
 
     has_mrp_production = fields.Boolean('tiene orden de producción')
 
