22253d9a9f29f97ca1d952a0f8e8711286f29107
dimabe-odoo <desarrolloti@dimabe.cl>
2020-03-06 18:01:05 -0300

    test

--- dimabe_export_order/models/packing_list}.py
+++ dimabe_export_order/models/packing_list}.py
@@ -1,13 +1,18 @@
 from odoo import models
+import xlsxwriter,xlrd
 
 
 class PackingList(models.AbstractModel):
     _name = 'report.stock_picking.report_packing_list'
     _inherit = 'report.report_xlsx.abstract'
 
-    def generate_xlsx_report(self, workbook, data, stock_pickings):
-        for obj in stock_pickings:
-            report_name = obj.name
+    name = 'Test'
+
+    workbook = xlsxwriter.Workbook('Packing List {}.xlsx'.format(name))
+
+
+    def generate_xlsx_report(self, workbook):
+            report_name = self.name
             sheet = workbook.add_worksheet(report_name[:31])
             bold = workbook.add_format({'bold': True})
-            sheet.write(0, 0, obj.name, bold)
+            sheet.write(0, 0, self.name, bold)
