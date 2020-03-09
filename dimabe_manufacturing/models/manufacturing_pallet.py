b10768daaf827b7e7ccb8f682a6e32d69869a8ea
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-03-09 17:11:50 -0300

    debug

--- dimabe_manufacturing/models/manufacturing_pallet.py
+++ dimabe_manufacturing/models/manufacturing_pallet.py
@@ -201,3 +201,13 @@ class ManufacturingPallet(models.Model):
         for item in self:
             item.lot_available_serial_ids.with_context(stock_picking_id=stock_picking_id).reserve_picking()
 
+    @api.multi
+    def remove_from_picking(self):
+        stock_picking_id = None
+        if 'stock_picking_id' in self.env.context:
+            stock_picking_id = self.env.context['stock_picking_id']
+            for item in self:
+                item.lot_serial_ids.filtered(
+                    lambda a: a.reserved_to_stock_picking_id == stock_picking_id
+                ).unreserved_serial()
+
