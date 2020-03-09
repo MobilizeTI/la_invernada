b37751a37fd3a07f2405b997cb16b2a3defbf06e
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-03-09 11:27:34 -0300

    debug

--- dimabe_manufacturing/models/stock_production_lot.py
+++ dimabe_manufacturing/models/stock_production_lot.py
@@ -140,7 +140,7 @@ class StockProductionLot(models.Model):
             picking_id = self.env.context['stock_picking_id']
         for item in self:
             serial_to_assign_ids = item.stock_production_lot_serial_ids.filtered(
-                lambda a: not a.consumed and a.reserved_to_stock_picking_id == False
+                lambda a: not a.consumed and a.reserved_to_stock_picking_id is False
             )
             raise  models.ValidationError(serial_to_assign_ids)
             serial_to_assign_ids.with_context(stock_picking_id=picking_id).reserve_picking()
