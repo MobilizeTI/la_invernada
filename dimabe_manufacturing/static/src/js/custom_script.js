df82e0a3c7dac6490be5a2c6a1423ea7b10ea21e
unknown <desarrolloti@dimabe.cl>
2020-03-10 17:17:23 -0300

    test

--- dimabe_manufacturing/static/src/js/custom_script.js
+++ dimabe_manufacturing/static/src/js/custom_script.js
@@ -1,4 +1,12 @@
-odoo.define('stock.production.lot',function (require) {
-        var core = require('web.core');
-        core.action.keydown(Enter,console.log('Enter'))
+odoo.define('stock.production.lot', function (require) {
+    'use strict';
+    $(document).ready(function () {
+        $(document).onkeydown(function (e) {
+            if(e.keyCode == 13){
+                if($('button:contains("Guardar")')){
+                    $('button:contains("Guardar")').click();
+                }
+            }
+        })
+    })
 })
