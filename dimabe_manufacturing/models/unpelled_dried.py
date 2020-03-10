eaf3067b5d162775013d56c1bf9b9e4ef957997e
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-03-10 16:35:20 -0300

    debug

--- dimabe_manufacturing/models/unpelled_dried.py
+++ dimabe_manufacturing/models/unpelled_dried.py
@@ -118,9 +118,8 @@ class UnpelledDried(models.Model):
     def _compute_total_pending_lot_count(self):
         for item in self:
             lot_ids = self.env['stock.production.lot'].search([
-                '&amp;', '&amp;', ('producer_id', '=', item.producer_id),
-                ('product_id', '=', item.product_in_id), '|', ('balance', '>', 0),
-                ('reception_state', '=', 'assigned')
+                '&', '&', ('producer_id', '=', item.producer_id),
+                ('product_id', '=', item.product_in_id), '|', ('balance', '>', 0), ('reception_state', '=', 'assigned')
             ])
             item.total_pending_lot_count = len(lot_ids)
 
