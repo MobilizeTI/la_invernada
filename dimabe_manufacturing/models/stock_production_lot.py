e7a63e1d31778bd9c09f78b89538e2cd98be746c
unknown <desarrolloti@dimabe.cl>
2020-03-10 09:35:53 -0300

    test

--- dimabe_manufacturing/models/stock_production_lot.py
+++ dimabe_manufacturing/models/stock_production_lot.py
@@ -171,7 +171,8 @@ class StockProductionLot(models.Model):
     @api.multi
     def generate_standard_serial(self):
         for item in self:
-            raise models.ValidationError(type(item.product_id))
+            pro_templ = self.env['product.template'].search([('id','=',item.product_id)])
+            raise models.ValidationError(pro_templ.name)
 
     @api.model
     def get_stock_quant(self):
