4dd9f0f25e88fab233bd856fa64b8ff1ecc85b12
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-03-10 14:12:38 -0300

    debug

--- dimabe_manufacturing/static/src/js/float_time_widget.js
+++ dimabe_manufacturing/static/src/js/float_time_widget.js
@@ -16,7 +16,7 @@ odoo.define('dimabe_manufacturing.integer_time', function (require) {
                 self._timeCounter();
             }, 1000);
             console.log(self.record.data.active_seconds)
-            console.log(this.$el)
+            console.log(this.$el.context.classList.remove('o_field_empty'))
             this.$el.html($('<span>' + self.record.data.active_seconds + '</span>',{
                 'class': 'success'
             }));
