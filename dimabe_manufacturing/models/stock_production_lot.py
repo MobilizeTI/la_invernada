67b5181b2734f07475422112b2d90a83d78d1042
unknown <desarrolloti@dimabe.cl>
2020-03-10 10:05:37 -0300

    test

--- dimabe_manufacturing/models/stock_production_lot.py
+++ dimabe_manufacturing/models/stock_production_lot.py
@@ -52,7 +52,7 @@ class StockProductionLot(models.Model):
 
     is_reserved = fields.Boolean('Esta reservado?', compute='reserved', default=False)
 
-    producer_id = fields.Many2one('res.partner','Productor')
+    producer_id = fields.Many2one('res.partner', 'Productor')
 
     @api.multi
     def _compute_reception_data(self):
@@ -166,18 +166,40 @@ class StockProductionLot(models.Model):
                 counter += 1
                 tmp = '00{}'.format(counter)
                 serial.serial_number = item.name + tmp[-3:]
-        return res
+        return resp
 
     @api.multi
     def generate_standard_serial(self):
         for item in self:
-            pro_templ = self.env['product.template'].search([('name','=',item.product_id.name)])
-            for pro in pro_templ:
-                models._logger.error(pro.id)
+            serial_ids = []
+            pro_tmp = item.env['product.template'].search([('name','=',item.product_id.name)])
+        for counter in range(item.qty_standard_serial):
+            tmp = '00{}'.format(counter + 1)
+            serial = item.stock_production_lot_serial_ids.filtered(
+                lambda a: a.serial_number == item.name + tmp[-3:]
+            )
+            if serial:
+                if not serial.consumed:
+                    serial.update({
+                        'display_weight': item.standard_weight
+                    })
+                    serial_ids.append(serial.id)
+            else:
+                new_serial = item.env['stock.production.lot.serial'].create({
+                    'stock_production_lot_id': item.id,
+                    'display_weight': item.standard_weight,
+                    'serial_number': item.name + tmp[-3:],
+                    'belong_to_prd_lot': True
+                })
+                serial_ids.append(new_serial.id)
+        serial_ids += list(item.stock_production_lot_serial_ids.filtered(
+            lambda a: a.consumed
+        ).mapped('id'))
+
+        item.stock_production_lot_serial_ids = [(6, 0, serial_ids)]
 
     @api.model
     def get_stock_quant(self):
         return self.quant_ids.filtered(
             lambda a: a.location_id.name == 'Stock'
         )
-
