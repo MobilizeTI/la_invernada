1ab6d8251c49755d617b6304006ae6ab8195615a
unknown <desarrolloti@dimabe.cl>
2020-03-10 09:20:24 -0300

    test

--- dimabe_manufacturing/models/stock_production_lot.py
+++ dimabe_manufacturing/models/stock_production_lot.py
@@ -171,31 +171,7 @@ class StockProductionLot(models.Model):
     @api.multi
     def generate_standard_serial(self):
         for item in self:
-            serial_ids = []
-        for counter in range(item.qty_standard_serial):
-            tmp = '00{}'.format(counter + 1)
-            serial = item.stock_production_lot_serial_ids.filtered(
-                lambda a: a.serial_number == item.name + tmp[-3:]
-            )
-            if serial:
-                if not serial.consumed:
-                    serial.update({
-                        'display_weight': item.standard_weight
-                    })
-                    serial_ids.append(serial.id)
-            else:
-                new_serial = item.env['stock.production.lot.serial'].create({
-                    'stock_production_lot_id': item.id,
-                    'display_weight': item.standard_weight,
-                    'serial_number': item.name + tmp[-3:],
-                    'belong_to_prd_lot': True
-                })
-                serial_ids.append(new_serial.id)
-        serial_ids += list(item.stock_production_lot_serial_ids.filtered(
-            lambda a: a.consumed
-        ).mapped('id'))
-
-        item.stock_production_lot_serial_ids = [(6, 0, serial_ids)]
+            raise models._logger.error(type(item.product_id))
 
     @api.model
     def get_stock_quant(self):
