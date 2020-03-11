aa3e444d1a7c205c4bc6df7b330282112a8e6ee7
unknown <desarrolloti@dimabe.cl>
2020-03-11 08:42:33 -0300

    test js

--- dimabe_manufacturing/static/src/js/custom_script.js
+++ dimabe_manufacturing/static/src/js/custom_script.js
@@ -2,7 +2,7 @@ odoo.define('dimabe_manufacturing.stock_production_lot', function (require) {
     var core = require('web.core');
 
     document.addEventListener("keydown",function (event) {
-            console.log(event.shiftKey)
+            console.log(core)
     })
 
 })
