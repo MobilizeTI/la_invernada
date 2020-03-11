a96d611a7b459292f4c7ce2e5cab5fe4b18c1ffa
unknown <desarrolloti@dimabe.cl>
2020-03-11 10:05:30 -0300

    test

--- dimabe_manufacturing/static/src/js/custom_script.js
+++ dimabe_manufacturing/static/src/js/custom_script.js
@@ -1,4 +1,4 @@
-odoo.define('dimabe_manufacturing.stock_production_lot', function (require) {
+odoo.define('dimabe_manufacturing', function (require) {
     'use strict';
     var core = require("stock.production.lot")
 })
