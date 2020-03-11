2d28df965412f6418fce47871192a3eca2b75f6d
unknown <desarrolloti@dimabe.cl>
2020-03-11 09:18:43 -0300

    test js

--- dimabe_manufacturing/static/src/js/custom_script.js
+++ dimabe_manufacturing/static/src/js/custom_script.js
@@ -1,10 +1,10 @@
 odoo.define('dimabe_manufacturing', function (require) {
-    var core = require('web.core');
-    var field = new odoo.Model('stock.production.lot')
-    field.query(['name'])
+    var model = require('web.Model');
+    var Lot = new Model('stock.production.lot');
     document.addEventListener("keydown",function (event) {
         if (event.code == 'Enter')
-            console.log(field)
+            console.log(Lot)
+
     })
 
 })
