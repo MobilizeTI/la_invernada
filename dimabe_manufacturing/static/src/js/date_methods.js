509e158c24aab7ecbae88431728afc0298eaa40a
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-03-11 09:44:47 -0300

    debug

--- dimabe_manufacturing/static/src/js/date_methods.js
+++ dimabe_manufacturing/static/src/js/date_methods.js
@@ -1,24 +1,27 @@
-function toDateFormat(seconds) {
-    var days = parseInt((seconds / 86400).toString())
-    var hours = 0
-    var minutes = 0
-    var sec = 0
-    if (seconds % 86400 > 0) {
-        hours = parseInt(((seconds % 86400) / 3600).toString())
-        if ((seconds % 86400) % 3600 > 0) {
-            minutes = parseInt((((seconds % 86400) % 3600) / 60).toString())
-            if (((seconds % 86400) % 3600) % 60 > 0)
-                sec = parseInt((((seconds % 86400) % 3600) % 60).toString())
+odoo.define('dimabe_manufacturing.date_helper', function (require) {
+    'use strict'
+    var toDateFormat = function (seconds) {
+        var days = parseInt((seconds / 86400).toString())
+        var hours = 0
+        var minutes = 0
+        var sec = 0
+        if (seconds % 86400 > 0) {
+            hours = parseInt(((seconds % 86400) / 3600).toString())
+            if ((seconds % 86400) % 3600 > 0) {
+                minutes = parseInt((((seconds % 86400) % 3600) / 60).toString())
+                if (((seconds % 86400) % 3600) % 60 > 0)
+                    sec = parseInt((((seconds % 86400) % 3600) % 60).toString())
+            }
+
         }
-
+        return `${normalizeNumber(days)}(d) ${normalizeNumber(hours)}:${normalizeNumber(minutes)}:${normalizeNumber(sec)}`
     }
-    return `${normalizeNumber(days)}(d) ${normalizeNumber(hours)}:${normalizeNumber(minutes)}:${normalizeNumber(sec)}`
-}
 
-function normalizeNumber(number) {
-    var tmp = '0' + number
-    tmp = tmp.substr(tmp.length - 2, 2)
-    return tmp
-}
+    var normalizeNumber = function (number) {
+        var tmp = '0' + number
+        tmp = tmp.substr(tmp.length - 2, 2)
+        return tmp
+    }
 
-export {normalizeNumber, toDateFormat}
+    return {normalizeNumber, toDateFormat}
+})
