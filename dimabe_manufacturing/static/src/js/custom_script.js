af2de302db872f00a98d8ba15f38f39320fabf8e
unknown <desarrolloti@dimabe.cl>
2020-03-11 09:08:57 -0300

    test js

--- dimabe_manufacturing/static/src/js/custom_script.js
+++ dimabe_manufacturing/static/src/js/custom_script.js
@@ -1,9 +1,10 @@
 odoo.define('dimabe_manufacturing.stock_production_lot', function (require) {
     var core = require('web.core');
-    var FormRenderer = require('web.FormRenderer');
+    var field = new odoo.Model('stock.production.lot')
+    field.query(['name'])
     document.addEventListener("keydown",function (event) {
         if (event.code == 'Enter')
-            console.log(FormRenderer)
+            console.log(field)
     })
 
 })
