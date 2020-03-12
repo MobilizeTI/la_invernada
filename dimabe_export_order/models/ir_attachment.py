3b31de7e44fea79748add238a9f0f2c8b6205b53
unknown <desarrolloti@dimabe.cl>
2020-03-12 15:21:38 -0300

    test}

--- dimabe_export_order/models/ir_attachment.py
+++ dimabe_export_order/models/ir_attachment.py
@@ -1,6 +1,10 @@
-from odoo import models,fields,api
+from odoo import models, fields, api
+
 
 class IrAttachment(models.Model):
     _inherit = "ir.attachment"
 
-
+    @api.model
+    def create(self):
+        res = super(StockProductionLotSerial, self).create(values_list)
+        return res
