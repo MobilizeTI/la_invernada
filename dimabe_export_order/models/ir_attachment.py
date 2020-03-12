7d50a97f68cfdfbd948a7b4696996433ca2d7016
unknown <desarrolloti@dimabe.cl>
2020-03-12 16:22:57 -0300

    debug

--- dimabe_export_order/models/ir_attachment.py
+++ dimabe_export_order/models/ir_attachment.py
@@ -5,3 +5,9 @@ class IrAttachment(models.Model):
     _inherit = "ir.attachment"
 
     counter = fields.Integer("Contador")
+
+
+    @api.model
+    def create(self,values_list):
+        res = super(IrAttachment, self).create(values_list)
+        return res
