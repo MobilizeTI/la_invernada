4f814e5d55cc1806abc6094ee0aabc3ecae303fb
unknown <desarrolloti@dimabe.cl>
2020-03-12 14:32:22 -0300

    test

--- dimabe_manufacturing/models/stock_production_lot.py
+++ dimabe_manufacturing/models/stock_production_lot.py
@@ -49,11 +49,11 @@ class StockProductionLot(models.Model):
 
     is_prd_lot = fields.Boolean('Es Lote de salida de Proceso')
 
-    is_standard_weight = fields.Boolean('Series Peso Estandar', compute='_compute_is_standard_weight')
+    is_standard_weight = fields.Boolean('Series Peso Estandar')
 
-    standard_weight = fields.Float('Peso Estandar',compute='_get_product_weight')
+    standard_weight = fields.Float('Peso Estandar')
 
-    qty_standard_serial = fields.Integer('Cantidad de Series', default=1)
+    qty_standard_serial = fields.Integer('Cantidad de Series')
 
     stock_production_lot_serial_ids = fields.One2many(
         'stock.production.lot.serial',
@@ -82,27 +82,6 @@ class StockProductionLot(models.Model):
 
     is_reserved = fields.Boolean('Esta reservado?', compute='reserved', default=False)
 
-<<<<<<< HEAD
-    producer_id = fields.Many2one('res.partner', 'Productor')
-
-    @api.multi
-    def print_all_serial(self):
-        return self.env.ref('dimabe_manufacturing.action_print_all_serial') \
-            .report_action(self.stock_production_lot_serial_ids)
-
-    @api.multi
-    def _get_product_weight(self):
-        self.ensure_one()
-        self.standard_weight = self.product_id.weight
-
-    @api.multi
-    def _compute_is_standard_weight(self):
-        self.ensure_one()
-        if self.product_id.is_standard_weight:
-            self.is_standard_weight = True
-        else:
-            self.is_standard_weight = False
-=======
     label_producer_id = fields.Many2one('res.partner','Productor')
 
     context_picking_id = fields.Integer(
@@ -158,6 +137,19 @@ class StockProductionLot(models.Model):
         'Hr en Secador',
         related='oven_use_ids.active_seconds'
     )
+
+    @api.multi
+    def _get_product_weight(self):
+        self.ensure_one()
+        self.standard_weight = self.product_id.weight
+
+    @api.multi
+    def _compute_is_standard_weight(self):
+        self.ensure_one()
+        if self.product_id.is_standard_weight:
+            self.is_standard_weight = True
+        else:
+            self.is_standard_weight = False
 
     @api.multi
     def _compute_product_variety(self):
@@ -218,7 +210,6 @@ class StockProductionLot(models.Model):
             )
 
         return [('id', 'in', stock_production_lot_ids.mapped('id'))]
->>>>>>> test
 
     @api.multi
     def _compute_reception_data(self):
@@ -362,39 +353,36 @@ class StockProductionLot(models.Model):
     def generate_standard_serial(self):
         for item in self:
             serial_ids = []
-            for counter in range(len(item.stock_production_lot_serial_ids) + quantity):
-                tmp = '00{}'.format(counter + 1)
-                serial = item.stock_production_lot_serial_ids.filtered(
-                    lambda a: a.serial_number == item.name + tmp[-3:]
-                )
-                if serial:
-                    if not serial.consumed:
-                        serial.update({
-                            'display_weight': item.product_id.weight
-                        })
-                        serial_ids.append(serial.id)
-                else:
-                    new_serial = item.env['stock.production.lot.serial'].create({
-                        'stock_production_lot_id': item.id,
-                        'display_weight': item.standard_weight,
-                        'serial_number': item.name + tmp[-3:],
-                        'belong_to_prd_lot': True
+        for counter in range(item.qty_standard_serial):
+            tmp = '00{}'.format(counter + 1)
+            serial = item.stock_production_lot_serial_ids.filtered(
+                lambda a: a.serial_number == item.name + tmp[-3:]
+            )
+            if serial:
+                if not serial.consumed:
+                    serial.update({
+                        'display_weight': item.standard_weight
                     })
-                    serial_ids.append(new_serial.id)
-            serial_ids += list(item.stock_production_lot_serial_ids.filtered(
-                lambda a: a.consumed
-            ).mapped('id'))
-            item.stock_production_lot_serial_ids = [(6, 0, serial_ids)]
+                    serial_ids.append(serial.id)
+            else:
+                new_serial = item.env['stock.production.lot.serial'].create({
+                    'stock_production_lot_id': item.id,
+                    'display_weight': item.standard_weight,
+                    'serial_number': item.name + tmp[-3:],
+                    'belong_to_prd_lot': True
+                })
+                serial_ids.append(new_serial.id)
+        serial_ids += list(item.stock_production_lot_serial_ids.filtered(
+            lambda a: a.consumed
+        ).mapped('id'))
 
-
+        item.stock_production_lot_serial_ids = [(6, 0, serial_ids)]
 
     @api.model
     def get_stock_quant(self):
         return self.quant_ids.filtered(
             lambda a: a.location_id.name == 'Stock'
         )
-<<<<<<< HEAD
-=======
 
     def show_available_serial(self):
         return {
@@ -407,4 +395,3 @@ class StockProductionLot(models.Model):
             'target': 'new',
             'context': self.env.context
         }
->>>>>>> test
