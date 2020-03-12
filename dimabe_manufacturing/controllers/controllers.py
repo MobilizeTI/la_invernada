d1bc42e5611064e2bb7bc3ddf534c521458abe6f
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-03-12 17:29:02 -0300

    debug

--- dimabe_manufacturing/controllers/controllers.py
+++ dimabe_manufacturing/controllers/controllers.py
@@ -12,6 +12,14 @@ class DataSetController(main.DataSet):
     def call_kw(self, model, method, args, kwargs, path=None):
         res = super(DataSetController, self).call_kw(model, method, args, kwargs)
         if model == 'stock.production.lot' and method == 'onchange':
+            for reg in args:
+                if 'id' in reg:
+                    lot_id = http.request.env['stock.production.lot'].search([('id', '=', reg['id'])])
+                    if lot_id:
+                        if 'stock_production_lot_serial_ids' in reg:
+                            lot_id.write({
+                                'stock_production_lot_serial_ids': reg['stock_production_lot_serial_ids']
+                            })
             _logger.error(res)
             _logger.error(args)
 
