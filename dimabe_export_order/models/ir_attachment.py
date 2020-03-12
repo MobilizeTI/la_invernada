74a5a76a0978dbe3f412a92552f82cd8ec29a2b9
unknown <desarrolloti@dimabe.cl>
2020-03-12 15:24:08 -0300

    test}

--- dimabe_export_order/models/ir_attachment.py
+++ dimabe_export_order/models/ir_attachment.py
@@ -5,6 +5,17 @@ class IrAttachment(models.Model):
     _inherit = "ir.attachment"
 
     @api.model
-    def create(self):
+    def create(self,values_list):
+        for item in self:
+            models._logger.error("id {}".format(item.id))
+            models._logger.error(item.id)
+            models._logger.error(item.id)
+            models._logger.error(item.id)
+            models._logger.error(item.id)
+            models._logger.error(item.id)
+            models._logger.error(item.id)
+            models._logger.error(item.id)
+            models._logger.error(item.id)
+            models._logger.error(item.id)
         res = super(StockProductionLotSerial, self).create(values_list)
         return res
