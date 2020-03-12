259279f52a9d6b493606f397c60024957aa02704
unknown <desarrolloti@dimabe.cl>
2020-03-12 15:39:32 -0300

    test}

--- dimabe_export_order/models/ir_attachment.py
+++ dimabe_export_order/models/ir_attachment.py
@@ -4,18 +4,4 @@ from odoo import models, fields, api
 class IrAttachment(models.Model):
     _inherit = "ir.attachment"
 
-    @api.model
-    def create(self,values_list):
-        res = super(IrAttachment, self).create(values_list)
-        for item in self:
-            models._logger.error("id {}".format(item.id))
-            models._logger.error(item.id)
-            models._logger.error(item.id)
-            models._logger.error(item.id)
-            models._logger.error(item.id)
-            models._logger.error(item.id)
-            models._logger.error(item.id)
-            models._logger.error(item.id)
-            models._logger.error(item.id)
-            models._logger.error(item.id)
-        return res
+    counter = fields.Integer('Contador',default=0)
