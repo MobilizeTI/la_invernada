89447690301013259c660907ab9ba70199869010
unknown <desarrolloti@dimabe.cl>
2020-03-13 17:24:26 -0300

    test

--- dimabe_export_order/models/stock_picking.py
+++ dimabe_export_order/models/stock_picking.py
@@ -221,7 +221,7 @@ class StockPicking(models.Model):
             item.counter = index
             index -= 1
             item.datas = tools.image_resize_image_medium(
-                item.datas, size=(800, 750)
+                item.datas, size=(200, 270)
             )
         return self.env.ref('dimabe_export_order.action_dispatch_label_report') \
             .report_action(self.picture)
