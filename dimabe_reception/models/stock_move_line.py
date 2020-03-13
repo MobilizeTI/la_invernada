729b6939776f6191b4b5ab27988337db1610ab00
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-03-13 16:27:09 -0300

    debug lote insumos

--- dimabe_reception/models/stock_move_line.py
+++ dimabe_reception/models/stock_move_line.py
@@ -12,7 +12,12 @@ class StockMoveLine(models.Model):
             prefix = ''
             if res.move_id.product_id.categ_id.is_canning:
                 prefix = 'ENV'
-            res.lot_name = '{}{}'.format(prefix, res.move_id.picking_id.name)
-            res.qty_done = res.product_uom_qty
+
+            if len(res.move_id.picking_id.move_line_ids) > 1 and not res.move_id.picking_id.move_lines.filtered(
+                lambda a: a.product_id.categ_id.is_mp
+            ):
+                suffix = str(res.move_id.product_id.id)
+                res.lot_name = '{}{}{}'.format(prefix, res.move_id.picking_id.name, suffix)
+                res.qty_done = res.product_uom_qty
 
         return res
