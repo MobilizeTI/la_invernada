ccd8a8507767010bd1523310aff2f2c9d7c797f1
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-03-16 10:54:19 -0300

    debug

--- dimabe_manufacturing/models/stock_production_lot.py
+++ dimabe_manufacturing/models/stock_production_lot.py
@@ -161,14 +161,11 @@ class StockProductionLot(models.Model):
     )
 
     @api.onchange('label_producer_id')
-    @api.returns('self')
     def _onchange_label_producer_id(self):
 
         self.stock_production_lot_serial_ids.write({
             'producer_id': self.label_producer_id.id
         })
-
-        return self
 
     @api.multi
     def _compute_all_pallet_ids(self):
