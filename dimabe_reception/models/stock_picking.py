c4f64057a66ee171d3d281edfd9668442e1c3670
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-03-16 08:47:14 -0300

    debug

--- dimabe_reception/models/stock_picking.py
+++ dimabe_reception/models/stock_picking.py
@@ -332,4 +332,5 @@ class StockPicking(models.Model):
 
         if len(res.move_ids_without_package) > len(res.move_ids_without_package.mapped('product_id')):
             raise models.ValidationError('no puede tener el mismo producto en más de una linea')
+
         return res
