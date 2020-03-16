240262886578c6ddf8590f12458f948b230a07fa
dimabe-odoo <desarrolloti@dimabe.cl>
2020-03-16 09:21:38 -0300

    test

--- dimabe_export_order/models/stock_picking.py
+++ dimabe_export_order/models/stock_picking.py
@@ -220,18 +220,19 @@ class StockPicking(models.Model):
 
     @api.multi
     def generate_report(self):
-        index = len(self.picture)
-        for item in self.picture:
-            item.counter = index
-            index -= 1
-            if item.counter >= 9:
-                item.datas = tools.image_resize_image_medium(
-                    item.datas, size=(438, 479)
-                )
-            else:
-                item.datas = tools.image_resize_image_medium(
-                    item.datas, size=(241, 320)
-                )
+        if not self.picture[0].counter:
+            index = len(self.picture)
+            for item in self.picture:
+                item.counter = index
+                index -= 1
+                if item.counter >= 9:
+                    item.datas = tools.image_resize_image_medium(
+                        item.datas, size=(438, 479)
+                    )
+                else:
+                    item.datas = tools.image_resize_image_medium(
+                        item.datas, size=(241, 320)
+                    )
         return self.env.ref('dimabe_export_order.action_dispatch_label_report') \
             .report_action(self.picture)
 
