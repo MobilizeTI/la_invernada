f605517caaa83b8d8fd794519a01db3a95d6b150
dimabe-odoo <desarrolloti@dimabe.cl>
2020-03-16 08:56:11 -0300

    change image truck report

--- dimabe_export_order/models/stock_picking.py
+++ dimabe_export_order/models/stock_picking.py
@@ -225,7 +225,7 @@ class StockPicking(models.Model):
             item.counter = index
             index -= 1
             item.datas = tools.image_resize_image_medium(
-                item.datas, size=(200, 270)
+                item.datas, size=(320, 241)
             )
         return self.env.ref('dimabe_export_order.action_dispatch_label_report') \
             .report_action(self.picture)
