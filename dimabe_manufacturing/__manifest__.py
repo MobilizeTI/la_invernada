2e18d30706c5dd2403f32ae17951fe04212367e4
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-03-17 15:09:34 -0300

    booleano a vista de partner

--- dimabe_manufacturing/__manifest__.py
+++ dimabe_manufacturing/__manifest__.py
@@ -50,8 +50,9 @@
         'views/oven_use.xml',
         'views/dried_unpelled_history.xml',
         'views/manufacturing_pallet.xml',
-       'views/product_product.xml',
-        'views/stock_picking_type.xml'
+        'views/product_product.xml',
+        'views/stock_picking_type.xml',
+        'views/res_partner.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
