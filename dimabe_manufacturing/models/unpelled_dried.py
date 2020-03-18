47577f304de9bfe269489020fcb9c73c42d83348
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-03-18 15:01:44 -0300

    debug

--- dimabe_manufacturing/models/unpelled_dried.py
+++ dimabe_manufacturing/models/unpelled_dried.py
@@ -243,8 +243,8 @@ class UnpelledDried(models.Model):
                                                  'Solo puede encontrarse en un registro'.format(
                         oven_use.used_lot_id.name
                     ))
-            if item.out_lot_id.stock_production_lot_serial_id:
-                for serial_id in item.out_lot_id.stock_production_lot_serial_id:
+            if item.out_lot_id.stock_production_lot_serial_ids:
+                for serial_id in item.out_lot_id.stock_production_lot_serial_ids:
                     serial_id.canning_id = item.canning_id
         return res
 
