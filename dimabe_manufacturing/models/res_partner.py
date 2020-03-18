9a66dc7dcfe77e0800e45923f941f8cb48c4e999
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-03-18 11:58:55 -0300

    PT Label datos

--- dimabe_manufacturing/models/res_partner.py
+++ dimabe_manufacturing/models/res_partner.py
@@ -22,6 +22,12 @@ class ResPartner(models.Model):
         compute='_compute_state_id_address'
     )
 
+    region_address_id_address = fields.Many2one(
+        'region_address',
+        'Región',
+        compute='_compute_region_address_id_address'
+    )
+
     @api.multi
     def _compute_city_address(self):
         for item in self:
@@ -35,6 +41,13 @@ class ResPartner(models.Model):
             address_child = item.get_address_child()
             if address_child:
                 item.state_id_address = address_child[0].state_id
+
+    @api.multi
+    def _compute_region_address_id_address(self):
+        for item in self:
+            address_child = item.get_address_child()
+            if address_child:
+                item.region_address_id_address = address_child[0].region_address_id
 
     @api.model
     def get_address_child(self):
