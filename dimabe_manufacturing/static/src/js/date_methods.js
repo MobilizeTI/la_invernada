094ff61e4add4db78a4f5fe6494348bf576605c5
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-03-19 15:45:52 -0300

    tiempo transcurrido sin dias

--- dimabe_manufacturing/static/src/js/date_methods.js
+++ dimabe_manufacturing/static/src/js/date_methods.js
@@ -1,20 +1,16 @@
 odoo.define('dimabe_manufacturing.date_helper', function (require) {
     'use strict'
     var toDateFormat = function (seconds) {
-        var days = parseInt((seconds / 86400).toString())
-        var hours = 0
         var minutes = 0
         var sec = 0
-        if (seconds % 86400 > 0) {
-            hours = parseInt(((seconds % 86400) / 3600).toString())
-            if ((seconds % 86400) % 3600 > 0) {
-                minutes = parseInt((((seconds % 86400) % 3600) / 60).toString())
-                if (((seconds % 86400) % 3600) % 60 > 0)
-                    sec = parseInt((((seconds % 86400) % 3600) % 60).toString())
-            }
-
+        var hours = parseInt((seconds / 3600).toString())
+        if (seconds % 3600 > 0) {
+            minutes = parseInt(((seconds % 3600) / 60).toString())
+            if ((seconds % 3600) % 60 > 0)
+                sec = parseInt(((seconds % 3600) % 60).toString())
         }
-        return `${normalizeNumber(days)}(d) ${normalizeNumber(hours)}:${normalizeNumber(minutes)}:${normalizeNumber(sec)}`
+
+        return `${normalizeNumber(hours)}:${normalizeNumber(minutes)}:${normalizeNumber(sec)}`
     }
 
     var normalizeNumber = function (number) {
