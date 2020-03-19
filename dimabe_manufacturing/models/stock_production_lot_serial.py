0cc24137b173bd5dd45e7184581da976f19748ed
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-03-19 09:21:27 -0300

    test de modalidad de agrgear series

--- dimabe_manufacturing/models/stock_production_lot_serial.py
+++ dimabe_manufacturing/models/stock_production_lot_serial.py
@@ -127,6 +127,7 @@ class StockProductionLotSerial(models.Model):
             production = stock_move_line.move_id.production_id[0]
         else:
             work_order = self.env['mrp.workorder'].search([
+                ('final_lot_id', '!=', False),
                 ('final_lot_id', '=', res.stock_production_lot_id.id)
             ])
 
