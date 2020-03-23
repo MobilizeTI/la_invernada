a3c3e72cebf28e5159d8f89bf3038003eb2e7e0a
leonardodimabe <leonardo.norambuena@dimabe.cl>
2020-03-23 12:12:12 -0300

    test data on response dry process controller

--- dimabe_quality_integration/controllers/dry_process_controller.py
+++ dimabe_quality_integration/controllers/dry_process_controller.py
@@ -7,6 +7,13 @@ class DryProcessController(http.Controller):
     @http.route('/api/dry_process', type='json', methods=['GET'], auth='token', cors='*')
     def get_dry_process(self):
         res = request.env['dried.unpelled.history'].sudo().search([])
-        return {
-            'data': res
-        }
+        return res.read([
+            'name',
+            'in_lot_ids',
+            'init_date',
+            'lot_guide_numbers',
+            'finish_date',
+            'in_product_id.name',
+            'in_product_variety',
+            'out_lot_id.name'
+        ])
