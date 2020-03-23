62c79a5c131fb2f2c460fe995d262efa991c9ec4
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-03-23 14:25:53 -0300

    debug

--- dimabe_manufacturing/models/res_config_setttings.py
+++ dimabe_manufacturing/models/res_config_setttings.py
@@ -7,18 +7,29 @@ class ResConfigSettings(models.TransientModel):
 
     label_percent_subtract = fields.Float(
         '% peso Etiqueta',
-        digits=dp.get_precision('% peso Etiqueta'),
-        related='stock_production_lot_serial.label_percent.subtract'
+        digits=dp.get_precision('% peso Etiqueta')
     )
 
-    label_percent_subtract_value = fields.Float(
-        'Valor a Calcular',
-        compute='_compute_label_percent_subtract_value',
-        store=True
-    )
+    def set_values(self):
+        super(ResConfigSettings, self).set_values()
+        set_param = self.env['ir.config_parameter'].sudo().set_param
+        set_param('dimabe_manufacturing.label_percent_subtract', float(self.label_percent_subtract))
 
-    @api.multi
-    @api.depends('label_percent_subtract')
-    def _compute_label_percent_subtract_value(self):
-        for item in self:
-            item.label_percent_subtract_value = item.label_percent_subtract / 100
+    @api.model
+    def get_values(self):
+        res = super(ResConfigSettings, self).get_values()
+        get_param = self.env['ir.config_parameter'].sudo().get_param
+        res['label_percent_subtract'] = float(get_param('dimabe_manufacturing.label_percent_subtract'))
+        return res
+
+    # label_percent_subtract_value = fields.Float(
+    #     'Valor a Calcular',
+    #     compute='_compute_label_percent_subtract_value',
+    #     store=True
+    # )
+    #
+    # @api.multi
+    # @api.depends('label_percent_subtract')
+    # def _compute_label_percent_subtract_value(self):
+    #     for item in self:
+    #         item.label_percent_subtract_value = item.label_percent_subtract / 100
