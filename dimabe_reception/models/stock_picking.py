88447186058a157c20cc16215b59d5d0c10734f6
leonardodimabe <leonardo.norambuena@dimabe.cl>
2020-03-24 15:58:21 -0300

    default value

--- dimabe_reception/models/stock_picking.py
+++ dimabe_reception/models/stock_picking.py
@@ -254,11 +254,11 @@ class StockPicking(models.Model):
                             # calculated_weight = stock_move_line.qty_done / total_qty
 
                             if stock_move_line.lot_id:
-
+                                default_value = stock_picking.avg_unitary_weight or 1
                                 for i in range(int(total_qty)):
                                     tmp = '00{}'.format(i + 1)
                                     self.env['stock.production.lot.serial'].create({
-                                        'calculated_weight': stock_picking.avg_unitary_weight,
+                                        'calculated_weight': default_value,
                                         'stock_production_lot_id': stock_move_line.lot_id.id,
                                         'serial_number': '{}{}'.format(stock_move_line.lot_name, tmp[-3:])
                                     })
