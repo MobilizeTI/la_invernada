a698a0385d121c054ed40af2c7548ea73f82c643
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-03-25 09:41:07 -0300

    fix bom_id many2one

--- dimabe_manufacturing/models/stock_production_lot_serial.py
+++ dimabe_manufacturing/models/stock_production_lot_serial.py
@@ -187,10 +187,10 @@ class StockProductionLotSerial(models.Model):
     @api.model
     def set_bom_canning(self):
         canning_id = self.bom_id.bom_line_ids.filtered(
-            lambda a: 'envases' in [
-                str.lower(a.product_id.categ_id.name),
-                str.lower(a.product_id.categ_id.parent_id.name)
-            ]
+            lambda a: 'envases' in str.lower(a.product_id.categ_id.name) or (
+                    a.product_id.categ_id.parent_id and
+                    'envases' in str.lower(a.product_id.categ_id.parent_id.name)
+            )
         )
         if len(canning_id) == 1:
             self.canning_id = canning_id[0]
