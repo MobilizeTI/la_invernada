0060fe22ec40ac60e8136fddd46b2c13c8eb9eec
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-03-26 17:31:59 -0300

    debug

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -134,9 +134,6 @@ class MrpWorkorder(models.Model):
     @api.multi
     def _compute_potential_lot_planned_ids(self):
         for item in self:
-            raise models.ValidationError(item.production_id.potential_lot_ids.mapped(
-                'qty_to_reserve'
-            ))
             item.potential_serial_planned_ids = item.production_id.potential_lot_ids.filtered(
                 lambda a: a.qty_to_reserve > 0
             ).mapped('stock_production_lot_id.stock_production_lot_serial_ids').filtered(
