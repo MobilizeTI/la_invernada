3fa4878b7289587fa21b8c1854115ddc0dc801d3
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-03-27 09:33:36 -0300

    debug

--- dimabe_manufacturing/models/stock_move.py
+++ dimabe_manufacturing/models/stock_move.py
@@ -5,6 +5,6 @@ class StockMove(models.Model):
     _inherit = 'stock.move'
 
     def _action_assign(self):
-        raise models.ValidationError('{} {}'.format(self.product_id, self.product_uom_qty))
+        raise models.ValidationError('{}'.format(self))
         res = super(StockMove, self)._action_assign()
         return res
