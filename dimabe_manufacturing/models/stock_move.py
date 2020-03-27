d53c2350bc7c7b7995274a4862e9f4d62f97a23d
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-03-27 09:17:20 -0300

    debug

--- dimabe_manufacturing/models/stock_move.py
+++ dimabe_manufacturing/models/stock_move.py
@@ -4,5 +4,7 @@ from odoo import models
 class StockMove(models.Model):
     _inherit = 'stock.move'
 
-    # def _action_assign(self):
-    #     return True
+    def _action_assign(self):
+        raise models.ValidationError(self.production_id)
+        res = super(StockMove, self)._action_assign()
+        return res
