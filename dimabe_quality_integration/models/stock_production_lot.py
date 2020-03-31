e8bd9236fe067113d8b92534f06ee43aa3727d49
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-03-31 14:42:37 -0300

    debug

--- dimabe_quality_integration/models/stock_production_lot.py
+++ dimabe_quality_integration/models/stock_production_lot.py
@@ -1,4 +1,5 @@
 from odoo import fields, models, api
+from odoo.addons import decimal_precision as dp
 
 
 class StockProductionLot(models.Model):
@@ -6,4 +7,21 @@ class StockProductionLot(models.Model):
 
     quality_analysis_id = fields.Many2one('quality.analysis', 'Análisis de Calidad')
 
-    balance = fields.Float('Stock Disponible')
+    balance = fields.Float(
+        'Stock Disponible',
+        digits=dp.get_precision('Product Unit of Measure'),
+        compute='_compute_balance',
+        search='_search_balance'
+    )
+
+    @api.multi
+    def _compute_balance(self):
+        for item in self:
+            item.balance = item.get_stock_quant().balance
+
+    def _search_balance(self, operator, value):
+        lot_ids = self.env['stock.quant'].search([
+            ('balance', operator, value)
+        ]).mapped('lot_id')
+
+        return [('id', 'in', lot_ids)]
