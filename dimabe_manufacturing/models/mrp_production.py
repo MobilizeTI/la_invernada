780cc597c9fc568219c3f3c45f1664b87ac4d433
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-04-02 12:22:28 -0300

    template

--- dimabe_manufacturing/models/mrp_production.py
+++ dimabe_manufacturing/models/mrp_production.py
@@ -309,6 +309,9 @@ class MrpProduction(models.Model):
 
             res = super(MrpProduction, order).button_plan()
 
+            template_id = self.env.ref('dimabe_manufacturing.moving_fruit_template')
+            self.message_post_with_template(template_id.id)
 
+            raise models.ValidationError('lala')
 
             return res
