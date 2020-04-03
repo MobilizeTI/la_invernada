e2f5da2fe632305f5ade1800e1d039f5e93c863b
dimabe-odoo <desarrolloti@dimabe.cl>
2020-04-03 18:54:24 -0300

    test

--- dimabe_manufacturing/models/dried_unpelled_history.py
+++ dimabe_manufacturing/models/dried_unpelled_history.py
@@ -203,15 +203,16 @@ class DriedUnpelledHistory(models.Model):
     def _compute_lot_guide_numbers(self):
         for item in self:
             tmp = ''
-            models._logger.error(item.in_lot_ids.mapped('guide_number'))
-            for guide_number in item.in_lot_ids.mapped('_guide_number'):
+            models._logger.error(item.in_lot_ids.mapped('reception_guide_number'))
+            for guide_number in item.in_lot_ids.mapped('reception_guide_number'):
                 tmp += '{} '.format(guide_number)
                 item.lot_guide_numbers = tmp
 
     @api.multi
     def _compute_in_lot_ids(self):
         for item in self:
-            models._logger.error(item.oven_use_ids.mapped('used_lot_id'))
+            for guide in item.oven_use_ids.mapped('used_lot_id'):
+                models._logger.error(item.oven_use_ids.mapped('used_lot_id'))
             item.in_lot_ids = item.oven_use_ids.mapped('used_lot_id')
 
     @api.multi
