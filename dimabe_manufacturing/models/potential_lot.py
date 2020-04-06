472bd12b9223c2bb50316ae5b3b50a06fb98bd8f
dimabe-odoo <desarrolloti@dimabe.cl>
2020-04-06 19:38:17 -0400

    test

--- dimabe_manufacturing/models/potential_lot.py
+++ dimabe_manufacturing/models/potential_lot.py
@@ -85,6 +85,8 @@ class PotentialLot(models.Model):
     def reserve_stock_lot(self):
         for item in self:
             serials = self.env['stock.production.lot.serial'].search([('stock_production_lot_id','=',self.stock_production_lot_id.id)])
+            for serial in serials:
+                models._logger.error(serial)
             raise models.ValidationError("LotSerials : {}".format(serials))
             item.is_reserved = True
 
