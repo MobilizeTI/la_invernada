455c2782572b824ac4340861c263d8de98fb0762
leonardodimabe <leonardo.norambuena@dimabe.cl>
2020-04-06 16:38:32 -0400

    fix lotes de salida no terminados

--- dimabe_quality_integration/controllers/dry_process_controller.py
+++ dimabe_quality_integration/controllers/dry_process_controller.py
@@ -10,22 +10,23 @@ class DryProcessController(http.Controller):
         search_date = sinceDate or (date.today() - timedelta(days=1))
         result = request.env['dried.unpelled.history'].sudo().search([])
         processResult = []
-        for res in result: 
-            processResult.append({
-                'name': res.name,
-                'inLotIds': res.mapped('in_lot_ids.name'),
-                'initDate': res.init_date or res.create_date,
-                'guideNumbers': res.lot_guide_numbers,
-                'finishDate': res.finish_date or res.write_date,
-                'productName': res.in_product_id.name,
-                'productId': res.in_product_id.id,
-                'productVariety': res.in_product_variety,
-                'outLot': res.out_lot_id.name,
-                'producerName': res.producer_id.name,
-                'producerId': res.producer_id.id,
-                'totalInWeight': res.total_in_weight,
-                'totalOutWeight': res.total_out_weight,
-                'performance': res.performance,
-                'OdooUpdatedAt': res.write_date
-            })
+        for res in result:
+            if res.finish_date: 
+                processResult.append({
+                    'name': res.name,
+                    'inLotIds': res.mapped('in_lot_ids.name'),
+                    'initDate': res.init_date or res.create_date,
+                    'guideNumbers': res.lot_guide_numbers,
+                    'finishDate': res.finish_date or res.write_date,
+                    'productName': res.in_product_id.name,
+                    'productId': res.in_product_id.id,
+                    'productVariety': res.in_product_variety,
+                    'outLot': res.out_lot_id.name,
+                    'producerName': res.producer_id.name,
+                    'producerId': res.producer_id.id,
+                    'totalInWeight': res.total_in_weight,
+                    'totalOutWeight': res.total_out_weight,
+                    'performance': res.performance,
+                    'OdooUpdatedAt': res.write_date
+                })
         return processResult
