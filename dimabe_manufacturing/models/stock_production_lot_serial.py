5c6e774ea6609a831b6894388c4f715e89978cd8
dimabe-odoo <desarrolloti@dimabe.cl>
2020-04-07 10:11:26 -0400

    test

--- dimabe_manufacturing/models/stock_production_lot_serial.py
+++ dimabe_manufacturing/models/stock_production_lot_serial.py
@@ -250,7 +250,7 @@ class StockProductionLotSerial(models.Model):
 
     @api.multi
     def reserve_serial(self):
-        raise models.ValidationError(sum(self))
+        raise models.ValidationError(sum(self.display_weight))
         if 'mrp_production_id' in self.env.context and 'from_lot' in self.env.context:
             production = self.env['mrp.production'].search([('id', '=', self.env.context['mrp_production_id'])])
             from_lot = self.env.context['from_lot']
