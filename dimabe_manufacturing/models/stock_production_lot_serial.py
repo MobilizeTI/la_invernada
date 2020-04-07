26b6fd585d08cd959806f451486f47550858ae93
dimabe-odoo <desarrolloti@dimabe.cl>
2020-04-06 22:28:53 -0400

    test

--- dimabe_manufacturing/models/stock_production_lot_serial.py
+++ dimabe_manufacturing/models/stock_production_lot_serial.py
@@ -251,15 +251,13 @@ class StockProductionLotSerial(models.Model):
     @api.multi
     def reserve_serial(self):
         if 'mrp_production_id' in self.env.context:
-            # production = self.env['mrp.production'].search([('id', '=', self.env.context['mrp_production_id'])])
-            raise models.ValidationError(self.env['mrp.production'].search([('id', '=', self.env.context['mrp_production_id'])]))
-            if not self.env['mrp.production'].search([('id', '=', self.env.context['mrp_production_id'])]):
+            production = self.env['mrp.production'].search([('id', '=', self.env.context['mrp_production_id'])])
+            if not production:
                 raise models.ValidationError('No se encontró la orden de producción a la que reservar el producto')
         for item in self:
             models._logger.error(item)
             item.update({
-                'reserved_to_production_id': self.env['mrp.production'].search(
-                    [('id', '=', self.env.context['mrp_production_id'])]).id
+                'reserved_to_production_id': production.id
             })
             # stock_quant = item.stock_production_lot_id.get_stock_quant()
             # stock_quant.sudo().update({
