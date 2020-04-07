5d9cf52da1117af923791f1428e6ba5a1d28c786
dimabe-odoo <desarrolloti@dimabe.cl>
2020-04-07 11:12:08 -0400

    test

--- dimabe_manufacturing/models/stock_production_lot_serial.py
+++ dimabe_manufacturing/models/stock_production_lot_serial.py
@@ -252,15 +252,17 @@ class StockProductionLotSerial(models.Model):
         if 'mrp_production_id' in self.env.context:
             production = self.env['mrp.production'].search([('id', '=', self.env.context['mrp_production_id'])])
             from_lot = self.env.context['from_lot']
-            raise models.ValidationError(from_lot)
             if not production:
                 raise models.ValidationError('No se encontró la orden de producción a la que reservar el producto')
             for item in self:
+                if from_lot:
+                    item.update({
+                        'reserved_to_production_id': production.id
+                    })
+                else:
+                    total = total + item.display_weight
+                    models._logger.error(total)
 
-                # if from_lot:
-                item.update({
-                    'reserved_to_production_id': production.id
-                })
         else:
             raise models.ValidationError('no se pudo identificar producción')
 
