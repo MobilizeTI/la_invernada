a7a082b7bc5ea406ef97e15afb9289cdf6443f5b
dimabe-odoo <desarrolloti@dimabe.cl>
2020-04-09 16:08:30 -0400

    get potential

--- dimabe_manufacturing/models/mrp_production.py
+++ dimabe_manufacturing/models/mrp_production.py
@@ -276,7 +276,7 @@ class MrpProduction(models.Model):
                     serial.stock_production_lot_id.name
             ))
 
-            potential_lot = self.env['potential.lot'].search([('stock_production_lot_id','=',lot)])
+            potential_lot = self.env['potential.lot'].search([('stock_production_lot_id.id','=',lot.id)])
 
             move_line = self.env['stock.move.line'].create({
                 'product_id': lot.product_id.id,
