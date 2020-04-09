1254768944f18c977c16c0142d449b4259a41a8e
dimabe-odoo <desarrolloti@dimabe.cl>
2020-04-08 22:59:31 -0400

    test

--- dimabe_manufacturing/models/stock_production_lot_serial.py
+++ dimabe_manufacturing/models/stock_production_lot_serial.py
@@ -275,7 +275,7 @@ class StockProductionLotSerial(models.Model):
                     })
 
                     for stock in stock_move:
-                        item.add_move_line(stock)
+                        item.add_move_line(stock,stock_quant)
         else:
             raise models.ValidationError('no se pudo identificar producción')
 
