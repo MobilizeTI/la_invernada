6b8e70eb461d19edbace9b6cad160f009c4e95b2
dimabe-odoo <desarrolloti@dimabe.cl>
2020-04-09 15:19:05 -0400

    redo

--- dimabe_manufacturing/models/mrp_production.py
+++ dimabe_manufacturing/models/mrp_production.py
@@ -258,42 +258,42 @@ class MrpProduction(models.Model):
         ).mapped('stock_production_lot_serial_ids')
         models._logger.error('linea 258 {}'.format(serial_to_reserve_ids))
         serial_to_reserve_ids[0].with_context(stock_picking_id=self.stock_picking_id.id).reserve_picking(serial_to_reserve_ids)
-            # stock_move = self.stock_picking_id.move_lines.filtered(
-            #         lambda a: a.product_id == serial.stock_production_lot_id.product_id
-            # )
+            stock_move = self.stock_picking_id.move_lines.filtered(
+                    lambda a: a.product_id == serial.stock_production_lot_id.product_id
+            )
 
-            # stock_quant = serial.stock_production_lot_id.get_stock_quant()
+            stock_quant = serial.stock_production_lot_id.get_stock_quant()
 
-            # if not stock_quant:
-            #     raise models.ValidationError('El lote {} aún se encuentra en proceso.'.format(
-            #         serial.stock_production_lot_id.name
-            # ))
+            if not stock_quant:
+                raise models.ValidationError('El lote {} aún se encuentra en proceso.'.format(
+                    serial.stock_production_lot_id.name
+            ))
 
-            # move_line = self.env['stock.move.line'].create({
-            #     'product_id': serial.stock_production_lot_id.product_id.id,
-            #     'lot_id': serial.stock_production_lot_id.id,
-            #     'product_uom_qty': serial.display_weight,
-            #     'product_uom_id': stock_move.product_uom.id,
-            #     'location_id': stock_quant.location_id.id,
-            #     # 'qty_done': item.display_weight,
-            #     'location_dest_id': self.stock_picking_id.partner_id.property_stock_customer.id
-            # })
+            move_line = self.env['stock.move.line'].create({
+                'product_id': serial.stock_production_lot_id.product_id.id,
+                'lot_id': serial.stock_production_lot_id.id,
+                'product_uom_qty': serial.display_weight,
+                'product_uom_id': stock_move.product_uom.id,
+                'location_id': stock_quant.location_id.id,
+                # 'qty_done': item.display_weight,
+                'location_dest_id': self.stock_picking_id.partner_id.property_stock_customer.id
+            })
 
-            # stock_move.sudo().update({
-            #     'move_line_ids': [
-            #         (4, move_line.id)
-            #         ]
-            #     })
+            stock_move.sudo().update({
+                'move_line_ids': [
+                    (4, move_line.id)
+                    ]
+                })
 
-            # serial.reserved_to_stock_picking_id.update({
-            #     'move_line_ids': [
-            #         (4, move_line.id)
-            #     ]
-            #     })
+            serial.reserved_to_stock_picking_id.update({
+                'move_line_ids': [
+                    (4, move_line.id)
+                ]
+                })
 
-            # stock_quant.sudo().update({
-            #     'reserved_quantity': stock_quant.total_reserved
-            #     })
+            stock_quant.sudo().update({
+                'reserved_quantity': stock_quant.total_reserved
+                })
         serial_to_reserve_ids.mapped('stock_production_lot_id').write({
             'can_add_serial': False
         })
