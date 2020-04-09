b975ba0124537c214287cbe39e68960fd0a4ab52
dimabe-odoo <desarrolloti@dimabe.cl>
2020-04-09 10:14:45 -0400

    test

--- dimabe_reception/models/stock_picking.py
+++ dimabe_reception/models/stock_picking.py
@@ -13,38 +13,38 @@ class StockPicking(models.Model):
         'Kilos Guía',
         compute='_compute_weight_guide',
         store=True,
-        digits=dp.get_precision('Product Unit of Measure')
+        #digits=dp.get_precision('Product Unit of Measure')
     )
 
     gross_weight = fields.Float(
         'Kilos Brutos',
-        digits=dp.get_precision('Product Unit of Measure')
+        #digits=dp.get_precision('Product Unit of Measure')
     )
 
     tare_weight = fields.Float(
         'Peso Tara',
-        digits=dp.get_precision('Product Unit of Measure')
+        #digits=dp.get_precision('Product Unit of Measure')
     )
 
     net_weight = fields.Float(
         'Kilos Netos',
         compute='_compute_net_weight',
         store=True,
-        digits=dp.get_precision('Product Unit of Measure')
+        #digits=dp.get_precision('Product Unit of Measure')
     )
 
     canning_weight = fields.Float(
         'Peso Envases',
         compute='_compute_canning_weight',
         store=True,
-        digits=dp.get_precision('Product Unit of Measure')
+        #digits=dp.get_precision('Product Unit of Measure')
     )
 
     production_net_weight = fields.Float(
         'Kilos Netos Producción',
         compute='_compute_production_net_weight',
         store=True,
-        digits=dp.get_precision('Product Unit of Measure')
+        #digits=dp.get_precision('Product Unit of Measure')
     )
 
     # reception_type_selection = fields.Selection([
@@ -88,12 +88,12 @@ class StockPicking(models.Model):
     avg_unitary_weight = fields.Float(
         'Promedio Peso unitario',
         compute='_compute_avg_unitary_weight',
-        digits=dp.get_precision('Product Unit of Measure')
+        #digits=dp.get_precision('Product Unit of Measure')
     )
 
     quality_weight = fields.Float(
         'Kilos Calidad',
-        digits=dp.get_precision('Product Unit of Measure')
+        #digits=dp.get_precision('Product Unit of Measure')
     )
 
     carrier_rut = fields.Char(
