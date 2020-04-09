7173ba7228ce6f3ba0294060970842d02fb2aa7e
dimabe-odoo <desarrolloti@dimabe.cl>
2020-04-09 16:10:30 -0400

    get qty_to_reserve

--- dimabe_manufacturing/models/mrp_production.py
+++ dimabe_manufacturing/models/mrp_production.py
@@ -281,7 +281,7 @@ class MrpProduction(models.Model):
             move_line = self.env['stock.move.line'].create({
                 'product_id': lot.product_id.id,
                 'lot_id': lot.id,
-                'product_uom_qty': potential_lot.qty_to_reserved,
+                'product_uom_qty': potential_lot.qty_to_reserve,
                 'product_uom_id': stock_move.product_uom.id,
                 'location_id': stock_quant.location_id.id,
                 # 'qty_done': item.display_weight,
