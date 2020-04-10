361d1091e6838396449234a156188a01345a0579
dimabe-odo <desarrolloti@dimabe.cl>
2020-04-10 16:16:49 -0400

    test

--- dimabe_manufacturing/models/stock_production_lot_serial.py
+++ dimabe_manufacturing/models/stock_production_lot_serial.py
@@ -404,7 +404,7 @@ class StockProductionLotSerial(models.Model):
                 lambda
                     a: a.lot_id.id == item.stock_production_lot_id.id
             )
-            stock_picking = item.reserved_to_stock_picking_id
+            stock_picking = item.reserved_to_stock_picking_id.id
             if len(move_line) > 1:
 
                 for move in move_line:
