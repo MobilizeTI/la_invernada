c740caa36f6bd3e067e726ceb6cce365ce421b82
dimabe-odo <desarrolloti@dimabe.cl>
2020-04-10 15:46:49 -0400

    minus display_weight

--- dimabe_manufacturing/models/stock_production_lot_serial.py
+++ dimabe_manufacturing/models/stock_production_lot_serial.py
@@ -443,8 +443,8 @@ class StockProductionLotSerial(models.Model):
                 for ml in move_line:
                     if ml.qty_done > 0:
                         raise models.ValidationError('este producto ya ha sido validado')
-                    raise models.ValidationError(ml.product_uom_qty)
-                    ml.write({'move_id': None, 'product_uom_qty': 0})
+
+                    ml.write({'move_id': None, 'product_uom_qty': ml.product_uom_qty - item.display_weight})
                     picking_move_line.filtered(lambda a: a.id == ml.id).write({
                         'move_id': None,
                         'picking_id': None,
