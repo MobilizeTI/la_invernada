6f8f908192137fe6a1b1dc5f5dbe88076b9aecae
dimabe-odo <desarrolloti@dimabe.cl>
2020-04-14 10:02:37 -0400

    test

--- dimabe_manufacturing/models/stock_production_lot_serial.py
+++ dimabe_manufacturing/models/stock_production_lot_serial.py
@@ -342,55 +342,55 @@ class StockProductionLotSerial(models.Model):
     @api.multi
     def reserve_picking(self):
         models._logger.error('linea {330} reserve picking')
-        # if 'stock_picking_id' in self.env.context:
-        #     # stock_picking_id = self.env.context['stock_picking_id']
-        #     # stock_picking = self.env['stock.picking'].search([('id', '=', stock_picking_id)])
-        #     #
-        #     # if not stock_picking:
-        #     #     raise models.ValidationError('No se encontró el picking al que reservar el stock')
-        #     #
-        #     # for item in self:
-        #     #     item.update({
-        #     #         'reserved_to_stock_picking_id': stock_picking.id
-        #     #     })
-        #     #     stock_move = item.reserved_to_stock_picking_id.move_lines.filtered(
-        #     #         lambda a: a.product_id == item.stock_production_lot_id.product_id
-        #     #     )
-        #     #
-        #     #     stock_quant = item.stock_production_lot_id.get_stock_quant()
-        #     #
-        #     #     if not stock_quant:
-        #     #         raise models.ValidationError('El lote {} aún se encuentra en proceso.'.format(
-        #     #             item.stock_production_lot_id.name
-        #     #         ))
-        #     #
-        #     #     move_line = self.env['stock.move.line'].create({
-        #     #         'product_id': item.stock_production_lot_id.product_id.id,
-        #     #         'lot_id': item.stock_production_lot_id.id,
-        #     #         'product_uom_qty': item.display_weight,
-        #     #         'product_uom_id': stock_move.product_uom.id,
-        #     #         'location_id': stock_quant.location_id.id,
-        #     #         # 'qty_done': item.display_weight,
-        #     #         'location_dest_id': stock_picking.partner_id.property_stock_customer.id
-        #     #     })
-        #     #
-        #     #     stock_move.sudo().update({
-        #     #         'move_line_ids': [
-        #     #             (4, move_line.id)
-        #     #         ]
-        #     #     })
-        #     #
-        #     #     item.reserved_to_stock_picking_id.update({
-        #     #         'move_line_ids': [
-        #     #             (4, move_line.id)
-        #     #         ]
-        #     #     })
-        #     #
-        #     #     stock_quant.sudo().update({
-        #     #         'reserved_quantity': stock_quant.total_reserved
-        #     #     })
-        # else:
-        #     raise models.ValidationError('no se pudo identificar picking')
+        if 'stock_picking_id' in self.env.context:
+            stock_picking_id = self.env.context['stock_picking_id']
+            stock_picking = self.env['stock.picking'].search([('id', '=', stock_picking_id)])
+
+            if not stock_picking:
+                raise models.ValidationError('No se encontró el picking al que reservar el stock')
+
+            for item in self:
+                item.update({
+                    'reserved_to_stock_picking_id': stock_picking.id
+                })
+                stock_move = item.reserved_to_stock_picking_id.move_lines.filtered(
+                    lambda a: a.product_id == item.stock_production_lot_id.product_id
+                )
+                raise models.ValidationError(stock_move)
+                stock_quant = item.stock_production_lot_id.get_stock_quant()
+
+                if not stock_quant:
+                    raise models.ValidationError('El lote {} aún se encuentra en proceso.'.format(
+                        item.stock_production_lot_id.name
+                    ))
+
+                move_line = self.env['stock.move.line'].create({
+                    'product_id': item.stock_production_lot_id.product_id.id,
+                    'lot_id': item.stock_production_lot_id.id,
+                    'product_uom_qty': item.display_weight,
+                    'product_uom_id': stock_move.product_uom.id,
+                    'location_id': stock_quant.location_id.id,
+                    # 'qty_done': item.display_weight,
+                    'location_dest_id': stock_picking.partner_id.property_stock_customer.id
+                })
+
+                stock_move.sudo().update({
+                    'move_line_ids': [
+                        (4, move_line.id)
+                    ]
+                })
+
+                item.reserved_to_stock_picking_id.update({
+                    'move_line_ids': [
+                        (4, move_line.id)
+                    ]
+                })
+
+                stock_quant.sudo().update({
+                    'reserved_quantity': stock_quant.total_reserved
+                })
+        else:
+            raise models.ValidationError('no se pudo identificar picking')
 
     @api.multi
     def unreserved_picking(self):
