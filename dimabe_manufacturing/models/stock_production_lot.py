8e686e6b1732de60023f69dc8f7a670dea719bc5
dimabe-odo <desarrolloti@dimabe.cl>
2020-04-14 13:01:45 -0400

    test

--- dimabe_manufacturing/models/stock_production_lot.py
+++ dimabe_manufacturing/models/stock_production_lot.py
@@ -193,6 +193,10 @@ class StockProductionLot(models.Model):
         'Es Equipo Dimabe',
         compute='_compute_is_dimabe_team'
     )
+
+    is_for_dispatch = field.Boolean(
+        'Esta para despacho?'
+    )
     
     @api.multi
     def _compute_can_add_serial(self):
@@ -420,6 +424,9 @@ class StockProductionLot(models.Model):
 
                 stock_quant.sudo().update({
                     'reserved_quantity': stock_quant.total_reserved
+                })
+                item.update({
+                    'is_for_dispatch': True
                 })
             #serial_to_assign_ids.with_context(stock_picking_id=picking_id).reserve_picking()
 
