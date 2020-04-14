6665a9c0bf2c75965b031d79b267728f1760f503
dimabe-odo <desarrolloti@dimabe.cl>
2020-04-14 15:43:33 -0400

    test validation in label

--- dimabe_manufacturing/models/product_product.py
+++ dimabe_manufacturing/models/product_product.py
@@ -40,7 +40,8 @@ class ProductProduct(models.Model):
     def _compute_label_name(self):
         for item in self:
             for value in item.attribute_value_ids:
-                item.label_name = value.name
+                if value == 'Nuez con Cascara':
+                    item.label_name = 'Hola'
 
     @api.multi
     def compute_is_to_manufacturing(self):
