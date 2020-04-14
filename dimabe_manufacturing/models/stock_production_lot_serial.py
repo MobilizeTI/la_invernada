4b847cb48abe234aceef2b57f67a50ffa2d5bfb7
dimabe-odo <desarrolloti@dimabe.cl>
2020-04-14 12:42:15 -0400

    test

--- dimabe_manufacturing/models/stock_production_lot_serial.py
+++ dimabe_manufacturing/models/stock_production_lot_serial.py
@@ -328,7 +328,6 @@ class StockProductionLotSerial(models.Model):
 
             stock_quant = item.stock_production_lot_id.get_stock_quant()
 
-            raise models.ValidationError(stock_quant)
 
             item.update({
                 'reserved_to_production_id': None
@@ -462,7 +461,7 @@ class StockProductionLotSerial(models.Model):
                 )
 
                 stock_quant = item.stock_production_lot_id.get_stock_quant()
-
+                raise models.ValidationError(stock_quant)
                 item.update({
                     'reserved_to_stock_picking_id': None
                 })
