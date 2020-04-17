c343ffed73175031ca5e05d3ce927717c8a0ee30
dimabe-odo <desarrolloti@dimabe.cl>
2020-04-17 12:09:05 -0400

    modify package of product_product

--- dimabe_manufacturing/models/product_product.py
+++ dimabe_manufacturing/models/product_product.py
@@ -33,8 +33,8 @@ class ProductProduct(models.Model):
             for value in item.attribute_value_ids.mapped('name'):
                 if "Saco" in value:
                     list = value.split()
-                    list.insert(2,'de')
                     list.insert(1,' ')
+                    list.insert(2, 'de')
                     list.insert(3,' ')
                     text = ''
                     item.package = text.join(list)
