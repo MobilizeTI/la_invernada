2621d1f961f3f5f23e858288aace7b1379eecaaf
dimabe-odo <desarrolloti@dimabe.cl>
2020-04-22 17:40:36 -0400

    fix error

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -263,12 +263,10 @@ class MrpWorkorder(models.Model):
         self.qty_done = qty_done + custom_serial.display_weight
 
         if custom_serial.id in self.potential_serial_planned_ids.mapped('id'):
-
-            if custom_serial.stock_production_lot_id.product_id == self.product_id:
-                custom_serial.update({
-                    'reserved_to_production_id': self.production_id,
-                    'consumed': True
-                })
+            custom_serial.update({
+                'reserved_to_production_id': self.production_id,
+                'consumed': True
+            })
 
         return res
 
