cd6ea7c49445603c51093f4734bae36a6fb87c71
dimabe-odo <desarrolloti@dimabe.cl>
2020-04-22 09:19:56 -0400

    set page Seleccion Lote

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -139,7 +139,7 @@ class MrpWorkorder(models.Model):
                 lambda a: a.qty_to_reserve > 0
             ).mapped('stock_production_lot_id.stock_production_lot_serial_ids').filtered(
                 lambda b: b.reserved_to_production_id == item.production_id
-            )
+             )
 
     def _inverse_potential_lot_planned_ids(self):
 
