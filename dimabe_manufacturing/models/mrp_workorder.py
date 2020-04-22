6c92c661993711608edb5bd00c2b4862676ef121
dimabe-odo <desarrolloti@dimabe.cl>
2020-04-22 15:00:12 -0400

    test

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -261,7 +261,7 @@ class MrpWorkorder(models.Model):
             return res
         self.qty_done = qty_done + custom_serial.display_weight
         if not custom_serial in self.potential_serial_planned_ids:
-            raise models.ValidationError('La serie {} no esta en las series planificadas'.format(custom_serial.serial_number))
+            raise models.ValidationError(custom_serial)
         custom_serial.update({
             'consumed': True
         })
