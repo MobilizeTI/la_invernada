188b1417c67c251d6f3a71e76adb3c1d88a7a17a
dimabe-odo <desarrolloti@dimabe.cl>
2020-04-22 12:26:27 -0400

    modify black screen

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -136,7 +136,7 @@ class MrpWorkorder(models.Model):
     @api.multi
     def _compute_potential_lot_planned_ids(self):
         for item in self:
-            if item.potential_serial_planned_ids.filtered(lambda a: a.qty_to_reserve > 0).mapped('stock_production_lot_id.stock_production_lot_serial_ids').filtered(
+            if item.potential_serial_planned_ids.mapped('stock_production_lot_id.stock_production_lot_serial_ids').filtered(
                 lambda b:b.reserved_to_production_id == item.production_id
             ):
                 item.potential_serial_planned_ids = item.production_id.potential_lot_ids.filtered(
