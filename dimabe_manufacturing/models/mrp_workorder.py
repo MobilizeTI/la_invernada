30661878f8c808b15a1e6c0cd0392b0aef7fbde8
dimabe-odo <desarrolloti@dimabe.cl>
2020-04-23 09:02:11 -0400

    add validations

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -150,7 +150,8 @@ class MrpWorkorder(models.Model):
 
     def _inverse_potential_lot_planned_ids(self):
         self.potential_serial_planned_ids = self.env['stock.production.lot.serial'].search(
-            [('reserved_to_production_id', '=', self.production_id.id)])
+            [('reserved_to_production_id', '=', self.production_id.id),
+             ('stock_production_lot_id.product_id.id', '=', self.component_id.id)])
 
     @api.multi
     def _compute_summary_out_serial_ids(self):
