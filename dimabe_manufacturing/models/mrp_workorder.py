6a8e814feed557a65548da2ed4829d97d02762c0
dimabe-odo <desarrolloti@dimabe.cl>
2020-04-22 23:50:42 -0400

    modify order

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -260,15 +260,16 @@ class MrpWorkorder(models.Model):
         if res:
             return res
         self.qty_done = qty_done + custom_serial.display_weight
+        custom_serial.update({
+            'reserved_to_production_id': self.production_id.id,
+            'consumed': True
+        })
         self.update({
             'potential_serial_planned_ids': [
                 (4, custom_serial.id)
             ]
         })
-        custom_serial.update({
-            'reserved_to_production_id': self.production_id.id,
-            'consumed': True
-        })
+
         return res
 
     @api.model
