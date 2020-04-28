242d9eee67b4dc63de4cb7e0a044971756170825
leonardodimabe <leonardo.norambuena@dimabe.cl>
2020-04-28 11:56:40 -0400

    change to integer dte type code

--- dimabe_billing_rut/models/dte_type.py
+++ dimabe_billing_rut/models/dte_type.py
@@ -2,5 +2,5 @@ from odoo import models, fields
 
 class DteType(models.Model):
     _name = 'dte.type'
-    code = fields.Char('Código')
+    code = fields.Integer('Código')
     name = fields.Char('Nombre')
