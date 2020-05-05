f905b867ccc413270baa711cc180a21d2576001f
dimabe-odoo <desarrolloti@dimabe.cl>
2020-05-05 18:35:00 -0400

    merge

--- dimabe_manufacturing/models/stock_production_lot_serial.py
+++ dimabe_manufacturing/models/stock_production_lot_serial.py
@@ -223,12 +223,8 @@ class StockProductionLotSerial(models.Model):
     @api.multi
     def _compute_gross_weight(self):
         for item in self:
-            item.real_weight = item.display_weight
-            if not item.is_dried_serial:
-                canning_weight = item.canning_id.weight
-                if not canning_weight:
-                    canning_weight = sum(item.get_possible_canning_id().mapped('weight'))
-                item.gross_weight = item.display_weight + canning_weight
+            canning_weight = sum(item.get_possible_canning_id().mapped('weight'))
+            item.gross_weight = item.display_weight + canning_weight
 
     @api.multi
     def _inverse_real_weight(self):
