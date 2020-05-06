077031d9ceb0d18ea676812cab6b27c662f632ac
unknown <leonardo.norambuenam@gmail.com>
2020-05-06 17:25:15 -0400

    add dte helper class

--- dimabe_billing_rut/models/account_invoice.py
+++ dimabe_billing_rut/models/account_invoice.py
@@ -1,5 +1,49 @@
-from . import base_dte
+from . import dte_helper
+from odoo import models, api, fields
 
-class AccountInvoice(base_dte.BaseDte):
+class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
-    
+    dte_folio = fields.Text(string='Folio DTE')
+    dte_type_id =  fields.Many2one(
+        'dte.type', string = 'Tipo Documento'
+    )
+    dte_xml = fields.Text("XML")
+    dte_pdf = fields.Text("PDF")
+    ted = fields.Text("TED")
+    pdf_url = fields.Text("URL PDF")
+
+    partner_economic_activities = fields.Many2many('custom.economic.activity',related='partner_id.economic_activities')
+    company_economic_activities = fields.Many2many('custom.economic.activity', related='company_id.economic_activities')
+    partner_activity_id = fields.Many2one('custom.economic.activity', string='Actividad del Proveedor')
+    company_activity_id = fields.Many2one('custom.economic.activity', string='Actividad de la Compañía')
+    references = fields.One2many(
+        'account.invoice.references',
+        'invoice_id',
+        readonly=True,
+        states={'draft': [('readonly', False)]},
+    )
+
+    method_of_payment = fields.Selection(
+        [
+            ('1', 'Contado'),
+            ('2', 'Crédito'),
+            ('3', 'Gratuito')
+        ],
+        string="Forma de pago",
+        readonly=True,
+        states={'draft': [('readonly', False)]},
+        default='1',
+    )
+
+    @api.onchange('partner_id')
+    @api.multi
+    def _compute_partner_activity(self):
+        for item in self:
+            activities = []
+            for activity in item.partner_id.economic_activities:
+                activities.append(activity.id)
+            item.partner_activity_id = activities
+
+    @api.one()
+    def send_to_sii(self):
+        dte_helper.DteHelper().send_to_sii(self)
