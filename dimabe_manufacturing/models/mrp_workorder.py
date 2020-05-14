b6571cc4b1ea02983d5f2cce7757fa4142b26223
dimabe-odoo <desarrolloti@gmail.com>
2020-05-14 17:39:42 -0400

    validation

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -244,15 +244,21 @@ class MrpWorkorder(models.Model):
             if self.lot_id.id not in self.active_move_line_ids.mapped('lot_id').mapped('id'):
                 raise models.ValidationError('rot')
             else:
-                for item in self.active_move_line_ids:
-                    if self.lot_id.id == item.lot_id.id:
-                        item.update({
-                            'product_uom_qty': sum(self.potential_serial_planned_ids.filtered(
-                                lambda a: a.stock_production_lot_id.id == item.lot_id.id).mapped('display_weight'))
-                        })
-                        self.qty_done = 0
-                super(MrpWorkorder, self).action_skip()
+                self.active_move_line_ids.filtered(lambda a: a.lot_id.id == self.lot_id.id).write({
+                    'product_uom_qty': self.active_move_line_ids.filtered(
+                        lambda a: a.lot_id.id == self.lot_id.id).product_uom_qty + self.qty_done
+                })
+                super(MrpWorkorder,self,)
         self.qty_done = 0
+
+    @api.multi
+    def organize_move_line(self):
+        for item in self.active_move_line_ids:
+            if self.lot_id.id == item.lot_id.id:
+                item.write({
+                    'product_uom_qty': sum(self.potential_serial_planned_ids.filtered(
+                        lambda a: a.stock_production_lot_id.id == item.lot_id.id).mapped('display_weight'))
+                })
 
     def action_skip(self):
         if self.qty_done > 0:
