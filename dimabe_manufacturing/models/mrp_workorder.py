07e8a4e3363c3f7b4b4ca780f3d7c78f05ac84b4
dimabe-odoo <desarrolloti@dimabe.cl>
2020-05-14 23:52:12 -0400

    validation stock_move -fix error

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -263,11 +263,15 @@ class MrpWorkorder(models.Model):
                     ]
                 })
             else:
-                stock_move.update({[1, item.id, sum(
-                    self.potential_serial_planned_ids.filtered(
-                        lambda a: a.stock_production_lot_id.id == item.id).mapped(
-                        'display_weight')), stock_move.product_uom.id]
-                                   })
+                stock_move.update({
+                    'active_move_line_ids':[
+                        (1,0,{
+                            'qty_done':sum(self.potential_serial_planned_ids.filtered(
+                                lambda a: a.stock_production_lot_id.id == item.id).mapped('display_weight')),
+                            'product_uom_id':stock_move.product_uom.id
+                        })
+                    ]
+                })
 
         self.qty_done = 0
 
