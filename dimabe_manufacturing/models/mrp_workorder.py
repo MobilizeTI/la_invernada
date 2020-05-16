2283ba3507bbb63a28e292a41825aefb20d80adb
dimabe-odoo <desarrolloti@gmail.com>
2020-05-15 22:54:36 -0400

    fix

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -270,7 +270,10 @@ class MrpWorkorder(models.Model):
 
     @api.multi
     def organize_move_line(self):
-
+        for move in self.production_id.move_raw_ids:
+            for active in move.active_move_line_ids:
+                if active.product_qty > 0:
+                    active.unlink()
         for item in self.potential_serial_planned_ids.mapped('stock_production_lot_id'):
             stock_quant = item.get_stock_quant()
             stock_move = self.production_id.move_raw_ids.filtered(lambda a: a.product_id.id == item.product_id.id)
