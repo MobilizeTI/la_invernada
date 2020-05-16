83a38a51358f3d0892a4338b7a9ce337c7ad93d2
dimabe-odoo <desarrolloti@gmail.com>
2020-05-15 21:14:14 -0400

    fix

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -215,23 +215,23 @@ class MrpWorkorder(models.Model):
         res = super(MrpWorkorder, self).write(vals)
         return res
 
-    def open_tablet_view(self):
-        while self.current_quality_check_id:
-            check = self.current_quality_check_id
-            if not check.component_is_byproduct:
-                check.qty_done = 0
-                self.action_skip()
-            else:
-                if not check.lot_id:
-                    lot_tmp = self.env['stock.production.lot'].create({
-                        'name': self.env['ir.sequence'].next_by_code('mrp.workorder'),
-                        'product_id': check.component_id.id,
-                        'is_prd_lot': True
-                    })
-                    check.lot_id = lot_tmp.id
-                    check.qty_done = self.component_remaining_qty
-        self.action_first_skipped_step()
-        return super(MrpWorkorder, self).open_tablet_view()
+    # def open_tablet_view(self):
+    #     while self.current_quality_check_id:
+    #         check = self.current_quality_check_id
+    #         if not check.component_is_byproduct:
+    #             check.qty_done = 0
+    #             self.action_skip()
+    #         else:
+    #             if not check.lot_id:
+    #                 lot_tmp = self.env['stock.production.lot'].create({
+    #                     'name': self.env['ir.sequence'].next_by_code('mrp.workorder'),
+    #                     'product_id': check.component_id.id,
+    #                     'is_prd_lot': True
+    #                 })
+    #                 check.lot_id = lot_tmp.id
+    #                 check.qty_done = self.component_remaining_qty
+    #     self.action_first_skipped_step()
+    #     return super(MrpWorkorder, self).open_tablet_view()
 
     def action_next(self):
         self.validate_lot_code(self.lot_id.name)
