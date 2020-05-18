978092a8d318fe774cb25b60fc406a41e65474cc
dimabe-odoo <desarrolloti@dimabe.cl>
2020-05-17 23:48:13 -0400

    test

--- dimabe_manufacturing/__manifest__.py
+++ dimabe_manufacturing/__manifest__.py
@@ -70,6 +70,7 @@
         'reports/process/laser_discart_report.xml',
         'reports/process/calibrate_report.xml',
         'reports/stock/calibrate_stock_report.xml',
+        'reports/filters/filter_raw.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
