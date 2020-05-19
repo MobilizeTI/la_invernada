d0d7832a3da36d06b1ddccb43d46f9540fefa2c8
dimabe-odoo <desarrolloti@gmail.com>
2020-05-19 14:51:24 -0400

    models.ValidationError

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -219,37 +219,30 @@ class MrpWorkorder(models.Model):
 
     def open_tablet_view(self):
         while self.current_quality_check_id:
-            models._logger.error("Paso 1")
             check = self.current_quality_check_id
-            models._logger.error("Paso 2")
             if not check.component_is_byproduct:
                 check.qty_done = 0
                 self.action_skip()
             else:
-                models._logger.error("Paso 5")
                 if not check.lot_id:
-                    models._logger.error("Paso 6")
                     lot_tmp = self.env['stock.production.lot'].create({
                         'name': self.env['ir.sequence'].next_by_code('mrp.workorder'),
                         'product_id': check.component_id.id,
                         'is_prd_lot': True
                     })
-                    models._logger.error("Paso 7")
                     check.lot_id = lot_tmp.id
-                    models._logger.error("Paso 8")
                     check.qty_done = self.component_remaining_qty
-                    models._logger.error("Paso 9")
                     if check.quality_state == 'none':
-                        models._logger.error("Paso 10")
+                        raise models.ValidationError(check.qty_done)
                         self.action_next()
                 self.action_skip()
-                models._logger.error("Paso 11")
+
         self.action_first_skipped_step()
-        models._logger.error("Paso 12")
         return super(MrpWorkorder, self).open_tablet_view()
 
     def action_next(self):
         self.validate_lot_code(self.lot_id.name)
+
         super(MrpWorkorder, self).action_next()
         self.qty_done = 0
 
