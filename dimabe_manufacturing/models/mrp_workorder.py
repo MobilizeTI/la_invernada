849a1996b4fa5ada39d1294aff5d49f8ee330d61
dimabe-odoo <desarrolloti@gmail.com>
2020-05-20 16:22:55 -0400

    back validation

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -264,7 +264,8 @@ class MrpWorkorder(models.Model):
             ])
             if item not in stock_move.active_move_line_ids.mapped('lot_id'):
                 if not item.location_id:
-                    item.location_id = item.stock_production_lot_serial_ids.mapped('production_id').location_dest_id
+                   # item.location_id = item.stock_production_lot_serial_ids.mapped('production_id').location_dest_id
+                    raise models.ValidationError("Lote {} aun esta en proceso".format(item.name))
                 if not self.lot_produced_id:
                     stock_move.update({
                         'active_move_line_ids': [
