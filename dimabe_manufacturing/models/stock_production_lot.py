52e1083b13e3fa99fe5bdc2cf2c5a200a04ac076
dimabe-odoo <desarrolloti@gmail.com>
2020-05-20 14:48:00 -0400

    add field to raw_report fix error

--- dimabe_manufacturing/models/stock_production_lot.py
+++ dimabe_manufacturing/models/stock_production_lot.py
@@ -228,7 +228,7 @@ class StockProductionLot(models.Model):
             if item.is_dried_lot:
                 dried = self.env['dried.unpelled.history'].search(
                     [('out_lot_id', '=', item.id)]).dest_location_id
-                item.location_id = dried.lot_guide_number
+                item.location_id = dried.lot_guide_numbers
 
     @api.depends('stock_production_lot_serial_ids')
     @api.multi
