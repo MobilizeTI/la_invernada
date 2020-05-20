2a4754c9c7fde14354c74f6cc53c73fdba2e9c43
dimabe-odoo <desarrolloti@gmail.com>
2020-05-20 11:52:08 -0400

    fix validation

--- dimabe_manufacturing/models/mrp_production.py
+++ dimabe_manufacturing/models/mrp_production.py
@@ -213,8 +213,10 @@ class MrpProduction(models.Model):
     @api.multi
     def button_mark_done(self):
         self.calculate_done()
-        super(MrpProduction,self).post_inventory()
+
         res = super(MrpProduction, self).button_mark_done()
+        moves_to_do = self.move_raw_ids.filtered(lambda x: x.state not in ('done', 'cancel'))
+        moves_to_do._action_done()
         serial_to_reserve_ids = self.workorder_ids.mapped('production_finished_move_line_ids').mapped(
             'lot_id').filtered(
             lambda a: a.product_id in self.stock_picking_id.move_ids_without_package.mapped('product_id')
