1f87170da2be56d2853c1ce334310d4f4a1af3d0
dimabe-odoo <desarrolloti@gmail.com>
2020-05-22 18:49:54 -0400

    fix

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -311,7 +311,6 @@ class MrpWorkorder(models.Model):
         self.write({
             'lot_produced_id': self.final_lot_id.id
         })
-
         super(MrpWorkorder, self).do_finish()
         #self.organize_move_line()
 
