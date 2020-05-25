146dcacd1c7019579c37b4151e47d3a68b0c80eb
dimabe-odoo <desarrolloti@gmail.com>
2020-05-24 20:20:04 -0400

    add field to process_report

--- dimabe_manufacturing/models/stock_production_lot_serial.py
+++ dimabe_manufacturing/models/stock_production_lot_serial.py
@@ -139,7 +139,7 @@ class StockProductionLotSerial(models.Model):
     production_id_to_view = fields.Many2one('mrp.production', 'Order de Fabricacion',
                                             compute='_compute_production_id_to_view')
 
-    workcenter_id = fields.Many2one('Centro de Trabajo',related='work_order_id.workcenter_id')
+    workcenter_id = fields.Many2one('Centro de Trabajo','mrp.workcenter',related='work_order_id.workcenter_id')
 
     @api.multi
     def _compute_production_id_to_view(self):
