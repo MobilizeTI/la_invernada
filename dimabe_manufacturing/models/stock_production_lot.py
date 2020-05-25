e6bcf62ff92fe4d1a8ca483eddf610d336ca2fdc
dimabe-odoo <desarrolloti@gmail.com>
2020-05-25 13:04:30 -0400

    modify available_weight

--- dimabe_manufacturing/models/stock_production_lot.py
+++ dimabe_manufacturing/models/stock_production_lot.py
@@ -230,9 +230,10 @@ class StockProductionLot(models.Model):
                     [('out_lot_id', '=', item.id)])
                 item.show_guide_number = dried.lot_guide_numbers
 
+
+    @api.multi
     @api.onchange('serial_not_consumed')
     @api.depends('stock_production_lot_serial_ids')
-    @api.multi
     def _compute_available_weight(self):
         for item in self:
             available_weight = sum(item.stock_production_lot_serial_ids.filtered(
