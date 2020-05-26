d019fb1cd87cf1a18c802d8cf339520d2c4059d3
dimabe-odoo <desarrolloti@gmail.com>
2020-05-26 17:27:53 -0400

    fix error

--- dimabe_manufacturing/models/stock_production_lot.py
+++ dimabe_manufacturing/models/stock_production_lot.py
@@ -262,7 +262,7 @@ class StockProductionLot(models.Model):
                     [('out_lot_id', '=', item.id)]).dest_location_id
                 item.location_id = location_id_dried
             if item.is_prd_lot:
-                if item.location_id = item.stock_production_lot_serial_ids.mapped('production_id').state == 'done'
+                if item.location_id = item.stock_production_lot_serial_ids.mapped('production_id').state == 'done':
                     item.location_id = item.stock_production_lot_serial_ids.mapped('production_id').location_dest_id
 
     @api.depends('stock_production_lot_serial_ids')
