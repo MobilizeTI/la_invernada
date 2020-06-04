4533d673bc187cef85ba715cc1c6f7ef04a2dadc
dimabe-odoo <desarrolloti@gmail.com>
2020-06-04 09:34:32 -0400

    test

--- dimabe_manufacturing/models/stock_production_lot.py
+++ dimabe_manufacturing/models/stock_production_lot.py
@@ -232,6 +232,7 @@ class StockProductionLot(models.Model):
                     duplicates.append(serial)
             for duplicate in duplicates:
                 serial = self.env['stock.production.lot.serial'].search([('serial_number','=',duplicate)])
+                raise models.ValidationError(len(not_duplicates))
                 tmp = '000{}'.format(1 + len(not_duplicates))
                 models._logger.error(item.name + tmp[:-3])
 
