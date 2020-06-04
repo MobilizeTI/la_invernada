df6910db85e80b8ee9fdb1e43d21f1fff871b818
dimabe-odoo <desarrolloti@gmail.com>
2020-06-04 10:59:01 -0400

    test

--- dimabe_manufacturing/models/stock_production_lot.py
+++ dimabe_manufacturing/models/stock_production_lot.py
@@ -234,12 +234,13 @@ class StockProductionLot(models.Model):
             item.stock_production_lot_serial_ids[999].update({
                 'serial_number':item.name + '1000'
             })
-            for duplicate in duplicates:
-                serial = self.env['stock.production.lot.serial'].search([('serial_number', '=', duplicate)])
-                serie += 1
-                serial[1].update({
-                    'serial_number': item.name + '{}'.format(serie)
-                })
+            if duplicates > 0:
+                for duplicate in duplicates:
+                    serial = self.env['stock.production.lot.serial'].search([('serial_number', '=', duplicate)])
+                    serie += 1
+                    serial[1].update({
+                        'serial_number': item.name + '{}'.format(serie)
+                    })
 
     @api.multi
     def _compute_serial_available(self):
