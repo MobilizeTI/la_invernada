0ccd63d215ffe611185cd53987f4f9cac3ccd6c0
dimabe-odoo <desarrolloti@gmail.com>
2020-06-04 10:04:42 -0400

    test

--- dimabe_manufacturing/models/stock_production_lot.py
+++ dimabe_manufacturing/models/stock_production_lot.py
@@ -235,7 +235,6 @@ class StockProductionLot(models.Model):
                 serial = self.env['stock.production.lot.serial'].search([('serial_number','=',duplicate)])
                 serie += 1
                 models._logger.error(item.name + '{}'.format(serie))
-                serie += 1
     @api.multi
     def _compute_serial_available(self):
         for item in self:
