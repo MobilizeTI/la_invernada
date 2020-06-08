ccdd685f3d9fd4ec7cb6a8213dd4fcada9d11b4f
Fabian <Fabian@DESKTOP-FA6C97G>
2020-06-08 13:03:08 -0400

    test

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -334,10 +334,10 @@ class MrpWorkorder(models.Model):
                     })
                     check.lot_id = lot_tmp.id
                     check.qty_done = self.component_remaining_qty
-                    if check.quality_state == 'none' and check.qty_done > 0:
+                    if check.quality_state == 'none':
                         self.action_next()
 
-            self.action_first_skipped_step()
+        self.action_first_skipped_step()
         return super(MrpWorkorder, self).open_tablet_view()
 
 
