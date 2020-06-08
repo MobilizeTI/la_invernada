56e30e5a07ae25cbdf3c8084c28ed879a4557df2
Fabian <Fabian@DESKTOP-FA6C97G>
2020-06-08 13:17:17 -0400

    test

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -322,12 +322,13 @@ class MrpWorkorder(models.Model):
     def open_tablet_view(self):
         while self.current_quality_check_id:
             check = self.current_quality_check_id
+            models._logger.error(check.component_id.name)
             if not check.component_is_byproduct:
                 check.qty_done = 0
                 self.action_skip()
             else:
                 if not check.lot_id:
-                    models._logger.error(check.component_id.name)
+
                     lot_tmp = self.env['stock.production.lot'].create({
                         'name': self.env['ir.sequence'].next_by_code('mrp.workorder'),
                         'product_id': check.component_id.id,
