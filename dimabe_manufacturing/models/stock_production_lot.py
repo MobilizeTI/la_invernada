66541f34a803a638a7281822358406262b1d56ef
Fabian <Fabian@DESKTOP-FA6C97G>
2020-06-09 16:20:10 -0400

    fix error of serials

--- dimabe_manufacturing/models/stock_production_lot.py
+++ dimabe_manufacturing/models/stock_production_lot.py
@@ -635,13 +635,16 @@ class StockProductionLot(models.Model):
         for item in self:
             write_date = self.write_date
             res = super(StockProductionLot, self).write(values)
-            counter = 0
             if not item.is_standard_weight:
                 if write_date == self.write_date:
                     for serial in item.stock_production_lot_serial_ids:
-                        counter += 1
-                        tmp = '00{}'.format(counter)
-                        serial.serial_number = item.name + tmp[-3:]
+                        if not serial.serial_number:
+                            if item.stock_production_lot_serial_ids[-1].serial_number:
+                                counter = int(item.stock_production_lot_serial_ids[-1].serial_number) + 1
+                            else:
+                                counter = 1
+                            tmp = '00{}'.format(counter)
+                            serial.serial_number = item.name + tmp[-3:]
             if len(item.stock_production_lot_serial_ids) > 999:
                 item.check_duplicate()
             return res
