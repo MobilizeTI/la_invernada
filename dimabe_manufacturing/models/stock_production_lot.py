a22df162c9aab579fe17c07ea0026b022925bede
Fabian <Fabian@DESKTOP-FA6C97G>
2020-06-12 17:39:51 -0400

    add filter to potential_lot in stock_picking

--- dimabe_manufacturing/models/stock_production_lot.py
+++ dimabe_manufacturing/models/stock_production_lot.py
@@ -230,7 +230,7 @@ class StockProductionLot(models.Model):
         for item in self:
             if item.is_prd_lot:
                 item.sale_order_id = item.stock_production_lot_serial_ids.mapped('production_id').mapped(
-                    'stock_picking').mapped('sale_id')
+                    'stock_picking_id').mapped('sale_id')
 
     @api.multi
     def _compute_reception_weight(self):
