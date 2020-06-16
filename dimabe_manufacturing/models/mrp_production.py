e05c981ca0b7236616df42f91ad4969de56935cb
Fabian <Fabian@DESKTOP-FA6C97G>
2020-06-16 17:19:20 -0400

    test

--- dimabe_manufacturing/models/mrp_production.py
+++ dimabe_manufacturing/models/mrp_production.py
@@ -289,7 +289,8 @@ class MrpProduction(models.Model):
                 'state': 'assigned'
             })
             group = self.env['res.groups'].search([('id', '=', 68)])
-            raise models.UserError(group.users)
+            user_logon = self.env.user
+            raise models.ValidationError(user_logon)
             for move in item.move_raw_ids:
                 if move.reserved_availability > 0:
                     query = 'DELETE FROM stock_move_line where move_id = {}'.format(move.id)
