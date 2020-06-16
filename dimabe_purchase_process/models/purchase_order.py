4fdcbd65b66e2b2df842f4b47f046e240a5147cb
Fabian <Fabian@DESKTOP-FA6C97G>
2020-06-16 17:46:13 -0400

    add security_validation in button_confirm of purchase_order

--- dimabe_purchase_process/models/purchase_order.py
+++ dimabe_purchase_process/models/purchase_order.py
@@ -44,6 +44,10 @@ class PurchaseOrder(models.Model):
     @api.multi
     def action_rfq_send(self):
         for item in self:
+            group = self.env['res.groups'].search([('name', '=', 'Confirme')])
+            user_logon = self.env.user
+            if user_logon not in group.users:
+                raise models.ValidationError('Usted no cuenta con los permisos para realizar este acción')
             if not item.boss_approval_id:
                 models._logger.error(item.state)
                 item.update({
