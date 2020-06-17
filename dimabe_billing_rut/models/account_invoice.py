4a26ff914e0208beefd8be1862d39f522c24d093
Fabian <Fabian@DESKTOP-FA6C97G>
2020-06-17 13:01:09 -0400

    validate data

--- dimabe_billing_rut/models/account_invoice.py
+++ dimabe_billing_rut/models/account_invoice.py
@@ -3,18 +3,19 @@ import json
 import requests
 from datetime import date
 
+
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
     dte_folio = fields.Text(string='Folio DTE')
-    dte_type_id =  fields.Many2one(
-        'dte.type', string = 'Tipo Documento'
+    dte_type_id = fields.Many2one(
+        'dte.type', string='Tipo Documento'
     )
     dte_xml = fields.Text("XML")
     dte_pdf = fields.Text("PDF")
     ted = fields.Text("TED")
     pdf_url = fields.Text("URL PDF")
 
-    partner_economic_activities = fields.Many2many('custom.economic.activity',related='partner_id.economic_activities')
+    partner_economic_activities = fields.Many2many('custom.economic.activity', related='partner_id.economic_activities')
     company_economic_activities = fields.Many2many('custom.economic.activity', related='company_id.economic_activities')
     partner_activity_id = fields.Many2one('custom.economic.activity', string='Actividad del Proveedor')
     company_activity_id = fields.Many2one('custom.economic.activity', string='Actividad de la Compañía')
@@ -45,9 +46,10 @@ class AccountInvoice(models.Model):
             for activity in item.partner_id.economic_activities:
                 activities.append(activity.id)
             item.partner_activity_id = activities
+
     @api.one
     def send_to_sii(self):
-        #PARA COMPLETAR EL DOCUMENTO SE DEBE BASAR EN http://www.sii.cl/factura_electronica/formato_dte.pdf
+        # PARA COMPLETAR EL DOCUMENTO SE DEBE BASAR EN http://www.sii.cl/factura_electronica/formato_dte.pdf
         if not self.company_activity_id or not self.partner_activity_id:
             raise models.ValidationError('Por favor seleccione las actividades de la compañía y del proveedor')
         if not self.company_id.invoice_rut or not self.partner_id.invoice_rut:
@@ -59,19 +61,20 @@ class AccountInvoice(models.Model):
             raise models.ValidationError('Debe seleccionar el giro de la compañí y proveedor a utilizar')
 
         if self.dte_type_id.code is '110' and self.currency_id.name is not 'USD' and not self.exchange_rate:
-            raise models.ValidationError('Para emitir una factura de exportación la moneda debe ser en USD y debe tener una tasa de cambio')
+            raise models.ValidationError(
+                'Para emitir una factura de exportación la moneda debe ser en USD y debe tener una tasa de cambio')
 
         dte = {}
         dte["Encabezado"] = {}
         dte["Encabezado"]["IdDoc"] = {}
         # El Portal completa los datos del Emisor
         dte["Encabezado"]["IdDoc"] = {"TipoDTE": self.dte_type_id.code}
-        #Si es Boleta de debe indicar el tipo de servicio, por defecto de venta de servicios
+        # Si es Boleta de debe indicar el tipo de servicio, por defecto de venta de servicios
         if self.dte_type_id.code in ('39', 39):
             dte["Encabezado"]["IdDoc"]["IndServicio"] = 3
 
         if not self.dte_type_id.code in ('39', 39, 110):
-            #Se debe inicar SOLO SI los valores indicados en el documento son con iva incluido
+            # Se debe inicar SOLO SI los valores indicados en el documento son con iva incluido
             dte["Encabezado"]["IdDoc"]["MntBruto"] = 1
 
         if self.dte_type_id.code is '110':
@@ -87,45 +90,45 @@ class AccountInvoice(models.Model):
                 'MntTotal': self.amount_total
             }
 
-        #EL CAMPO RUT DE FACTURACIÓN, debe corresponder al RUT de la Empresa
-        dte["Encabezado"]["Emisor"] = {"RUTEmisor": self.company_id.invoice_rut.replace(".","")}
+        # EL CAMPO RUT DE FACTURACIÓN, debe corresponder al RUT de la Empresa
+        dte["Encabezado"]["Emisor"] = {"RUTEmisor": self.company_id.invoice_rut.replace(".", "")}
 
         # EL CAMPO VAT o NIF Del Partner, debe corresponder al RUT , si es empresa extranjera debe ser 55555555-5
-        dte["Encabezado"]["Receptor"] = {"RUTRecep": self.partner_id.invoice_rut.replace(".",""),
+        dte["Encabezado"]["Receptor"] = {"RUTRecep": self.partner_id.invoice_rut.replace(".", ""),
                                          "RznSocRecep": self.partner_id.name,
-                                         "DirRecep": self.partner_id.street +  ' ' + self.partner_id.city,
+                                         "DirRecep": self.partner_id.street + ' ' + self.partner_id.city,
                                          "CmnaRecep": self.partner_id.city,
                                          "GiroRecep": self.partner_activity_id.name}
-        
+
         dte["Encabezado"]["IdDoc"]["TermPagoGlosa"] = self.comment or ''
         dte["Encabezado"]["IdDoc"]["Folio"] = '0'
         dte["Encabezado"]["IdDoc"]["FchEmis"] = str(date.today())
         dte["Detalle"] = []
         for line in self.invoice_line_ids:
-            #El Portal Calculos los Subtotales
+            # El Portal Calculos los Subtotales
             ld = {'NmbItem': line.product_id.name,
-             'DscItem': '',
-             'QtyItem': round(line.quantity, 6),
-             'PrcItem': round(line.price_unit,4)
-            }
+                  'DscItem': '',
+                  'QtyItem': round(line.quantity, 6),
+                  'PrcItem': round(line.price_unit, 4)
+                  }
             if line.product_id.default_code:
                 ld['CdgItem'] = {"TpoCodigo": "INT1",
-                              "VlrCodigo": line.product_id.default_code}
+                                 "VlrCodigo": line.product_id.default_code}
             if line.discount:
-                ld['DescuentoPct']= round(line.discount,2)
+                ld['DescuentoPct'] = round(line.discount, 2)
             dte["Detalle"].append(ld)
         referencias = []
         for reference in self.references:
-            ref = {'TpoDocRef':reference.document_type_reference_id.code or 'SET',
-                   'FolioRef':reference.folio_reference,
-                   'FchRef':reference.document_date.__str__(),
-                   'RazonRef':reference.reason}
+            ref = {'TpoDocRef': reference.document_type_reference_id.code or 'SET',
+                   'FolioRef': reference.folio_reference,
+                   'FchRef': reference.document_date.__str__(),
+                   'RazonRef': reference.reason}
             if reference.code_reference:
-                ref['CodRef'] =reference.code_reference
+                ref['CodRef'] = reference.code_reference
             referencias.append(ref)
         if referencias:
             dte['Referencia'] = referencias
-        #raise models.ValidationError(json.dumps(dte))
+        # raise models.ValidationError(json.dumps(dte))
         self.send_dte(json.dumps(dte))
 
     def send_dte(self, dte):
@@ -146,9 +149,13 @@ class AccountInvoice(models.Model):
         self.ted = data.get("ted", None)
         fecha = data.get("fecha", None)
         total = data.get("total", None)
-        self.pdf_url = "%s/dte/dte_emitidos/pdf/%s/%s/0/%s/%s/%s" % (url, self.dte_type_id.code, self.dte_folio, rut_emisor, fecha, total)
+        self.pdf_url = "%s/dte/dte_emitidos/pdf/%s/%s/0/%s/%s/%s" % (
+        url, self.dte_type_id.code, self.dte_folio, rut_emisor, fecha, total)
 
     @api.multi
     def get_dte(self):
         url = self.company_id.dte_url
-        raise models.ValidationError(url)
+        rut_company = self.company_id.invoice_rut.replace(".", "").split("-")[0]
+        hash = self.company_id.dte_hash
+        auth = requests.auth.HTTPBasicAuth(hash, 'X')
+        raise models.ValidationError(auth)
