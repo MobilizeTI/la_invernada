36db82ce3bb5d20e24ff76b0c40e5e29e7e5f2fb
Fabian <Fabian@DESKTOP-FA6C97G>
2020-06-19 13:55:49 -0400

    add new models and new view for custom_invoice

---
+++ dimabe_billing_rut/static/src/js/action_call.js
@@ -0,0 +1,17 @@
+odoo.define('dimabe_billing_rut.sincronize_now', function (require) {
+        "use string"
+        var core = require('web.core');
+        var ListController = require('web.ListController');
+        var rpc = require('web.rpc')
+        var session = require('web.session');
+        var _t = core._t;
+        ListController.include({
+            renderButtons : function ($node) {
+                this._super.apply(this,arguments);
+                if(this.$buttons){
+                    this.$buttons.find('.oe_action_button').click(this.proxy('action_def'))
+                }
+            }
+        })
+    }
+)
