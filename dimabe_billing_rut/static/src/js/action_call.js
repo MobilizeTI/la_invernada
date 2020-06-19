26a6f1f52c8cb779046817c8d54a4b3d47c2eeda
Fabian <Fabian@DESKTOP-FA6C97G>
2020-06-19 14:28:35 -0400

    add new models and new view for custom_invoice

--- dimabe_billing_rut/static/src/js/action_call.js
+++ dimabe_billing_rut/static/src/js/action_call.js
@@ -1,26 +1,26 @@
-odoo.define('dimabe_billing_rut.sincronize_now', function (require) {
-        "use string"
-        var core = require('web.core');
-        var ListController = require('web.ListController');
-        var rpc = require('web.rpc')
-        var session = require('web.session');
-        var _t = core._t;
-        ListController.include({
-            renderButtons : function ($node) {
-                this._super.apply(this,arguments);
-                if(this.$buttons){
-                    this.$buttons.find('.oe_action_button').click(this.proxy('action_def'))
-                }
-            },
-            action_def: function () {
-            var self =this
+odoo.define('custom_invoice.sincronize_button', function (require) {
+    "use strict";
+    var core = require('web.core');
+    var ListController = require('web.ListController');
+    var rpc = require('web.rpc');
+    var session = require('web.session');
+    var _t = core._t;
+    ListController.include({
+        renderButtons: function ($node) {
+            this._super.apply(this, arguments);
+            if (this.$buttons) {
+                this.$buttons.find('.oe_action_button').click(this.proxy('action_def'));
+            }
+        },
+        action_def: function () {
+            var self = this
             var user = session.uid;
             rpc.query({
-                model: 'custom.invoce',
+                model: 'custom.invoice',
                 method: 'get_dte',
-                args: [[user],{'id':user}],
-                });
-            },
-        })
-    }
+                args: [[user], {'id': user}],
+            });
+        }
+    });
+}
 )
