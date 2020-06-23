3138753590726e377cd1eb0a0283221de2cb5c2e
Fabian <Fabian@DESKTOP-FA6C97G>
2020-06-23 14:33:58 -0400

    start create button for fix error in dispatch

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -496,15 +496,3 @@ class MrpWorkorder(models.Model):
     @api.multi
     def export_excel(self):
         return self.env.ref('report.dimabe_manufacturing.import_excel').report_action()
-
-    class PartnerXlsx(models.AbstractModel):
-        _name = 'report.module_name.report_name'
-        _inherit = 'report.report_xlsx.abstract'
-
-        def generate_xlsx_report(self, workbook, data, partners):
-            for obj in partners:
-                report_name = obj.name
-                # One sheet by partner
-                sheet = workbook.add_worksheet(report_name[:31])
-                bold = workbook.add_format({'bold': True})
-                sheet.write(0, 0, obj.name, bold)
