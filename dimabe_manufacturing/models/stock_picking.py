7e2dc2141c25cbfc809c5de0b07e0469f3510ceb
Fabian <Fabian@DESKTOP-FA6C97G>
2020-06-23 12:12:26 -0400

    fix typo error in last commit

--- dimabe_manufacturing/models/stock_picking.py
+++ dimabe_manufacturing/models/stock_picking.py
@@ -67,7 +67,7 @@ class StockPicking(models.Model):
     def clean_reserved(self):
         for item in self:
             move_without_lot = self.env['stock.move.line'].search([('lot_id','=',None)])
-            quants = self.env['stock_quant'].search([('lot_id','=',None)])
+            quants = self.env['stock.quant'].search([('lot_id','=',None)])
             raise models.ValidationError('moves{},quants{}'.format(len(move_without_lot.filtered(x.state not in ('done', 'cancel')))),len(quants))
             for line in item.move_line_ids_without_package:
                 if line.lot_id not in item.packing_list_lot_ids:
