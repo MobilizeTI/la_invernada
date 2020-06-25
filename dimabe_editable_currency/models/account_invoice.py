d02115e58f7b664480578796c55b9da9c78dacc2
Fabian <Fabian@DESKTOP-FA6C97G>
2020-06-25 14:38:42 -0400

    validation self

--- dimabe_editable_currency/models/account_invoice.py
+++ dimabe_editable_currency/models/account_invoice.py
@@ -31,6 +31,7 @@ class AccountInvoice(models.Model):
     def action_invoice_open(self):
 
         if self.id:
+            raise models.ValidationError(self)
             if not self.exchange_rate or self.exchange_rate == 0:
                 raise models.ValidationError('debe existir una taza de cambio')
 
