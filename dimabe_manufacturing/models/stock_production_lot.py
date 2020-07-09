7a4b2d12a1e460a6069048ff6637b970e80c2020
dimabe-odoo <desarrolloti@dimabe.cl>
2020-07-09 09:10:55 -0400

    modify method

--- dimabe_manufacturing/models/stock_production_lot.py
+++ dimabe_manufacturing/models/stock_production_lot.py
@@ -282,7 +282,7 @@ class StockProductionLot(models.Model):
     def fix_error_inventory(self):
         product_with_lot = self.env['product.product'].search([('tracking', '=', 'lot')]).mapped('id')
         move_line_with_error = self.env['stock.move.line'].search(
-            [('product_id', 'in', product_with_lot), ('lot_id', '=', None)])
+            [('product_id', 'in', product_with_lot), ('lot_id', '=', None),('state','=','done')])
         stock_quant_with_error = self.env['stock.quant'].search(
             [('product_id', 'in', product_with_lot), ('lot_id', '=', None)])
         lot_without_name = self.env['stock.production.lot'].search([('name', '=', '')])
