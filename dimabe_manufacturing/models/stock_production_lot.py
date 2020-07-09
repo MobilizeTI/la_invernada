87959b85af89ef57b570171f71e5849d1722e301
dimabe-odoo <desarrolloti@dimabe.cl>
2020-07-09 16:07:33 -0400

    modify method

--- dimabe_manufacturing/models/stock_production_lot.py
+++ dimabe_manufacturing/models/stock_production_lot.py
@@ -282,7 +282,7 @@ class StockProductionLot(models.Model):
     def fix_error_inventory(self):
         lot_with_problem = []
         for item in self.env['stock.production.lot'].search([]):
-            if item.product_qty != item.available_weight and item.available_weight == 0:
+            if item.product_qty != item.available_weight and item.available_weight == 0 and item.product_id == 2723:
                 lot_with_problem.append(item.name)
         raise models.ValidationError('{},{}'.format(lot_with_problem,len(lot_with_problem)))
 
