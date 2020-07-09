43feb650cd52d6eeab6a6dd649d717f52d198d41
dimabe-odoo <desarrolloti@dimabe.cl>
2020-07-09 15:23:28 -0400

    modify method

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -294,21 +294,8 @@ class MrpWorkorder(models.Model):
     @api.multi
     def fix_order(self):
         for item in self:
-            if item.final_lot_id:
-                item.summary_out_serial_ids = item.final_lot_id.stock_production_lot_serial_ids
-                if item.byproduct_move_line_ids:
-                    item.summary_out_serial_ids += item.byproduct_move_line_ids.filtered(
-                        lambda a: a.lot_id not in item.potential_serial_planned_ids).mapped(
-                        'lot_id'
-                    ).mapped(
-                        'stock_production_lot_serial_ids'
-                    )
-            else:
-                item.summary_out_serial_ids = item.production_finished_move_line_ids.mapped(
-                    'lot_id'
-                ).mapped(
-                    'stock_production_lot_serial_ids'
-                )
+            raise models.ValidationError(item.production_id.check_to_done)
+
 
     @api.multi
     def _compute_byproduct_move_line_ids(self):
