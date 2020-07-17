862ae0c616854b98ef177731de6985f07d58ce54
dimabe-odoo <desarrolloti@dimabe.cl>
2020-07-17 10:19:03 -0400

    test

--- dimabe_billing_rut/models/custom_settlement.py
+++ dimabe_billing_rut/models/custom_settlement.py
@@ -6,8 +6,7 @@ class CustomSettlement(models.Model):
     _name = 'custom.settlement'
     employee_id = fields.Many2one('hr.employee')
 
-
-    contract_id = fields.Many2one('hr.contract', 'Contrato',related='employee_id.contract_id')
+    contract_id = fields.Many2one('hr.contract', 'Contrato', related='employee_id.contract_id')
 
     fired_id = fields.Many2one('custom.fired', 'Causal de Despido')
 
@@ -22,10 +21,4 @@ class CustomSettlement(models.Model):
     @api.multi
     def test(self):
         for item in self:
-            date = datetime.datetime.strftime(item.date_start_contract,"%Y-%m-%d")
-            raise models.ValidationError(date.month)
-
-
-
-
-
+            raise models.UserError(type(item.date_start_contract))
