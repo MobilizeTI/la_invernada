d90265d269d959fcf81dd2b297b5f490f4a7416d
dimabe-odoo <desarrolloti@dimabe.cl>
2020-07-29 18:24:56 -0400

    test

--- dimabe_billing_rut/models/hr_payslip.py
+++ dimabe_billing_rut/models/hr_payslip.py
@@ -6,4 +6,6 @@ class HrPayslip(models.Model):
     @api.multi
     def test(self):
         for item in self:
-            raise models.ValidationError(item.contract_id.employee_id)
+            employee_id = item.contract_id.employee_id
+            leaves = self.env['hr.leave'].search([('employee_id','=',employee_id.id)])
+            raise models.ValidationError(leaves)
