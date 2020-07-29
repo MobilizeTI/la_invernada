60a15c442d8ffe26d7648fa990d118a10bfabc3b
dimabe-odoo <desarrolloti@dimabe.cl>
2020-07-29 18:12:40 -0400

    test

--- dimabe_billing_rut/models/hr_payslip.py
+++ dimabe_billing_rut/models/hr_payslip.py
@@ -6,4 +6,4 @@ class HrPayslip(models.Model):
     @api.multi
     def test(self):
         for item in self:
-            raise models.ValidationError(item.contract_id)
+            raise models.ValidationError(item.contract_id.employee_id)
