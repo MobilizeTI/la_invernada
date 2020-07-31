11a4dc4f6d30be2db19c574b99c728da31d2e08d
dimabe-odoo <desarrolloti@dimabe.cl>
2020-07-31 10:09:33 -0400

    verify compute_number_of_day and modify action_draft

--- dimabe_billing_rut/models/hr_leave.py
+++ dimabe_billing_rut/models/hr_leave.py
@@ -17,6 +17,20 @@ class HrLeave (models.Model):
             sundays = pd.date_range(start=holiday.request_date_from, end=holiday.request_date_to, freq='W-SUN').strftime(
                 '%m/%d/%Y').tolist()
             holiday.number_of_days_display = days.days - (len(saturdays) + len(sundays))
+
+    @api.onchange('date_from', 'date_to', 'employee_id')
+    def _onchange_leave_dates(self):
+        if self.date_from and self.date_to:
+            days = self.request_date_to - self.request_date_from
+            saturdays = pd.date_range(start=self.request_date_from, end=self.request_date_to,
+                                      freq='W-SAT').strftime(
+                '%m/%d/%Y').tolist()
+            sundays = pd.date_range(start=self.request_date_from, end=self.request_date_to,
+                                    freq='W-SUN').strftime(
+                '%m/%d/%Y').tolist()
+            self.number_of_days =  days.days - (len(saturdays) + len(sundays))
+        else:
+            self.number_of_days = 0
     
 
 
