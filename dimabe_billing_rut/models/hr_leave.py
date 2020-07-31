b998c5d655993f9607fdf05559da7785822490cb
dimabe-odoo <desarrolloti@dimabe.cl>
2020-07-31 10:02:42 -0400

    verify compute_number_of_day and modify action_draft

--- dimabe_billing_rut/models/hr_leave.py
+++ dimabe_billing_rut/models/hr_leave.py
@@ -8,20 +8,15 @@ class HrLeave (models.Model):
     _inherit = 'hr.leave'
 
     @api.multi
-    def action_draft(self):
-        days =self.request_date_to - self.request_date_from
-        saturdays = pd.date_range(start=self.request_date_from, end=self.request_date_to, freq='W-SAT').strftime('%m/%d/%Y').tolist()
-        sundays = pd.date_range(start=self.request_date_from, end=self.request_date_to, freq='W-SUN').strftime('%m/%d/%Y').tolist()
-
-        raise models.ValidationError(days.days - (len(saturdays) + len(sundays)) )
-
-    # @api.multi
-    # @api.depends('number_of_days')
-    # def _compute_number_of_days_display(self):
-    #     for holiday in self:
-    #         days = holiday.request_date_from - request_date_to
-    #         raise models.ValidationError(days)
-    #         holiday.number_of_days_display = days
+    @api.depends('number_of_days')
+    def _compute_number_of_days_display(self):
+        for holiday in self:
+            days = holiday.request_date_to - holiday.request_date_from
+            saturdays = pd.date_range(start=holiday.request_date_from, end=holiday.request_date_to, freq='W-SAT').strftime(
+                '%m/%d/%Y').tolist()
+            sundays = pd.date_range(start=holiday.request_date_from, end=holiday.request_date_to, freq='W-SUN').strftime(
+                '%m/%d/%Y').tolist()
+            holiday.number_of_days_display = days.days - (len(saturdays) + len(sundays))
     
 
 
