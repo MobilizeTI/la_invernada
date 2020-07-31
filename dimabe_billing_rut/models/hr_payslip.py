155bc995fcf0d0658767ac0a5e43ff254a6b9693
dimabe-odoo <desarrolloti@dimabe.cl>
2020-07-31 10:14:38 -0400

    verify compute_number_of_day

--- dimabe_billing_rut/models/hr_payslip.py
+++ dimabe_billing_rut/models/hr_payslip.py
@@ -1,7 +1,8 @@
-from odoo import models,api,fields
+from odoo import models, api, fields
 from dateutil.relativedelta import *
 import pandas as pd
 from odoo.addons import decimal_precision as dp
+
 
 class HrPayslip(models.Model):
     _inherit = 'hr.payslip'
@@ -10,5 +11,6 @@ class HrPayslip(models.Model):
     def test(self):
         for item in self:
             employee_id = item.contract_id.employee_id
-            leaves = self.env['hr.leave'].search([('employee_id','=',employee_id.id)]).mapped('number_of_days_display')
+            leaves = self.env['hr.leave'].search(
+                [('employee_id', '=', employee_id.id), ('state', '=', 'validate')]).mapped('number_of_days')
             raise models.ValidationError(sum(leaves))
