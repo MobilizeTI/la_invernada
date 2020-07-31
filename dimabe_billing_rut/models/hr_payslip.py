f02da5b200cc49202be19d07917cff18e39fffae
dimabe-odoo <desarrolloti@dimabe.cl>
2020-07-31 11:49:10 -0400

    test

--- dimabe_billing_rut/models/hr_payslip.py
+++ dimabe_billing_rut/models/hr_payslip.py
@@ -15,12 +15,10 @@ class HrPayslip(models.Model):
             leaves = self.env['hr.leave'].search(
                 [('employee_id', '=', employee_id.id), ('state', '=', 'validate')])
             for leave in leaves:
-                res = re.sub(r'[AEIOUÜÁÉÍÓÚ]','',leave.holiday_status_id.name,flags=re.IGNORECASE)
-                res = res.replace(' ','').upper()
-                raise models.ValidationError(res)
-
-
-
-
-
-
+                self.env['hr.payslip.worked_days'].create({
+                    'name': leave.holiday_status_id.name,
+                    'number_of_days': leave.number_of_days,
+                    'code':'',
+                    'contract_id': item.contract_id.id,
+                    'payslip_id': item.id
+                })
