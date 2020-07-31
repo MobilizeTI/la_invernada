7fa83ec10ddf0ac5bb8a305bbfeb6953865a8151
dimabe-odoo <desarrolloti@dimabe.cl>
2020-07-31 18:09:11 -0400

    test

--- dimabe_billing_rut/models/test.py
+++ dimabe_billing_rut/models/test.py
@@ -4,8 +4,9 @@ if (contract.type_id.name == 'Sueldo Empresarial') and (
 elif (contract.type_id.name == 'Sueldo Empresarial'):
     result = contract.wage
 else:
-    if worked_days.filtered(lambda a: a.unpaid == True):
-        result = round(
-            (contract.wage / 30) * (worked_days.WORK100.number_of_days - worked_days.PRM130.number_of_days))
+    if payslip.have_absence:
+        result =  round(
+            (contract.wage / 30) * (worked_days.WORK100.number_of_days - payslip.absence_days))
     else:
-        result = result = round((contract.wage / 30) * (worked_days.WORK100.number_of_days))
+        result = round(
+            (contract.wage / 30) * (worked_days.WORK100.number_of_days))
