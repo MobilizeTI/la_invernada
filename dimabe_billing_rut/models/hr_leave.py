a68a5c4dda1c1b0d8099dbe2b5283a79f0154dc6
dimabe-odoo <desarrolloti@dimabe.cl>
2020-07-31 09:28:58 -0400

    verify compute_number_of_day

--- dimabe_billing_rut/models/hr_leave.py
+++ dimabe_billing_rut/models/hr_leave.py
@@ -4,20 +4,13 @@ from datetime import datetime, time
 class HrLeave (models.Model):
     _inherit = 'hr.leave'
 
-    # def _get_number_of_days(self, date_from, date_to, employee_id):
-    #     """ Returns a float equals to the timedelta between two dates given as string."""
-    #     if employee_id:
-    #         raise models.ValidationError(employee_id)
-    #         employee = self.env['hr.employee'].browse(employee_id)
-    #         return employee.get_work_days_data(date_from, date_to)['days']
-    #
-    #     today_hours = self.env.user.company_id.resource_calendar_id.get_work_hours_count(
-    #         datetime.combine(date_from.date(), time.min),
-    #         datetime.combine(date_from.date(), time.max),
-    #         False)
-    #
-    #     return self.env.user.company_id.resource_calendar_id.get_work_hours_count(date_from, date_to) / (
-    #                 today_hours or HOURS_PER_DAY)
+    @api.multi
+    @api.depends('number_of_days')
+    def _compute_number_of_days_display(self):
+        for holiday in self:
+            days = holiday.request_date_from - request_date_to
+            raise models.ValidationError(days)
+            holiday.number_of_days_display =
     
 
 
