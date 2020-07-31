15405daabc536b191463201f4f72dab0fff9c883
dimabe-odoo <desarrolloti@dimabe.cl>
2020-07-31 09:39:21 -0400

    verify compute_number_of_day and modify action_draft

--- dimabe_billing_rut/models/hr_leave.py
+++ dimabe_billing_rut/models/hr_leave.py
@@ -9,13 +9,13 @@ class HrLeave (models.Model):
         days = holiday.request_date_from - request_date_to
         raise models.ValidationError(days)
 
-    @api.multi
-    @api.depends('number_of_days')
-    def _compute_number_of_days_display(self):
-        for holiday in self:
-            days = holiday.request_date_from - request_date_to
-            raise models.ValidationError(days)
-            holiday.number_of_days_display = days
+    # @api.multi
+    # @api.depends('number_of_days')
+    # def _compute_number_of_days_display(self):
+    #     for holiday in self:
+    #         days = holiday.request_date_from - request_date_to
+    #         raise models.ValidationError(days)
+    #         holiday.number_of_days_display = days
     
 
 
