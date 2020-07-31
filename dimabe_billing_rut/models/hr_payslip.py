b316c0688498f7d1cc4bceb1e4560054a11a4129
dimabe-odoo <desarrolloti@dimabe.cl>
2020-07-31 13:45:17 -0400

    verify compute_number_of_day

--- dimabe_billing_rut/models/hr_payslip.py
+++ dimabe_billing_rut/models/hr_payslip.py
@@ -15,7 +15,7 @@ class HrPayslip(models.Model):
         for item in self:
             if item.input_id:
                 item.write({
-                    'input_line_ids': [4, item.input_id.id]
+                    'input_line_ids': [4, item.input_id]
                 })
 
     @api.multi
