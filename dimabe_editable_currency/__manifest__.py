050c813fe7e1c706f2670006ebab2fb6e60e032e
dimabe-odoo <desarrolloti@dimabe.cl>
2020-08-05 13:21:03 -0400

    test

--- dimabe_editable_currency/__manifest__.py
+++ dimabe_editable_currency/__manifest__.py
@@ -32,6 +32,7 @@
         'views/account_payment.xml',
         'views/templates.xml',
         'views/account_move_line.xml',
+        'views/balance_sheet_clp.xml',
         'views/account_move.xml'
     ],
     # only loaded in demonstration mode
---
+++ dimabe_editable_currency/models/balance_sheet_clp.py
@@ -0,0 +1,25 @@
+from odoo import fields, models, api
+
+
+class ModelName(models.Model):
+    _name = 'balance.sheet.clp'
+    _description = 'Balance de Situacion CLP'
+
+    currency_id = fields.Many2one('res.currency', 'Moneda',
+                                  default=lambda self: self.env['res.currency'].search([('name', '=', 'CLP')]))
+
+    account_id = fields.Many2one('account.account', 'Cuenta')
+
+    balance = fields.Monetary('Balance')
+
+    @api.multi
+    def get_data(self):
+        for item in self:
+            accounts = self.env['account.account'].search([])
+            for ac in accounts:
+                ac_move_line = self.env['account.move.line'].search([('account_id','=',ac.id)])
+                self.env['balance.sheet.clp'].create({
+                    'account_id':ac.id,
+                    'balance': sum(ac_move_line.mapped('debit') - sum(ac_move_line.mapped('credit')))
+                })
+
