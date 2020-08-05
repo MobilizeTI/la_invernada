d2e1991c83b563d9fd75b533bbe2fb1d0709c113
dimabe-odoo <desarrolloti@dimabe.cl>
2020-08-05 16:02:31 -0400

    modify search for balance_sheet_clp

--- dimabe_editable_currency/models/balance_sheet_clp.py
+++ dimabe_editable_currency/models/balance_sheet_clp.py
@@ -17,5 +17,5 @@ class ModelName(models.Model):
         for item in self:
             accounts = self.env['account.account'].search([]).mapped('id')
             for ac in accounts:
-                ac_move_line = self.env['account.move.line'].search([('account_id','=',ac)])
+                ac_move_line = self.env['account.move.line'].search([])
                 raise models.ValidationError(ac_move_line)
