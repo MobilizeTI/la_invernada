1664192aceda7307e15dfc4b3caf786910bfc55b
dimabe-odoo <desarrolloti@dimabe.cl>
2020-08-05 15:43:49 -0400

    test

--- dimabe_editable_currency/models/balance_sheet_clp.py
+++ dimabe_editable_currency/models/balance_sheet_clp.py
@@ -17,7 +17,7 @@ class ModelName(models.Model):
         for item in self:
             accounts = self.env['account.account'].search([])
             for ac in accounts:
-                ac_move_line = self.env['account.move.line'].search([('account_id', '=', ac.id)])
+                ac_move_line = self.env['account.move.line'].search([('account_id', '=', ac)])
                 result = sum(ac_move_line.mapped('debit')) - sum(ac_move_line.mapped('credit'))
                 raise models.ValidationError(ac_move_line)
                 self.env['balance.sheet.clp'].create({
