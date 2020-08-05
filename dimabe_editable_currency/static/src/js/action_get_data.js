413ba37cc743df0ea461dc6cf51844ce7ce34382
dimabe-odoo <desarrolloti@dimabe.cl>
2020-08-05 14:16:42 -0400

    test

---
+++ dimabe_editable_currency/static/src/js/action_get_data.js
@@ -0,0 +1,28 @@
+odoo.define('balance_sheet_clp.sincronize_button', function (require) {
+    "use strict";
+    var core = require('web.core');
+    var ListController = require('web.ListController');
+    var rpc = require('web.rpc');
+    var session = require('web.session');
+    var _t = core._t;
+    ListController.include({
+        renderButtons: function ($node) {
+            this._super.apply(this, arguments);
+            if (this.$buttons) {
+                this.$buttons.find('.oe_action_button').click(this.proxy('action_def'));
+            }
+        },
+        action_def: function () {
+            var self = this
+            var user = session.uid;
+            rpc.query({
+                model: 'balance.sheet.clp',
+                method: 'get_data',
+                args: [[user], {'id': user}],
+            });
+            location.reload();
+            this.trigger_up('reload');
+        }
+    });
+}
+)
