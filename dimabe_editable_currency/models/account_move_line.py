db36e25b53ca47c3a2cbb69d050eb2dd071f4f19
dimabe-odoo <desarrolloti@dimabe.cl>
2020-08-06 14:48:48 -0400

    test

--- dimabe_editable_currency/models/account_move_line.py
+++ dimabe_editable_currency/models/account_move_line.py
@@ -71,10 +71,10 @@ class AccountMoveLine(models.Model):
                         'account_type': ac.user_type_id.id,
                         'balance': debit - credit
                     })
-        raise models.ValidationError('Hola')
+        raise models.ValidationError(self.env.ref('dimabe_editable_currency.balance_sheet_clp_view_tree'))
         return {
             'name': "Balance",
-            'view_type': 'form',
+            'view_type': 'tree',
             'view_mode': 'tree,graph,form,pivot',
             'res_model': 'balance.sheet.clp',
             'view_id': False,
