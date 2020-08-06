a824ac3177c428dc21b5e78eef6cf597ba8108c1
dimabe-odoo <desarrolloti@dimabe.cl>
2020-08-06 11:20:02 -0400

    test

--- dimabe_editable_currency/static/src/js/action_get_data.js
+++ dimabe_editable_currency/static/src/js/action_get_data.js
@@ -5,11 +5,6 @@ odoo.define('balance_sheet_clp.get_data', function (require) {
         var rpc = require('web.rpc');
         var session = require('web.session');
         var _t = core._t;
-        var model_obj = new instance.web.Model('ir.model.data');
-        var view_id = false;
-        model_obj.call('get_object_reference', ['balance.sheet.clp', 'balance_sheet_clp_view_tree']).then(function (result) {
-            view_id = result[1];
-        });
         ListController.include({
             renderButtons: function ($node) {
                 this._super.apply(this, arguments);
@@ -17,27 +12,25 @@ odoo.define('balance_sheet_clp.get_data', function (require) {
                     this.$buttons.find('.oe_action_button').click(this.proxy('action_def'));
                 }
             },
-            receive_invoice: function () {
-            var self = this
-            var user = session.uid;
-            rpc.query({
-                model: 'balance.sheet.clp',
-                method: 'get_data',
-                args: [[user], {'id': user}],
-            });
-                // }).then(function (e) {
-                //     self.do_action({
-                //         name: _t('action'),
-                //         type: 'ir.actions.act_window',
-                //         res_model: 'balance.sheet.clp',
-                //         views: [[false, 'form']],
-                //         view_mode: 'form',
-                //         target: 'new',
-                //     });
-                //     window.location
-                // });
-            },
+            action_def: function () {
+                var self = this
+                var user = session.uid;
+                rpc.query({
+                    model: 'balance.sheet.clp',
+                    method: 'get_data',
+                    args: [[user], {'id': user}],
 
+                }).then(function (e) {
+                    self.do_action({
+                        name: _t('action_balance'),
+                        type: 'ir.actions.act_window',
+                        res_model: 'balance.sheet.clp',
+                        views: [[false, 'form']],
+                        view_mode: 'form',
+                        target: 'new',
+                    })
+                });
+            }
         });
     }
 )
