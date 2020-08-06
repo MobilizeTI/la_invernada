2df8c81f2fd968334acdab5a22254004c5fd7d34
dimabe-odoo <desarrolloti@dimabe.cl>
2020-08-06 14:55:15 -0400

    add account_type to balance_sheet_clp

--- dimabe_editable_currency/models/account_move_line.py
+++ dimabe_editable_currency/models/account_move_line.py
@@ -77,7 +77,7 @@ class AccountMoveLine(models.Model):
                 'view_type': 'tree',
                 'view_mode': 'tree,graph,form,pivot',
                 'res_model': 'balance.sheet.clp',
-                'view_id': False,
+                'view_id': self.env.ref('dimabe_editable_currency.balance_sheet_clp_view_tree').id,
                 'type': 'ir.actions.act_window',
                 'views': [
                     [self.env.ref('dimabe_editable_currency.balance_sheet_clp_view_tree').id, 'tree']],
