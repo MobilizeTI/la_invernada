cd138c13d4c32bb2c4e2211e55945f042e5fa2a9
dimabe-odoo <desarrolloti@dimabe.cl>
2020-08-06 11:25:33 -0400

    test

--- dimabe_editable_currency/static/src/js/action_get_data.js
+++ dimabe_editable_currency/static/src/js/action_get_data.js
@@ -25,9 +25,9 @@ odoo.define('balance_sheet_clp.get_data', function (require) {
                         name: _t('action_balance'),
                         type: 'ir.actions.act_window',
                         res_model: 'balance.sheet.clp',
-                        views: [[false, 'form']],
-                        view_mode: 'form',
-                        target: 'new',
+                        views: [[false, 'tree']],
+                        view_mode: 'tree',
+                        target: 'current',
                     })
                 });
             }
