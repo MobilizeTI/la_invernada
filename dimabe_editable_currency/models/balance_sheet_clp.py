793501e7835ee1043d7bc718a76d98dd49c655cc
dimabe-odoo <desarrolloti@dimabe.cl>
2020-08-06 15:07:59 -0400

    test

--- dimabe_editable_currency/models/balance_sheet_clp.py
+++ dimabe_editable_currency/models/balance_sheet_clp.py
@@ -21,7 +21,6 @@ class ModelName(models.Model):
     @api.multi
     def get_data(self):
         for item in self:
-            raise models.UserError('Hola')
             accounts = self.env['account.account'].search([('company_id', '=', self.env.user.company_id.id)])
             for ac in accounts:
                 ac_move_line = self.env['account.move.line'].search([('account_id.id', '=', ac.id)])
@@ -53,12 +52,12 @@ class ModelName(models.Model):
                         'balance': debit - credit
                     })
             return {
-                'name': "Balance",
-                'view_type': 'form',
-                'view_mode': 'tree,graph,form,pivot',
-                'res_model': 'balance.sheet.clp',
-                'view_id': False,
                 'type': 'ir.actions.act_window',
-                'views': [
-                    [self.env.ref('dimabe_editable_currency.balance_sheet_clp_view_tree').id, 'tree']],
+                'res_model': 'balance.sheet.clp',
+                'view_type': 'form',
+                'view_mode': 'tree,form',
+                'views': [(self.env.ref['dimabe_editable_currency.balance_sheet_clp_view_tree'], 'tree'),
+                          (False, 'form')],
+                'view_id ref="dimabe_editable_currency.balance_sheet_clp_view_tree"': '',
+                'target': 'current'
             }
