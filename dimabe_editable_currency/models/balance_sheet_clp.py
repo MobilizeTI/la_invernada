91ab4bb3ac0caee7e60dd769aedbb0512d169fcf
dimabe-odoo <desarrolloti@dimabe.cl>
2020-08-10 09:19:29 -0400

    add do_action to action_get_date.js

--- dimabe_editable_currency/models/balance_sheet_clp.py
+++ dimabe_editable_currency/models/balance_sheet_clp.py
@@ -26,21 +26,22 @@ class ModelName(models.Model):
         for item in self:
             balances = self.env['balance.sheet.clp'].search([])
             accounts = self.env['account.account'].search([('company_id', '=', self.env.user.company_id.id)])
+            date = datetime.date.today()
+            res = requests.request(
+                'GET',
+                'https://services.dimabe.cl/api/currencies?date={}'.format(date.strftime('%Y-%m-%d')),
+                headers={
+                    'apikey': '790AEC76-9D15-4ABF-9709-E0E3DC45ABBC'
+                }
+            )
             for ac in accounts:
                 if len(balances) < 0:
-                    date = datetime.date.today()
-                    res = requests.request(
-                        'GET',
-                        'https://services.dimabe.cl/api/currencies?date={}'.format(date.strftime('%Y-%m-%d')),
-                        headers={
-                            'apikey': '790AEC76-9D15-4ABF-9709-E0E3DC45ABBC'
-                        }
-                    )
                     ac_move_line = self.env['account.move.line'].search([('account_id', '=', ac.id)])
                     debit = sum(ac_move_line.mapped('debit'))
                     credit = sum(ac_move_line.mapped('credit'))
                     response = json.loads(res.text)
                     if res.status_code == 200:
+                        raise models.UserError(res.status_code)
                         for data in response:
                             if data['currency'] == 'USD':
                                 usd = data['value'].replace(',', '.')
