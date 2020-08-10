b40cdc21783586431c4d039c1a8538d380d91948
dimabe-odoo <desarrolloti@dimabe.cl>
2020-08-10 15:51:52 -0400

    test

--- dimabe_editable_currency/models/balance_sheet_clp.py
+++ dimabe_editable_currency/models/balance_sheet_clp.py
@@ -32,39 +32,28 @@ class ModelName(models.Model):
         for item in self:
             accounts = self.env['account.account'].search([('company_id', '=', self.env.user.company_id.id)])
             date = datetime.date.today()
-            res = requests.request(
-                'GET',
-                'https://services.dimabe.cl/api/currencies?date={}'.format(date.strftime('%Y-%m-%d')),
-                headers={
-                    'apikey': '790AEC76-9D15-4ABF-9709-E0E3DC45ABBC'
-                }
-            )
+
             for ac in accounts:
                 ac_move_line = self.env['account.move.line'].search([('account_id', '=', ac.id)])
                 debit = sum(ac_move_line.mapped('debit'))
                 credit = sum(ac_move_line.mapped('credit'))
-                response = json.loads(res.text)
-                if res.status_code == 200:
-                    for data in response:
-                        if data['currency'] == 'USD':
-                            usd = data['value'].replace(',', '.')
-
-                    tmp = (debit - credit) * float(usd)
-                    balance = self.env['balance.sheet.clp'].search([('account_id', '=', ac.id)])
-                    if balance:
-                        if len(balance) == 2:
+                account_invoice = self.env['account.invoice'].search([('account_id','=',ac.id)])
+                raise models.ValidationError(account_invoice)
+                balance = self.env['balance.sheet.clp'].search([('account_id', '=', ac.id)])
+                if balance:
+                    if len(balance) == 2:
                             balance[-1].unlink()
-                        else:
-                            balance.write({
-                                'balance': tmp
-                            })
                     else:
-                        self.env['balance.sheet.clp'].create({
-                            'account_id': ac.id,
-                            'account_type': ac.user_type_id.id,
-                            'balance': tmp,
-                            'is_balance': True
+                        balance.write({
+                            'balance': tmp
                         })
+                else:
+                    self.env['balance.sheet.clp'].create({
+                        'account_id': ac.id,
+                        'account_type': ac.user_type_id.id,
+                        'balance': tmp,
+                        'is_balance': True
+                    })
 
     @api.multi
     @api.depends('account_id')
