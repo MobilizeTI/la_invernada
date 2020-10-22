40be1c9fe4dc21aeabb24136282caeefc0b19c97
dimabe-odoo <desarrolloti@dimabe.cl>
2020-10-22 10:28:11 -0300

    test

--- dimabe_quality_integration/controllers/stock_picking_controller.py
+++ dimabe_quality_integration/controllers/stock_picking_controller.py
@@ -105,11 +105,11 @@ class StockPickingController(http.Controller):
         data = []
 
         if sale_order:
-            date = request.env['mail.message'].search([('res_id','=',sale_order.picking_ids.mapped('id'))]).filtered(lambda a: a.tracking_value_ids.mapped('new_value_char') == 'Realizado').date            
+            date = request.env['mail.message'].sudo().search([('res_id','=',sale_order.picking_ids.mapped('id'))]).mapped('date')
             data.append({
                 'ContainerNumber':sale_order.picking_ids.mapped('container_number'),
                 'DispatchDate':date,
                 'ClientName':sale_order.partner_id.name,
                 'ClientEmail':sale_order.partner_id.email
             })
-        return data
+        return date
