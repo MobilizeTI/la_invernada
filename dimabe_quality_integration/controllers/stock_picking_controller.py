3ccde2a6f126451099382794d3755ba6e5728031
dimabe-odoo <desarrolloti@dimabe.cl>
2020-10-27 10:00:09 -0300

    test

--- dimabe_quality_integration/controllers/stock_picking_controller.py
+++ dimabe_quality_integration/controllers/stock_picking_controller.py
@@ -19,28 +19,28 @@ class StockPickingController(http.Controller):
                     if res.picking_type_id:
                         if 'recepciones' in str.lower(res.picking_type_id.name):
                             kgs = 0
-                        if res.production_net_weight.is_integer():
-                            kgs = int(res.production_net_weight)
-                        data.append({
-                            'ProducerCode': res.partner_id.id,
-                            'ProducerName': res.partner_id.name,
-                            'VarietyName': res.move_ids_without_package.filtered(lambda a: 'MP' in a.product_id.barcode or 'PSE' in a.product_id.barcode or 'PT' in a.product_id.barcode).product_id.get_variety(),
-                            'LotNumber': res.name,
-                            'DispatchGuideNumber': res.guide_number,
-                            'ReceptionDate': res.scheduled_date or res.write_date,
-                            'ReceptionKgs': kgs if kgs > 0 else res.production_net_weight,
-                            'ContainerType': res.get_canning_move().product_id.display_name,
-                            'ContainerWeightAverage': res.avg_unitary_weight,
-                            'ContainerWeight': res.get_canning_move().product_id.weight,
-                            'Season': res.scheduled_date.year,
-                            'Tare': res.tare_weight,
-                            'Warehouse': res.location_dest_id.name,
-                            'QualityWeight': res.quality_weight,
-                            'ContainerQuantity': res.get_canning_move().quantity_done,
-                            'ArticleCode': res.get_mp_move().product_id.default_code,
-                            'ArticleDescription': res.get_mp_move().product_id.display_name,
-                            'OdooUpdated': res.write_date
-                        })
+                            if res.production_net_weight.is_integer():
+                                kgs = int(res.production_net_weight)
+                            data.append({
+                                'ProducerCode': res.partner_id.id,
+                                'ProducerName': res.partner_id.name,
+                                'VarietyName': res.move_ids_without_package.filtered(lambda a: 'MP' in a.product_id.barcode or 'PSE' in a.product_id.barcode or 'PT' in a.product_id.barcode).product_id.get_variety(),
+                                'LotNumber': res.name,
+                                'DispatchGuideNumber': res.guide_number,
+                                'ReceptionDate': res.scheduled_date or res.write_date,
+                                'ReceptionKgs': kgs if kgs > 0 else res.production_net_weight,
+                                'ContainerType': res.get_canning_move().product_id.display_name,
+                                'ContainerWeightAverage': res.avg_unitary_weight,
+                                'ContainerWeight': res.get_canning_move().product_id.weight,
+                                'Season': res.scheduled_date.year,
+                                'Tare': res.tare_weight,
+                                'Warehouse': res.location_dest_id.name,
+                                'QualityWeight': res.quality_weight,
+                                'ContainerQuantity': res.get_canning_move().quantity_done,
+                                'ArticleCode': res.get_mp_move().product_id.default_code,
+                                'ArticleDescription': res.get_mp_move().product_id.display_name,
+                                'OdooUpdated': res.write_date
+                            })
         return data
 
     @http.route('/api/stock_picking', type='json', methods=['GET'], auth='token', cors='*')
