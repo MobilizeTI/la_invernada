19442fe9d5f5b3e2af818e4d8bd8ca113e432be1
dimabe-odoo <desarrolloti@dimabe.cl>
2020-10-27 09:37:21 -0300

    test

--- dimabe_quality_integration/controllers/stock_picking_controller.py
+++ dimabe_quality_integration/controllers/stock_picking_controller.py
@@ -22,7 +22,7 @@ class StockPickingController(http.Controller):
                     data.append({
                         'ProducerCode': res.partner_id.id,
                         'ProducerName': res.partner_id.name,
-                        'VarietyName': res.move_ids_without_package.filtered(lambda a: 'envase' not in str.lower(a.product_id.categ_id.name) and 'bin' not in str.lower(a.product_id.display_name)).product_id.get_variety(),
+                        'VarietyName': res.move_ids_without_package.filtered(lambda a: 'envase' not in str.lower(a.product_id.categ_id.name) and 'bin' not in str.lower(a.product_id.display_name) and 'planta' not in str.lower(a.product_id.display_name)).product_id.get_variety(),
                         'LotNumber': res.name,
                         'DispatchGuideNumber': res.guide_number,
                         'ReceptionDate': res.scheduled_date or res.write_date,
