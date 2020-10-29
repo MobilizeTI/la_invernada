81a172599845bd013a13e8219625a2a14b01bd53
dimabe-odoo <desarrolloti@dimabe.cl>
2020-10-29 12:20:42 -0300

    test

--- dimabe_billing_rut/models/hr_payslip_xlsx.py
+++ dimabe_billing_rut/models/hr_payslip_xlsx.py
@@ -8,5 +8,6 @@ class HrPaySlipXlsx(models.AbstractModel):
         payslip = self.env['hr.payslip'].search([('state','=','done')])
         for obj in payslip:
             report_name = obj.name
+            # One sheet by partner
+            sheet = workbook.add_worksheet(report_name[:31])
             bold = workbook.add_format({'bold': True})
-            sheet.write(0, 0, obj.name, bold)
