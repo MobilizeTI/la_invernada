477aabcc0afa88acc1c134f9420c836aea310832
dimabe-odoo <desarrolloti@dimabe.cl>
2020-10-29 13:06:42 -0300

    test

--- dimabe_billing_rut/models/hr_payslip_xlsx.py
+++ dimabe_billing_rut/models/hr_payslip_xlsx.py
@@ -1,17 +1,25 @@
 import xlsxwriter
 from odoo import models
 
+
 class HrPaySlipXlsx(models.AbstractModel):
     _name = 'report.dimabe_billing_rut.remunerations_book'
     _inherit = 'report.report_xlsx.abstract'
 
     def generate_xlsx_report(self, workbook, data, partners):
-        payslip = self.env['hr.payslip'].search([('state','=','done')])
+        payslip = self.env['hr.payslip'].search([('state', '=', 'done')])
         report_name = "Libro de Remuneraciones"
         # One sheet by partner
         indicadores_id = payslip.mapped('indicadores_id').mapped('name')
         sheet = workbook.add_worksheet('Libro de Remuneraciones')
-        sheet.write(0,1,"Informe: Libro de Remuneraciones")
+        sheet.write(0, 1, "Informe: Libro de Remuneraciones")
+        merge_format = workbook.add_format({
+            'bold': 1,
+            'border': 1,
+            'align': 'center',
+            'valign': 'vcenter',
+            })
+
         sheet.merge_range("B1:E1")
-        sheet.write(1,1,"Mes a procesar :{}".format(indicadores_id[-1]))
+        sheet.write(1, 1, "Mes a procesar :{}".format(indicadores_id[-1]))
         bold = workbook.add_format({'bold': True})
