9137cefdbadf80a3def8d8d3950e555bfbe004da
dimabe-odoo <desarrolloti@dimabe.cl>
2020-10-29 13:23:22 -0300

    test

--- dimabe_billing_rut/models/hr_payslip_xlsx.py
+++ dimabe_billing_rut/models/hr_payslip_xlsx.py
@@ -7,7 +7,7 @@ class HrPaySlipXlsx(models.AbstractModel):
     _inherit = 'report.report_xlsx.abstract'
 
     def generate_xlsx_report(self, workbook, data, partners):
-        payslip = self.env['hr.payslip'].search([('state', '=', 'done')])
+        payslip = self.env['hr.payslip'].search([])
         report_name = "Libro de Remuneraciones"
         # One sheet by partner
         indicadores_id = payslip.mapped('indicadores_id').mapped('name')
@@ -19,5 +19,10 @@ class HrPaySlipXlsx(models.AbstractModel):
             'valign': 'vcenter',
             })
         sheet.merge_range("B2:E2","Informe: Libro de Remuneraciones",merge_format)
-        sheet.merge_range("B3:E3","Mes a procesar :{}".format(indicadores_id[-1]),merge_format)
+        sheet.merge_range("B3:E3","Mes a procesar : {}".format(indicadores_id[-1]),merge_format)
+        employees = self.env['hr.employee'].search([('address_id','=',423)])
+        row = 8
+        column = 8
+        for employee in employees:
+            sheet.merge_range("B{}:D{}".format(row,column),employee.display_name,merge_format)
         bold = workbook.add_format({'bold': True})
