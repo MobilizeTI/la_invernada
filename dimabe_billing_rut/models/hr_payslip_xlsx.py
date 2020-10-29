c07ff6d5a7df20521ba657cf9a8d30a9681445ed
dimabe-odoo <desarrolloti@dimabe.cl>
2020-10-29 13:09:59 -0300

    test

--- dimabe_billing_rut/models/hr_payslip_xlsx.py
+++ dimabe_billing_rut/models/hr_payslip_xlsx.py
@@ -12,14 +12,12 @@ class HrPaySlipXlsx(models.AbstractModel):
         # One sheet by partner
         indicadores_id = payslip.mapped('indicadores_id').mapped('name')
         sheet = workbook.add_worksheet('Libro de Remuneraciones')
-        sheet.write(0, 1, "Informe: Libro de Remuneraciones")
         merge_format = workbook.add_format({
             'bold': 1,
             'border': 1,
             'align': 'center',
             'valign': 'vcenter',
             })
-
-        sheet.merge_range("B1:E1",merge_format)
+        sheet.merge_range("B1:E1","Informe: Libro de Remuneraciones",merge_format)
         sheet.write(1, 1, "Mes a procesar :{}".format(indicadores_id[-1]))
         bold = workbook.add_format({'bold': True})
