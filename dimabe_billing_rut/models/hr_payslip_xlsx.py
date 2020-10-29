8051bf7269b4a5e85e4e7268db4f33c9b8e20e01
dimabe-odoo <desarrolloti@dimabe.cl>
2020-10-29 12:30:27 -0300

    test

--- dimabe_billing_rut/models/hr_payslip_xlsx.py
+++ dimabe_billing_rut/models/hr_payslip_xlsx.py
@@ -6,7 +6,7 @@ class HrPaySlipXlsx(models.AbstractModel):
 
     def generate_xlsx_report(self, workbook, data, partners):
         payslip = self.env['hr.payslip'].search([('state','=','done')])
-        report_name = obj.name
+        report_name = "Libro de Remuneraciones"
         # One sheet by partner
         sheet = workbook.add_worksheet('Libro de Remuneraciones')
         bold = workbook.add_format({'bold': True})
