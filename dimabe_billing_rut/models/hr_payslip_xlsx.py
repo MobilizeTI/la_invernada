46b4252a421764cae84c38f5b2db5dac75ea9455
dimabe-odoo <desarrolloti@dimabe.cl>
2020-10-29 15:29:03 -0300

    test

--- dimabe_billing_rut/models/hr_payslip_xlsx.py
+++ dimabe_billing_rut/models/hr_payslip_xlsx.py
@@ -7,10 +7,11 @@ class HrPaySlipXlsx(models.AbstractModel):
     _inherit = 'report.report_xlsx.abstract'
 
     def generate_xlsx_report(self, workbook, data, partners):
-        payslip = self.env['hr.payslip'].search([])
+        payslips = self.env['hr.payslip'].search([])
         report_name = "Libro de Remuneraciones"
         # One sheet by partner
-        indicadores_id = payslip.mapped('indicadores_id').mapped('name')
+        indicadores_id = payslip.mapped('indicadores_id')
+        names = indicadores_id.mapped('name')
         sheet = workbook.add_worksheet('Libro de Remuneraciones')
         merge_format = workbook.add_format({
             'bold': 1,
@@ -26,7 +27,7 @@ class HrPaySlipXlsx(models.AbstractModel):
         sheet.merge_range(
             "B2:E2", "Informe: Libro de Remuneraciones", merge_format)
         sheet.merge_range("B3:E3", "Mes a procesar : {}".format(
-            indicadores_id[-1]), merge_format)
+            names[-1]), merge_format)
         employees = self.env['hr.employee'].search([('address_id', '=', 423)])
         headers = ['Nombre', 'Rut', 'Dias Trabajados', 'Sueldo Base', 'Grat. Legal', 'Horas Extras', 'Bonos Imponibles', 'Aguinaldo Fiestas Patrias', 'Aguinaldo Navidad', 'Horas de Descuento', 'Total Imponible', 'Colacion', 'Movilizacion', 'Asig. Familiar', 'Asig. Varias', 'Total No Imponible',
                   'Total Haberes', 'AFP', 'Salud', 'Seg. Cesantia', 'Impto. Unico', 'Otros AFP', 'Anticipos', 'Anticipo Aguinaldo', 'Credito Social', 'Ahorro AFP', 'Ahorro APV', 'Ahorro CCAF', 'Seg. de Vida CCAF', 'Ptmo. Empresa', 'Retencion Judicial', 'Total Descuentos', 'Liquido A Pagar']
@@ -41,8 +42,11 @@ class HrPaySlipXlsx(models.AbstractModel):
             if employee.id == employees[0].id:
                 sheet.merge_range("A"+str(row - 1)+":"+"D"+str(row - 1),'Nombre:',merge_format)
                 sheet.merge_range("E"+str(row - 1)+":"+"F"+str(row - 1),'RUT:',merge_format)
+                sheet.merge_range("G"+str(row - 1)+":"+"H"+str(row - 1),'Sueldo Base:',merge_format)
             sheet.merge_range("A"+str(row)+":"+"D"+str(row),employee.display_name,merge_format_data)
-            sheet.merge_range("A"+str(row)+":"+"D"+str(row),employee.identification_id,merge_format_data)            
+            sheet.merge_range("E"+str(row)+":"+"F"+str(row),employee.identification_id,merge_format_data)
+            payslip =payslips.filtered(lambda a: a.employee_id.id == employee.id and a.indicadores_id.id == indicadores_id[-1].id)
+            sheet.merge_range("E"+str(row)+":"+"F"+str(row),payslip.mapped('line_ids').filtered(lambda a: a.name == 'SUELDO BASE').total,merge_format_data)          
             row += 1
         bold = workbook.add_format({'bold': True})
         
