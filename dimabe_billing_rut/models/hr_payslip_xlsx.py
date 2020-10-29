e899abcf1af0589405844ee2eb483e6ee868fcc6
dimabe-odoo <desarrolloti@dimabe.cl>
2020-10-29 13:39:28 -0300

    test

--- dimabe_billing_rut/models/hr_payslip_xlsx.py
+++ dimabe_billing_rut/models/hr_payslip_xlsx.py
@@ -17,14 +17,26 @@ class HrPaySlipXlsx(models.AbstractModel):
             'border': 1,
             'align': 'center',
             'valign': 'vcenter',
-            })
-        sheet.merge_range("B2:E2","Informe: Libro de Remuneraciones",merge_format)
-        sheet.merge_range("B3:E3","Mes a procesar : {}".format(indicadores_id[-1]),merge_format)
-        employees = self.env['hr.employee'].search([('address_id','=',423)])
+        })
+        sheet.merge_range(
+            "B2:E2", "Informe: Libro de Remuneraciones", merge_format)
+        sheet.merge_range("B3:E3", "Mes a procesar : {}".format(
+            indicadores_id[-1]), merge_format)
+        employees = self.env['hr.employee'].search([('address_id', '=', 423)])
+        headers = ['Nombre', 'Rut', 'Dias Trabajados', 'Sueldo Base', 'Grat. Legal', 'Horas Extras', 'Bonos Imponibles', 'Aguinaldo Fiestas Patrias', 'Aguinaldo Navidad', 'Horas de Descuento', 'Total Imponible', 'Colacion', 'Movilizacion', 'Asig. Familiar', 'Asig. Varias', 'Total No Imponible',
+                  'Total Haberes', 'AFP', 'Salud', 'Seg. Cesantia', 'Impto. Unico', 'Otros AFP', 'Anticipos', 'Anticipo Aguinaldo', 'Credito Social', 'Ahorro AFP', 'Ahorro APV', 'Ahorro CCAF', 'Seg. de Vida CCAF', 'Ptmo. Empresa', 'Retencion Judicial', 'Total Descuentos', 'Liquido A Pagar']
+        
         row = 8
         column = 8
+        column_head = 6
+        for head in headers :
+            sheet.write(column_head,8,head)
+            column_head += 1
         for employee in employees:
-            sheet.merge_range("B{}:D{}".format(row,column),employee.display_name,merge_format)
-            sheet.write(0,0,len(employees))
+            sheet.merge_range("B{}:D{}".format(row, column),
+                              employee.display_name, merge_format)
+            sheet.write(0, 0, len(employees))
+            sheet.
             row += 1
+
         bold = workbook.add_format({'bold': True})
