07974636f68365d4c563769d12d56e1d360b2298
dimabe-odoo <desarrolloti@dimabe.cl>
2020-10-29 12:06:22 -0300

    test

--- dimabe_billing_rut/models/hr_payslip_xlsx.py
+++ dimabe_billing_rut/models/hr_payslip_xlsx.py
@@ -1,9 +1,10 @@
 
-class PartnerXlsx(models.AbstractModel):
+class HrPaySlipXlsx(models.AbstractModel):
     _name = 'report.dimabe_billing_rut.remunerations_book'
     _inherit = 'report.report_xlsx.abstract'
 
-    def generate_xlsx_report(self, workbook, data, partners):
+    def generate_xlsx_report(self, workbook, data):
+        payslip = self.env['hr.payslip'].search([('state','=','done')])
         for obj in partners:
             report_name = obj.name
             # One sheet by partner
