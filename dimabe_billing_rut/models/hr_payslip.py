df00680d88befb580c6edd8630632e04bd108f22
dimabe-odoo <desarrolloti@dimabe.cl>
2020-10-30 14:20:42 -0300

    test

--- dimabe_billing_rut/models/hr_payslip.py
+++ dimabe_billing_rut/models/hr_payslip.py
@@ -115,23 +115,14 @@ class HrPayslip(models.Model):
 
     @api.multi
     def generate_excel_report(self):
-        filename = 'filename.xls'
-        workbook = xlwt.Workbook(encoding="UTF-8")
-        worksheet = workbook.add_sheet('Sheet 1')
-        style = xlwt.easyxf('font: bold True, name Arial;')
-        worksheet.write_merge(0, 1, 0, 3, 'your data that you want to show into excelsheet', style)
-        fp = StringIO()
-        workbook.save(fp)
-        record_id = self.env['wizard.excel.report'].create({'excel_file': base64.encodestring(fp.getvalue()),
-                                                            'file_name': filename}, )
-        fp.close()
-        return {'view_mode': 'form',
-                'res_id': record_id,
-                'res_model': 'wizard.excel.report',
-                'view_type': 'form',
-                'type': 'ir.actions.act_window',
-                'target': 'new',
-                }
+        stream = StringIO()
+        book = xlwt.Workbook(encoding='utf-8')
+        sheet = book.add_sheet(u'Sheet1')
+        sheet.write(1, 1, '2')
+        book.save(stream)
+        raise models.ValidationError(stream.getvalue())
+
+        self.your_file_name = self.name
 
     # def generate_remuneration_book(self):
     #     filename = 'Libro de Remuneraciones.xlsx'
