a9b19d82a8df8e05dfb052ef84758b11796189e0
dimabe-odoo <desarrolloti@dimabe.cl>
2020-11-03 18:01:19 -0300

    test

--- dimabe_billing_rut/models/wizard.py
+++ dimabe_billing_rut/models/wizard.py
@@ -19,8 +19,6 @@ class ExcelWizard(models.TransientModel):
 
     end_date = fields.Datetime(string="Fecha de Termino", default=datetime.datetime.now(), required=True)
 
-    report = fields.Binary()
-
     def print_xlsx(self):
         if self.start_date > self.end_date:
             raise ValidationError('La fecha de inicio no puede ser mayer a la fecha de termino')
@@ -28,36 +26,15 @@ class ExcelWizard(models.TransientModel):
             'start_date': self.start_date,
             'end_date': self.end_date
         }
+        return {
+            'type': 'ir_action_xlsx_download',
+            'data': {'model': 'hr.payslip.xlsx.report.wizard',
+                     'options': json.dumps(data, default=date_utils.json_default),
+                     'output_format': 'xlsx',
+                     'report_name': 'Excel Report'
+                     },
+        }
 
-        output = io.BytesIO()
-
-        workbook = xlsxwriter.Workbook(output, {'in_memory': True})
-
-        sheet = workbook.add_worksheet()
-
-        cell_format = workbook.add_format({'font_size': '12px'})
-
-        head = workbook.add_format({'align': 'center', 'bold': True,'font_size':'20px'})
-
-        txt = workbook.add_format({'font_size': '10px'})
-
-        sheet.merge_range('B2:I3', 'EXCEL REPORT', head)
-
-        sheet.write('B6', 'From:', cell_format)
-
-        sheet.merge_range('C6:D6', data['start_date'],txt)
-
-        sheet.write('F6', 'To:', cell_format)
-
-        sheet.merge_range('G6:H6', data['end_date'],txt)
-
-        workbook.close()
-
-        output.seek(0)
-
-        self.report = output.read()
-
-        output.close()
     def get_xlsx_report(self, data, response):
 
         output = io.BytesIO()
@@ -86,7 +63,6 @@ class ExcelWizard(models.TransientModel):
 
         output.seek(0)
 
-        raise models.ValidationError(output)
         response.stream.write(output.read())
 
         output.close()
