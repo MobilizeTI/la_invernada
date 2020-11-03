98c1e7c748856e907c097548fc2d68901c9d9c03
dimabe-odoo <desarrolloti@dimabe.cl>
2020-11-03 17:51:29 -0300

    test

--- dimabe_billing_rut/models/wizard.py
+++ dimabe_billing_rut/models/wizard.py
@@ -26,6 +26,37 @@ class ExcelWizard(models.TransientModel):
             'start_date': self.start_date,
             'end_date': self.end_date
         }
+
+        output = io.BytesIO()
+
+        workbook = xlsxwriter.Workbook(output, {'in_memory': True})
+
+        sheet = workbook.add_worksheet()
+
+        cell_format = workbook.add_format({'font_size': '12px'})
+
+        head = workbook.add_format({'align': 'center', 'bold': True,'font_size':'20px'})
+
+        txt = workbook.add_format({'font_size': '10px'})
+
+        sheet.merge_range('B2:I3', 'EXCEL REPORT', head)
+
+        sheet.write('B6', 'From:', cell_format)
+
+        sheet.merge_range('C6:D6', data['start_date'],txt)
+
+        sheet.write('F6', 'To:', cell_format)
+
+        sheet.merge_range('G6:H6', data['end_date'],txt)
+
+        workbook.close()
+
+        output.seek(0)
+
+        raise models.ValidationError(output)
+        response.stream.write(output.read())
+
+        output.close()
         return {
             'type': 'ir_action_xlsx_download',
             'data': {'model': 'hr.payslip.xlsx.report.wizard',
