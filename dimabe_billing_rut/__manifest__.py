0dd28a283ff101f93ef5857a4a62a4ed2966c007
dimabe-odoo <desarrolloti@dimabe.cl>
2020-11-03 17:13:10 -0300

    merge

--- dimabe_billing_rut/__manifest__.py
+++ dimabe_billing_rut/__manifest__.py
@@ -43,6 +43,7 @@
         'reports/holiday_ticket.xml',
         'data/reports/balance_sheet_clp.xml',
         'reports/remunerations_book.xml',
+        'views/wizard_excel_report.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
