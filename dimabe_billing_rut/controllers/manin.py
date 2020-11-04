02eab9aac2524a9eb6499443c0b4a26856cc0c11
dimabe-odoo <desarrolloti@dimabe.cl>
2020-11-04 10:00:48 -0300

    test

--- dimabe_billing_rut/controllers/manin.py
+++ dimabe_billing_rut/controllers/manin.py
@@ -12,7 +12,7 @@ class XLSXReportController(http.Controller):
 
     @http.route('/api/xlsx_report', type='json', methods=['GET'], auth='token', cors='*')
 
-    def get_report_xlsx(self, model, options, output_format, report_name, **kw):
+    def get_report_xlsx(self, model, output_format, report_name, **kw):
 
         uid = request.session.uid
 
