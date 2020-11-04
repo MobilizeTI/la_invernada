1df283d28f05202a3d56c39fa2d09d7920671c51
dimabe-odoo <desarrolloti@dimabe.cl>
2020-11-04 13:10:24 -0300

    test

--- dimabe_billing_rut/models/wizard.py
+++ dimabe_billing_rut/models/wizard.py
@@ -1,23 +1,24 @@
-from odoo import api, fields ,models
+from odoo import api, fields, models
 from odoo.tools.misc import xlwt
 import io
 import xlsxwriter
 import base64
 
+
 class WizardHrPaySlip(models.TransientModel):
     _name = "wizard.hr.payslip"
     _description = 'XLSX Report'
 
-    company_id = fields.Many2one('res.company','Compañia')
+    company_id = fields.Many2one('res.company', 'Compañia')
 
-    report = fields.Binary(default =lambda self: self.env['wizard.hr.payslip'].search([])[-1].report)
+    report = fields.Binary(default=lambda self: self.env['wizard.hr.payslip'].search([])[-1].report)
 
     def _get_data(self):
         current_date = fields.Date.today()
 
         domain = []
 
-        domain += [('date_from','>=',current_date,current_date,"<=",self.date_to)]
+        domain += [('date_from', '>=', current_date, current_date, "<=", self.date_to)]
 
         res = self.env['hr.payslip'].search([])
 
@@ -52,16 +53,18 @@ class WizardHrPaySlip(models.TransientModel):
             'valign': 'vcenter',
             'num_format': '$#'
         })
-        sheet_service.merge_range(
+        worksheet.merge_range(
             "B2:E2", "Informe: Libro de Remuneraciones", merge_format_title)
-        sheet_service.merge_range("B3:E3", "Mes a procesar : {}".format(
+        worksheet.merge_range("B3:E3", "Mes a procesar : {}".format(
             'Octubre 2020'), merge_format_title)
-
+        worksheet.merge_range('B4:E4', "Compañia : {}".format(
+            self.company_id.name
+        ), merge_format_title)
 
         workbook.close()
         with open(file_name, "rb") as file:
             file_base64 = base64.b64encode(file.read())
-        self.write({'report': file_base64 })
+        self.write({'report': file_base64})
         return {
             "type": "ir.actions.do_nothing",
         }
