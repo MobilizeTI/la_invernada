bb602d57de94943389fbb1d4c0012fa04ca72383
dimabe-odoo <desarrolloti@dimabe.cl>
2020-11-05 12:29:46 -0300

    test

--- dimabe_billing_rut/models/wizard.py
+++ dimabe_billing_rut/models/wizard.py
@@ -147,6 +147,8 @@ class WizardHrPaySlip(models.TransientModel):
         sheet.merge_range("U" + str(row) + ":" + "V" + str(row),
                           payslip[0].mapped('line_ids').filtered(lambda a: a.name == "BONO DE PERMANENCIA").total,
                           merge_format_number)
+        sheet.merge_range("W"+str(row)+":"+"V"+str(row),
+                          sum(payslip[0].mapped('line_id').filtered(lambda a: 'BONO' in a.name and a.category_id.name == 'Imponible').mapped('total')),merge_format_number)
         if 'Septiembre' in indicadores_id[-1].name or 'Diciembre' in indicadores_id[-1].name:
             sheet_service = self.data_format(sheet, row, merge_format_number, payslip, is_bonus=True)
         else:
