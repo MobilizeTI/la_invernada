2ec36a1d28462d3c0e6db87b17d1077dd86d91d3
dimabe-odoo <desarrolloti@dimabe.cl>
2020-11-12 12:47:52 -0300

    test

--- dimabe_billing_rut/models/custom_parameter.py
+++ dimabe_billing_rut/models/custom_parameter.py
@@ -1,7 +1,7 @@
 from odoo import models, fields , api
 
 class CustomParameter(models.Models):
-    _name = 'custom.paramenter'
+    _name = 'custom.parameter'
 
 
     name  = fields.Char('Nombre')
