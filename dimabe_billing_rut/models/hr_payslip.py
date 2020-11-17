a3a10f2c7e8fdde63432da968ccd6c26e1a811bd
dimabe-odoo <desarrolloti@dimabe.cl>
2020-11-17 17:49:36 -0300

    test

--- dimabe_billing_rut/models/hr_payslip.py
+++ dimabe_billing_rut/models/hr_payslip.py
@@ -119,7 +119,7 @@ class HrPayslip(models.Model):
         super(HrPayslip,self).compute_sheet()
         hr_payslip = self.env['hr.payslip'].search([('employee_id','=',self.employee_id.id)])
         worked_days = hr_payslip.mapped('worked_days_line_ids').filtered(lambda a: a.code == 'WORK100' and a.number_of_days > 0)
-        wages = worked_days.mapped('payslip_id').line_ids.filtered(lambda a : a.code == 'TOTIM').mapped('total')
+        wages = worked_days.mapped('payslip_id').mapped('line_ids').filtered(lambda a : a.code == 'TOTIM').mapped('total')
         raise models.ValidationError(wages)
         self.line_ids.filtered(lambda a : a.code == 'SIS').write({
             'total':1,
