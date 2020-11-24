b5ebb787214b122ba4b2afbff6fe4a63b467fbd8
dimabe-odoo <desarrolloti@dimabe.cl>
2020-11-24 09:15:00 -0300

    add to data onchange field request_date_from_period

--- dimabe_billing_rut/models/hr_leave.py
+++ dimabe_billing_rut/models/hr_leave.py
@@ -15,13 +15,13 @@ class HrLeave (models.Model):
                 days = holiday.request_date_to - holiday.request_date_from
                 holiday.number_of_days_display = days.days  + 1 
 
-    @api.onchange('date_from', 'date_to', 'employee_id','request_unit_half')
+    @api.onchange('date_from', 'date_to', 'employee_id','request_date_from_period')
     def _onchange_leave_dates(self):
         if self.date_from and self.date_to:
             if self.holiday_status_id.id == 22:
                 days = self.request_date_to - self.request_date_from
                 self.number_of_days =  days.days + 1
-        elif self.request_unit_half:
+        elif self.request_date_from_period:
             self.number_of_days = 0.5
         else:
             self.number_of_days = 0
