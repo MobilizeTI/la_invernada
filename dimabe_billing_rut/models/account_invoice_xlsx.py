9bc44590f80a477973bac25b7709d2c6fe388ec3
dimabe-odoo <desarrolloti@dimabe.cl>
2020-11-30 16:37:40 -0300

    test

--- dimabe_billing_rut/models/account_invoice_xlsx.py
+++ dimabe_billing_rut/models/account_invoice_xlsx.py
@@ -1,18 +1,20 @@
-from odoo import fields , models , api
+from odoo import fields, models, api
 import base64
 import csv
 import datetime
 import io
 import logging
 import time
-from datetime import datetime,date
+from datetime import datetime, date
 import xlsxwriter
 from dateutil import relativedelta
+
 
 class AccountInvoiceXlsx(models.Model):
     _name = 'account.invoice.xlsx'
 
-    report_file = fields.Binary("Libro de Compra",default=lambda self: self.env['account.invoice.xlsx'].search([])[-1].report_file)
+    report_file = fields.Binary(
+        "Libro de Compra", default=lambda self: self.env['account.invoice.xlsx'].search([])[-1].report_file)
 
     report_name = fields.Char("Reporte")
 
@@ -28,18 +30,20 @@ class AccountInvoiceXlsx(models.Model):
             file_name = 'temp'
             array_worksheet = []
             today = date.today()
-            companies = self.env['res.company'].search([('add_to_sale_book','=',True)],order='id asc')
-            workbook = xlsxwriter.Workbook(file_name,{'in_memory':True})
+            companies = self.env['res.company'].search(
+                [('add_to_sale_book', '=', True)], order='id asc')
+            workbook = xlsxwriter.Workbook(file_name, {'in_memory': True})
             for com in companies:
                 worksheet = workbook.add_worksheet(com.display_name)
-                array_worksheet.append({'company_name':com.display_name,'company_id':com.id,'worksheet':worksheet})
+                array_worksheet.append(
+                    {'company_name': com.display_name, 'company_id': com.id, 'worksheet': worksheet})
             for wk in array_worksheet:
                 sheet = wk['worksheet']
                 sheet.set_column('F:F', 40)
                 sheet.set_column('L:L', 20)
                 sheet.set_column('A:A', 6)
-                sheet.set_column('C:C',10)
-                sheet.set_row(9,6)
+                sheet.set_column('C:C', 10)
+                sheet.set_row(9, 6)
                 merge_format_string = workbook.add_format({
                     'border': 0,
                     'align': 'center',
@@ -51,26 +55,46 @@ class AccountInvoiceXlsx(models.Model):
                     'align': 'center',
                     'valign': 'vcenter',
                 })
-                company = self.env['res.company'].search([('id','=',wk['company_id'])])
-                region = self.env['region.address'].search([('id','=',1)])
-                sheet.merge_range('A1:C1',wk['company_name'],merge_format_string)
-                sheet.merge_range('A2:C2',company.vat,merge_format_string)
-                sheet.merge_range('A3:C3','{}, Region {}'.format(company.city,region.name.capitalize()),merge_format_string)
+                company = self.env['res.company'].search(
+                    [('id', '=', wk['company_id'])])
+                region = self.env['region.address'].search([('id', '=', 1)])
+                sheet.merge_range(
+                    'A1:C1', wk['company_name'], merge_format_string)
+                sheet.merge_range('A2:C2', company.vat, merge_format_string)
+                sheet.merge_range('A3:C3', '{}, Region {}'.format(
+                    company.city, region.name.capitalize()), merge_format_string)
                 sheet.merge_range('A5:L5', 'Libro de Compras',
                                   merge_format_title)
-                sheet.merge_range('A6:L6', 'Libro de Compras Ordenado Por fecha	',merge_format_string)
-                sheet.write('K7','Fecha:',merge_format_string)
-                sheet.write('L7',today.strftime("%d-%m-%Y"),merge_format_string)
+                sheet.merge_range(
+                    'A6:L6', 'Libro de Compras Ordenado Por fecha	', merge_format_string)
+                sheet.write('K7', 'Fecha:', merge_format_string)
+                sheet.write('L7', today.strftime(
+                    "%d-%m-%Y"), merge_format_string)
 
-                sheet.merge_range('A8:L8','Desde : {} Hasta : {}'.format(self.from_date.strftime("%d/%m/%Y"),self.to_date.strftime("%d/%m/%Y")),merge_format_string)
-                sheet.merge_range('A9:L9','Moneda : Peso Chileno',merge_format_string)
-                sheet.write('A11', 'Cod.SII', merge_format_title)
-                sheet.write('B11','Folio',merge_format_title)
-                sheet.write('C11','Cor.Interno',merge_format_title)
+                sheet.merge_range('A8:L8', 'Desde : {} Hasta : {}'.format(self.from_date.strftime(
+                    "%d/%m/%Y"), self.to_date.strftime("%d/%m/%Y")), merge_format_string)
+                sheet.merge_range(
+                    'A9:L9', 'Moneda : Peso Chileno', merge_format_string)
+                sheet = self.set_title(sheet,merge_format_title)
             workbook.close()
             with open(file_name, "rb") as file:
                 file_base64 = base64.b64encode(file.read())
-            self.write({'report_file': file_base64, 'report_name': 'Libro de Ventas'})
+            self.write({'report_file': file_base64,
+                        'report_name': 'Libro de Ventas'})
             return {
                 "type": "ir.actions.do_nothing",
             }
+
+    def set_title(self, sheet, format):
+        sheet.write('A11', 'Cod.SII', format)
+        sheet.write('B11', 'Folio', format)
+        sheet.write('C11', 'Cor.Interno', format)
+        sheet.write('D11', 'Fecha', format)
+        sheet.write('E11', 'RUT', format)
+        sheet.write('F11', 'Nombre de Proevedor', format)
+        sheet.write('H11', 'EXENTO', format)
+        sheet.write('I11', 'NETO', format)
+        sheet.write('J11', 'iIVA', format)
+        sheet.write('K11', 'IVA NO RECUPERABLE', format)
+        sheet.write('L11', 'Total', format)
+        return sheet
