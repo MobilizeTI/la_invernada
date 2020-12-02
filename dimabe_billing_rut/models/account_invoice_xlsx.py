bf68a0ace2f340e24129a92cfb8d52a5ebf25d31
dimabe-odoo <desarrolloti@dimabe.cl>
2020-12-02 09:08:25 -0300

    test

--- dimabe_billing_rut/models/account_invoice_xlsx.py
+++ dimabe_billing_rut/models/account_invoice_xlsx.py
@@ -193,10 +193,7 @@ class AccountInvoiceXlsx(models.Model):
                 row = 13
                 
                 for inv in invoice:
-                    if inv.reference.is_numberic():
-                        sheet.write('B{}'.format(str(row)), inv.reference, merge_format_number)
-                    else:
-                        sheet.write('B{}'.format(str(row)), inv.reference, merge_format_string)
+                    sheet.write('B{}'.format(str(row)),inv.reference,merge_format_string)
                     sheet.write('C{}'.format(str(row)),inv.number,merge_format_string)
                     sheet.write('D{}'.format(str(row)),inv.date_invoice.strftime("%d/%m/%Y"),merge_format_string)
                     rut = inv.partner_id.invoice_rut
