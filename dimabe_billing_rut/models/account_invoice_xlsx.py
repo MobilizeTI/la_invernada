774a7c6c5c8cbce6e5a5bba05b8641d3d5ab5a3c
dimabe-odoo <desarrolloti@dimabe.cl>
2020-12-02 15:57:37 -0300

    test

--- dimabe_billing_rut/models/account_invoice_xlsx.py
+++ dimabe_billing_rut/models/account_invoice_xlsx.py
@@ -1,4 +1,3 @@
-from odoo import fields, models, api
 import base64
 import csv
 import datetime
@@ -6,8 +5,10 @@ import io
 import logging
 import time
 from datetime import datetime, date
+
 import xlsxwriter
 from dateutil import relativedelta
+from odoo import fields, models, api
 
 
 class AccountInvoiceXlsx(models.Model):
@@ -16,20 +17,21 @@ class AccountInvoiceXlsx(models.Model):
     purchase_file = fields.Binary(
         "Libro de Compra", default=lambda self: self.env['account.invoice.xlsx'].search([])[-1].purchase_file)
 
-    purchase_report_name = fields.Char("Reporte Compra",default=lambda self: self.env['account.invoice.xlsx'].search([])[-1].purchase_report_name)
+    purchase_report_name = fields.Char("Reporte Compra",
+                                       default=lambda self: self.env['account.invoice.xlsx'].search([])[
+                                           -1].purchase_report_name)
 
-    sale_file  = fields.Binary(
-        "Libro de Venta",default=lambda self: self.env['account.invoice.xlsx'].search([])[-1].sale_file)
+    sale_file = fields.Binary(
+        "Libro de Venta", default=lambda self: self.env['account.invoice.xlsx'].search([])[-1].sale_file)
 
-    sale_report_name = fields.Char("Reporte Venta",default=lambda self: self.env['account.invoice.xlsx'].search([])[-1].sale_report_name)
+    sale_report_name = fields.Char("Reporte Venta", default=lambda self: self.env['account.invoice.xlsx'].search([])[
+        -1].sale_report_name)
 
     from_date = fields.Date('Desde')
 
     to_date = fields.Date('Hasta')
 
     both = fields.Boolean("Ambas")
-
-    
 
     @api.multi
     def generate_sale_book(self):
@@ -38,7 +40,7 @@ class AccountInvoiceXlsx(models.Model):
             array_worksheet = []
             today = date.today()
             companies = self.env['res.company'].search(
-                [('id','=',self.env.user.company_id.id)], order='id asc')
+                [('id', '=', self.env.user.company_id.id)], order='id asc')
             workbook = xlsxwriter.Workbook(file_name, {'in_memory': True})
             for com in companies:
                 worksheet = workbook.add_worksheet(com.display_name)
@@ -60,16 +62,16 @@ class AccountInvoiceXlsx(models.Model):
                 })
                 merge_format_title = workbook.add_format({
                     'border': 1,
-                    'bold':1,
-                    'align':'center',
-                    'valign':'vcenter'
+                    'bold': 1,
+                    'align': 'center',
+                    'valign': 'vcenter'
                 })
                 merge_format_total = workbook.add_format({
                     'border': 1,
-                    'bold':1,
-                    'align':'center',
-                    'valign':'vcenter',
-                    'num_format':'0,000'
+                    'bold': 1,
+                    'align': 'center',
+                    'valign': 'vcenter',
+                    'num_format': '0,000'
                 })
                 company = self.env['res.company'].search(
                     [('id', '=', wk['company_id'])])
@@ -93,30 +95,33 @@ class AccountInvoiceXlsx(models.Model):
                     'A9:L9', 'Moneda : Peso Chileno', merge_format_string)
                 sheet = self.set_title(sheet, merge_format_title)
 
-                invoice = self.env['account.invoice'].search([('type','=','out_invoice'),('state','=','paid'),('date_invoice','>',self.from_date),('date_invoice','<',self.to_date)])
+                invoice = self.env['account.invoice'].search(
+                    [('type', '=', 'out_invoice'), ('state', '=', 'paid'), ('date_invoice', '>', self.from_date),
+                     ('date_invoice', '<', self.to_date)])
                 row = 13
-                
+
                 for inv in invoice:
-                    sheet.write('B{}'.format(str(row)),inv.reference,merge_format_string)
-                    sheet.write('C{}'.format(str(row)),inv.number,merge_format_string)
-                    sheet.write('D{}'.format(str(row)),inv.date_invoice.strftime("%d/%m/%Y"),merge_format_string)
+                    sheet.write('B{}'.format(str(row)), inv.reference, merge_format_string)
+                    sheet.write('C{}'.format(str(row)), inv.number, merge_format_string)
+                    sheet.write('D{}'.format(str(row)), inv.date_invoice.strftime("%d/%m/%Y"), merge_format_string)
                     rut = inv.partner_id.invoice_rut
                     if not rut:
                         rut = ''
-                    sheet.write('E{}'.format(str(row)),rut,merge_format_string)
-                    sheet.write('F{}'.format(str(row)),inv.partner_id.display_name,merge_format_string)
-                    sheet.write('H{}'.format(str(row)),round(inv.amount_untaxed_invoice_signed),merge_format_number)
-                    sheet.write('I{}'.format(str(row)),round(inv.amount_total_signed),merge_format_number)
-                    sheet.write('J{}'.format(str(row)),round(inv.amount_tax_signed),merge_format_number)
-                    sheet.write_formula('L{}'.format(str(row)),'=SUM(H{}:J{})'.format(str(row),str(row)),merge_format_number)
+                    sheet.write('E{}'.format(str(row)), rut, merge_format_string)
+                    sheet.write('F{}'.format(str(row)), inv.partner_id.display_name, merge_format_string)
+                    sheet.write('H{}'.format(str(row)), round(inv.amount_untaxed_invoice_signed), merge_format_number)
+                    sheet.write('I{}'.format(str(row)), round(inv.amount_total_signed), merge_format_number)
+                    sheet.write('J{}'.format(str(row)), round(inv.amount_tax_signed), merge_format_number)
+                    sheet.write_formula('L{}'.format(str(row)), '=SUM(H{}:J{})'.format(str(row), str(row)),
+                                        merge_format_number)
                     row += 1
-                total = row +1
-                sheet.write('G{}'.format(str(total)),str(len(invoice)),merge_format_total)
-                sheet.write_formula('H{}'.format(str(total)),'=SUM(H13:H{})'.format(row),merge_format_total)
-                sheet.write_formula('I{}'.format(str(total)),'=SUM(I13:I{})'.format(row),merge_format_total)
-                sheet.write_formula('J{}'.format(str(total)),'=SUM(J13:J{})'.format(row),merge_format_total)
-                sheet.write_formula('K{}'.format(str(total)),'=SUM(K13:K{})'.format(row),merge_format_total)
-                sheet.write_formula('L{}'.format(str(total)),'=SUM(L13:L{})'.format(row),merge_format_total)
+                total = row + 1
+                sheet.write('G{}'.format(str(total)), str(len(invoice)), merge_format_total)
+                sheet.write_formula('H{}'.format(str(total)), '=SUM(H13:H{})'.format(row), merge_format_total)
+                sheet.write_formula('I{}'.format(str(total)), '=SUM(I13:I{})'.format(row), merge_format_total)
+                sheet.write_formula('J{}'.format(str(total)), '=SUM(J13:J{})'.format(row), merge_format_total)
+                sheet.write_formula('K{}'.format(str(total)), '=SUM(K13:K{})'.format(row), merge_format_total)
+                sheet.write_formula('L{}'.format(str(total)), '=SUM(L13:L{})'.format(row), merge_format_total)
             workbook.close()
             with open(file_name, "rb") as file:
                 file_base64 = base64.b64encode(file.read())
@@ -126,7 +131,6 @@ class AccountInvoiceXlsx(models.Model):
                 "type": "ir.actions.do_nothing",
             }
 
-    
     @api.multi
     def generate_purchase_book(self):
         for item in self:
@@ -134,7 +138,7 @@ class AccountInvoiceXlsx(models.Model):
             array_worksheet = []
             today = date.today()
             companies = self.env['res.company'].search(
-                [('id','=',self.env.user.company_id.id)], order='id asc')
+                [('id', '=', self.env.user.company_id.id)], order='id asc')
             workbook = xlsxwriter.Workbook(file_name, {'in_memory': True})
             for com in companies:
                 worksheet = workbook.add_worksheet(com.display_name)
@@ -156,16 +160,16 @@ class AccountInvoiceXlsx(models.Model):
                 })
                 merge_format_title = workbook.add_format({
                     'border': 1,
-                    'bold':1,
-                    'align':'center',
-                    'valign':'vcenter'
+                    'bold': 1,
+                    'align': 'center',
+                    'valign': 'vcenter'
                 })
                 merge_format_total = workbook.add_format({
                     'border': 1,
-                    'bold':1,
-                    'align':'center',
-                    'valign':'vcenter',
-                    'num_format':'0,000'
+                    'bold': 1,
+                    'align': 'center',
+                    'valign': 'vcenter',
+                    'num_format': '0,000'
                 })
                 company = self.env['res.company'].search(
                     [('id', '=', wk['company_id'])])
@@ -189,36 +193,76 @@ class AccountInvoiceXlsx(models.Model):
                     'A9:L9', 'Moneda : Peso Chileno', merge_format_string)
                 sheet = self.set_title(sheet, merge_format_title)
 
-                invoice = self.env['account.invoice'].search([('company_id.id','=',company.id),('type','=','in_invoice'),('state','=','paid'),('date_invoice','>',self.from_date),('date_invoice','<',self.to_date)])
-                row = 13
-                
+                invoice = self.env['account.invoice'].search(
+                    [('company_id.id', '=', company.id), ('type', '=', 'in_invoice'), ('state', '=', 'paid'),
+                     ('date_invoice', '>', self.from_date), ('date_invoice', '<', self.to_date)])
+                row = 14
+                sheet.merge_range('A13:F13', 'Factura de compra electronica. (FACTURA COMPRA ELECTRONICA)',
+                                  merge_format_string)
                 for inv in invoice:
-                    sheet.write('B{}'.format(str(row)),inv.reference,merge_format_string)
-                    sheet.write('C{}'.format(str(row)),inv.number,merge_format_string)
-                    sheet.write('D{}'.format(str(row)),inv.date_invoice.strftime("%d/%m/%Y"),merge_format_string)
+                    sheet.write('B{}'.format(str(row)), inv.reference, merge_format_string)
+                    sheet.write('C{}'.format(str(row)), inv.number, merge_format_string)
+                    sheet.write('D{}'.format(str(row)), inv.date_invoice.strftime("%d/%m/%Y"), merge_format_string)
                     rut = inv.partner_id.invoice_rut
                     if not rut:
-                         rut = ''
-                    sheet.write('E{}'.format(str(row)),rut,merge_format_string)
-                    sheet.write('F{}'.format(str(row)),inv.partner_id.display_name,merge_format_string)
-                    sheet.write('H{}'.format(str(row)),round(inv.amount_untaxed_invoice_signed),merge_format_number)
-                    sheet.write('I{}'.format(str(row)),round(inv.amount_total_signed),merge_format_number)
-                    days = self.diff_dates(inv.date_invoice,today)
+                        rut = ''
+                    sheet.write('E{}'.format(str(row)), rut, merge_format_string)
+                    sheet.write('F{}'.format(str(row)), inv.partner_id.display_name, merge_format_string)
+                    sheet.write('H{}'.format(str(row)), round(inv.amount_untaxed_invoice_signed), merge_format_number)
+                    sheet.write('I{}'.format(str(row)), round(inv.amount_total_signed), merge_format_number)
+                    days = self.diff_dates(inv.date_invoice, today)
                     if days > 90:
                         sheet.write('K{}'.format(str(row)), round(inv.amount_tax), merge_format_number)
                         sheet.write('J{}'.format(str(row)), '0', merge_format_number)
                     else:
                         sheet.write('K{}'.format(str(row)), '0', merge_format_number)
                         sheet.write('J{}'.format(str(row)), round(inv.amount_tax), merge_format_number)
-                    sheet.write_formula('L{}'.format(str(row)),'=SUM(H{}:J{})'.format(str(row),str(row)),merge_format_number)
+                    sheet.write_formula('L{}'.format(str(row)), '=SUM(H{}:J{})'.format(str(row), str(row)),
+                                        merge_format_number)
                     row += 1
-                total = row +1
-                sheet.write('G{}'.format(str(total)),str(len(invoice)),merge_format_total)
-                sheet.write_formula('H{}'.format(str(total)),'=SUM(H13:H{})'.format(row),merge_format_total)
-                sheet.write_formula('I{}'.format(str(total)),'=SUM(I13:I{})'.format(row),merge_format_total)
-                sheet.write_formula('J{}'.format(str(total)),'=SUM(J13:J{})'.format(row),merge_format_total)
-                sheet.write_formula('K{}'.format(str(total)),'=SUM(K13:K{})'.format(row),merge_format_total)
-                sheet.write_formula('L{}'.format(str(total)),'=SUM(L13:L{})'.format(row),merge_format_total)
+                total = row + 1
+                sheet.merge_range('A{}:F{}'.format((row + 1), (row + 1)),
+                                  'Total Factura de compra electronica. (FACTURA COMPRA ELECTRONICA)',
+                                  merge_format_title)
+                sheet.write('G{}'.format(str(total)), str(len(invoice)), merge_format_total)
+                sheet.write_formula('H{}'.format(str(total)), '=SUM(H{}:H{})'.format((len(invoice) - row), row),
+                                    merge_format_total)
+                sheet.write_formula('I{}'.format(str(total)), '=SUM(I{}:I{})'.format((len(invoice) - row), row),
+                                    merge_format_total)
+                sheet.write_formula('J{}'.format(str(total)), '=SUM(J{}:J{})'.format((len(invoice) - row), row),
+                                    merge_format_total)
+                sheet.write_formula('K{}'.format(str(total)), '=SUM(K{}:K{})'.format((len(invoice) - row), row),
+                                    merge_format_total)
+                sheet.write_formula('L{}'.format(str(total)), '=SUM(L{}:L{})'.format((len(invoice) - row), row),
+                                    merge_format_total)
+                invoice_refund = self.env['account.invoice'].search(
+                    [('company_id.id', '=', company.id), ('type', '=', 'in_refund'), ('state', '=', 'paid'),
+                     ('date_invoice', '>', self.from_date), ('date_invoice', '<', self.to_date)])
+                row += 1
+                sheet.merge_range('A{}:F{}'.format(row, row),
+                                  'Factura de compra electronica. (FACTURA COMPRA ELECTRONICA)',
+                                  merge_format_string)
+                for ref in invoice_refund:
+                    sheet.write('B{}'.format(str(row)), ref.reference, merge_format_string)
+                    sheet.write('C{}'.format(str(row)), ref.number, merge_format_string)
+                    sheet.write('D{}'.format(str(row)), ref.date_invoice.strftime("%d/%m/%Y"), merge_format_string)
+                    rut = ref.partner_id.invoice_rut
+                    if not rut:
+                        rut = ''
+                    sheet.write('E{}'.format(str(row)), ref, merge_format_string)
+                    sheet.write('F{}'.format(str(row)), ref.partner_id.display_name, merge_format_string)
+                    sheet.write('H{}'.format(str(row)), round(ref.amount_untaxed_invoice_signed), merge_format_number)
+                    sheet.write('I{}'.format(str(row)), round(ref.amount_total_signed), merge_format_number)
+                    days = self.diff_dates(ref.date_invoice, today)
+                    if days > 90:
+                        sheet.write('K{}'.format(str(row)), round(ref.amount_tax), merge_format_number)
+                        sheet.write('J{}'.format(str(row)), '0', merge_format_number)
+                    else:
+                        sheet.write('K{}'.format(str(row)), '0', merge_format_number)
+                        sheet.write('J{}'.format(str(row)), round(ref.amount_tax), merge_format_number)
+                    sheet.write_formula('L{}'.format(str(row)), '=SUM(H{}:J{})'.format(str(row), str(row)),
+                                        merge_format_number)
+                    row += 1
             workbook.close()
             with open(file_name, "rb") as file:
                 file_base64 = base64.b64encode(file.read())
@@ -235,7 +279,7 @@ class AccountInvoiceXlsx(models.Model):
         sheet.write('D11', 'Fecha', format)
         sheet.write('E11', 'RUT', format)
         sheet.write('F11', 'Nombre de Proevedor', format)
-        sheet.write('G11',' ',format)
+        sheet.write('G11', ' ', format)
         sheet.write('H11', 'EXENTO', format)
         sheet.write('I11', 'NETO', format)
         sheet.write('J11', 'IVA', format)
@@ -245,21 +289,21 @@ class AccountInvoiceXlsx(models.Model):
 
     def set_size(self, sheet):
         sheet.set_column('F:F', 40)
-        sheet.set_column('B:B',10)
-        sheet.set_column('H:H',11)
-        sheet.set_column('J:J',11)
-        sheet.set_column('I:I',10.44)
+        sheet.set_column('B:B', 10)
+        sheet.set_column('H:H', 11)
+        sheet.set_column('J:J', 11)
+        sheet.set_column('I:I', 10.44)
         sheet.set_column('L:L', 20)
         sheet.set_column('A:A', 6)
-        sheet.set_column('D:D',10)
-        sheet.set_column('E:E',12)
+        sheet.set_column('D:D', 10)
+        sheet.set_column('E:E', 12)
         sheet.set_column('C:C', 11)
-        sheet.set_column('K:K',20)
-        sheet.set_column('G:G',2.89)
-        sheet.set_column('C:C',15.89)
-        sheet.set_column('F:F',45)
+        sheet.set_column('K:K', 20)
+        sheet.set_column('G:G', 2.89)
+        sheet.set_column('C:C', 15.89)
+        sheet.set_column('F:F', 45)
         sheet.set_row(9, 6)
         return sheet
 
-    def diff_dates(self,date1, date2):
+    def diff_dates(self, date1, date2):
         return abs(date2 - date1).days
