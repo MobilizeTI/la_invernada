a17791c43c81638860df7fd1ff4e93a9a1037b14
dimabe-odoo <desarrolloti@dimabe.cl>
2020-12-04 09:07:39 -0300

    test

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -353,20 +353,16 @@ class MrpWorkorder(models.Model):
         for check in self.check_ids:
             if check.id == self.current_quality_check_id.id:
                 check = self.current_quality_check_id
-                if not check.component_is_byproduct:
-                    check.qty_done = 0
-                    self.action_skip()
-                else:
-                    if not check.lot_id:
-                        lot_tmp = self.env['stock.production.lot'].create({
-                            'name': self.env['ir.sequence'].next_by_code('mrp.workorder'),
-                            'product_id': check.component_id.id,
-                            'is_prd_lot': True
-                        })
-                        check.lot_id = lot_tmp.id
-                        check.qty_done = self.component_remaining_qty
-                        if check.quality_state == 'none' and check.qty_done > 0:
-                            self.action_next()
+                if check.component_is_byproduct:
+                    lot_tmp = self.env['stock.production.lot'].create({
+                        'name': self.env['ir.sequence'].next_by_code('mrp.workorder'),
+                        'product_id': check.component_id.id,
+                        'is_prd_lot': True
+                    })
+                    check.lot_id = lot_tmp.id
+                    check.qty_done = self.component_remaining_qty
+                    if check.quality_state == 'none' and check.qty_done > 0:
+                        self.action_next()
             self.action_first_skipped_step()
         return super(MrpWorkorder, self).open_tablet_view()
 
