7174a26d267a64eb879815d3453bd499b053507e
dimabe-odoo <desarrolloti@dimabe.cl>
2020-12-18 12:39:41 -0300

    test

--- dimabe_editable_currency/models/account_invoice_line.py
+++ dimabe_editable_currency/models/account_invoice_line.py
@@ -2,13 +2,18 @@ from odoo import models, fields, api
 
 class AccountInvoiceLine(models.Model):
 
-    _inherit = 'account.invoice.line'
+    _inherit = 'account.invoic.line'
 
     exempt = fields.Selection([
             ("1", 'No afecto o exento de IVA'),
             ("2", 'Producto o servicio no es facturable'),
             ("3", 'Garantía de depósito por envases, autorizados por Resolución especial'),
-            ("4", 'Item No Venta'),
-            ("5", 'Item a rebajar'),
-            ("6", 'Producto/Servicio no facturable negativo'),
-            ])
+            ("4", 'Ítem No Venta'),
+            ("5", 'Ítem a rebajar'),
+            ("6", 'Producto o servicio no facturable negativo'),
+            ], 'Tipo Exento', default='1')
+
+    @api.onchange('invoice_line_tax_ids')
+    def valid_exempt(self):
+        if len(self.invoice_line_tax_ids) == 0:
+            exempt = 1
