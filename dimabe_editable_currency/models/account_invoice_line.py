8e7724365146836db278acc9d9d826863669b190
dimabe-odoo <desarrolloti@dimabe.cl>
2020-12-18 12:23:29 -0300

    test

--- dimabe_editable_currency/models/account_invoice_line.py
+++ dimabe_editable_currency/models/account_invoice_line.py
@@ -8,7 +8,7 @@ class AccountInvoiceLine(models.Model):
             ("1", 'No afecto o exento de IVA'),
             ("2", 'Producto o servicio no es facturable'),
             ("3", 'Garantía de depósito por envases, autorizados por Resolución especial'),
-            ("4", 'Ítem No Venta'),
-            ("5", 'Ítem a rebajar'),
-            ("6", 'Producto o servicio no facturable negativo'),
+            ("4", 'Item No Venta'),
+            ("5", 'Item a rebajar'),
+            ("6", 'Producto/Servicio no facturable negativo'),
             ])
