45c113749d3f95c4aad9ae6d1b8bb37cd4a4e5f6
dimabe-odoo <desarrolloti@dimabe.cl>
2020-12-21 17:59:07 -0300

    test

--- dimabe_editable_currency/models/account_invoice.py
+++ dimabe_editable_currency/models/account_invoice.py
@@ -118,7 +118,7 @@ class AccountInvoice(models.Model):
             "expirationDate": self.date_due.strftime("%Y-%m-%d"),
             "dteType": self.dte_type_id.code,
             "transmitter": {
-                "EnterpriseRut": self.env.user.company_id.invoice_rut,
+                "EnterpriseRut": "11111111-1", #self.env.user.company_id.invoice_rut,
                 "EnterpriseActeco": self.company_activity_id.code,
                 "EnterpriseAddressOrigin": self.env.user.company_id.street,
                 "EnterpriseCity": self.env.user.company_id.city,
@@ -127,7 +127,7 @@ class AccountInvoice(models.Model):
                 "EnterpriseTurn": self.company_activity_id.name
             },
             "recipient": {
-                "EnterpriseRut": self.partner_id.invoice_rut,
+                "EnterpriseRut": "22222222-2", #self.partner_id.invoice_rut,
                 "EnterpriseAddressOrigin": self.partner_id.street,
                 "EnterpriseCity": self.partner_id.city,
                 "EnterpriseCommune": self.partner_id.state_id.name,
