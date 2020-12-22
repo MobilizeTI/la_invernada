c0762e58f62de967901e3e10732eacc9e7c4ab2d
dimabe-odoo <desarrolloti@dimabe.cl>
2020-12-22 15:28:53 -0300

    test

--- dimabe_editable_currency/models/account_invoice.py
+++ dimabe_editable_currency/models/account_invoice.py
@@ -127,10 +127,9 @@ class AccountInvoice(models.Model):
                     }
                 )
 
-        raise models.ValidationError(self.observations_ids[0].observations)
         if len(self.observations_ids) > 0:
-            for obs in self.observations_ids:
-                additional.append(obs)
+            for item in self.observations_ids:
+                additional.append(item.observations)
 
         for item in self.invoice_line_ids:
             haveExempt = False
