9f143d16c2752630808ed5a5e503545b175d1b7b
dimabe-odoo <desarrolloti@dimabe.cl>
2020-12-22 15:18:47 -0300

    test

--- dimabe_editable_currency/models/account_invoice.py
+++ dimabe_editable_currency/models/account_invoice.py
@@ -127,7 +127,7 @@ class AccountInvoice(models.Model):
                     }
                 )
 
-        raise models.ValidationError(self.observations)
+        raise models.ValidationError(self.observations_ids.observations)
         if len(self.observations_ids) > 0:
             for obs in self.observations_ids:
                 additional.append(obs)
