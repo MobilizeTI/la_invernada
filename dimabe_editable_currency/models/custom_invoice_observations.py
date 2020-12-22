2956a8c9cd0096c7ac8d79a89842bcf16ec2fc0b
dimabe-odoo <desarrolloti@dimabe.cl>
2020-12-22 10:57:54 -0300

    test

--- dimabe_editable_currency/models/custom_invoice_observations.py
+++ dimabe_editable_currency/models/custom_invoice_observations.py
@@ -3,8 +3,8 @@ from odoo import models, fields, api
 class CustomInvoiceObservations(models.Model):
     _name = 'custom.invoice.observations'
 
-    observations = fields.char(
-        'Observación',
+    observations = fields.Char(
+        string='Observación',
         nullable = True,
         default= None,
         size=140
