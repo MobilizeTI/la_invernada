e1072199c0e3b9e547341328f6359d63da4fe4fc
dimabe-odoo <desarrolloti@dimabe.cl>
2020-12-29 17:15:38 -0300

    Test

--- dimabe_billing_rut/models/stock_picking.py
+++ dimabe_billing_rut/models/stock_picking.py
@@ -288,9 +288,9 @@ class StockPicking(models.Model):
             invoice['additional'] =  additionals    
 
 
-        #r = requests.post(url, json=invoice, headers=headers)
+        r = requests.post(url, json=invoice, headers=headers)
 
-        raise models.ValidationError(json.dumps(invoice))
+        #raise models.ValidationError(json.dumps(invoice))
 
         jr = json.loads(r.text)
 
