b1bda6d6374f004a4b49fc0d01de569ae8945c93
dimabe-odoo <desarrolloti@dimabe.cl>
2020-12-31 10:18:49 -0300

    Test

--- dimabe_billing_rut/models/account_invoice_line.py
+++ dimabe_billing_rut/models/account_invoice_line.py
@@ -15,8 +15,9 @@ class AccountInvoiceLine(models.Model):
             ], 'Tipo Exento', default='7')
 
     # VALIDAR QUE INGRESE ALGO EN EXEPT CUANDO ESTE VACIO O EXENTO EN IMPUESTOS
-    @api.onchange('invoice_line_ids')
+    @api.onchange('invoice_line_tax_ids')
     def valid_exempt(self):
         if len(self.invoice_line_tax_ids) == 0:
+            raise models.Validations('es vacio es dcir debe elegir tipo exento')
             exempt = 1
 
