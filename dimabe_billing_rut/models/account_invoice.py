3f0c17ef6eb7e105bc9d2ba956becfffc2971e64
dimabe-odoo <desarrolloti@dimabe.cl>
2021-01-04 09:16:34 -0300

    Test

--- dimabe_billing_rut/models/account_invoice.py
+++ dimabe_billing_rut/models/account_invoice.py
@@ -254,8 +254,8 @@ class AccountInvoice(models.Model):
 
         for item in self.invoice_line_ids:
             for tax_line in item.invoice_line_tax_ids:
-                if (tax_line.id == 6 or tax_line.id == None) and (item.exempt != "7"):
-                    raise models.ValidationError('El Producto {} no tiene impuesto por ende debe seleccionar el Tipo Exento {} {}'.format(item.name, item.exempt, tax_line.id))
+                if (tax_line.id == 6 or tax_line.id == None) and (item.exempt == "7"):
+                    raise models.ValidationError('El Producto {} no tiene impuesto por ende debe seleccionar el Tipo Exento'.format(item.name))
 
         if len(self.references) > 10:
             raise models.ValidationError('Solo puede generar 20 Referencias')
