d746a992895a62a6194401c05145fcab5d572688
dimabe-odoo <desarrolloti@dimabe.cl>
2021-01-04 09:07:36 -0300

    Test

--- dimabe_billing_rut/models/account_invoice.py
+++ dimabe_billing_rut/models/account_invoice.py
@@ -63,7 +63,7 @@ class AccountInvoice(models.Model):
             ('0','Líneas de Detalle en Montos Brutos'),
             ('2','Líneas de Detalle en Montos Netos')
         ],
-        string="Indicador Monto Neto"
+        string="Indicqador Monto Neto"
         #dte_type_id={'39':[('invisible', False)]}
     )
 
@@ -81,117 +81,9 @@ class AccountInvoice(models.Model):
             for activity in item.partner_id.economic_activities:
                 activities.append(activity.id)
             item.partner_activity_id = activities
-    #eliminar
-    @api.one
-    def send_to_sii_old(self):
-        # PARA COMPLETAR EL DOCUMENTO SE DEBE BASAR EN http://www.sii.cl/factura_electronica/formato_dte.pdf
-        if not self.company_activity_id or not self.partner_activity_id:
-            raise models.ValidationError('Por favor seleccione las actividades de la compañía y del proveedor')
-        if not self.company_id.invoice_rut or not self.partner_id.invoice_rut:
-            raise models.ValidationError('No se encuentra registrado el rut de facturación')
 
-        if not self.dte_type_id:
-            raise models.ValidationError('Por favor seleccione tipo de documento a emitir')
-        if not self.company_activity_id or not self.partner_activity_id:
-            raise models.ValidationError('Debe seleccionar el giro de la compañí y proveedor a utilizar')
 
-        if self.dte_type_id.code is '110' and self.currency_id.name is not 'USD' and not self.exchange_rate:
-            raise models.ValidationError(
-                'Para emitir una factura de exportación la moneda debe ser en USD y debe tener una tasa de cambio')
-
-        dte = {}
-        dte["Encabezado"] = {}
-        dte["Encabezado"]["IdDoc"] = {}
-        # El Portal completa los datos del Emisor
-        dte["Encabezado"]["IdDoc"] = {"TipoDTE": self.dte_type_id.code}
-        # Si es Boleta de debe indicar el tipo de servicio, por defecto de venta de servicios
-        if self.dte_type_id.code in ('39', 39):
-            dte["Encabezado"]["IdDoc"]["IndServicio"] = 3
-
-        if not self.dte_type_id.code in ('39', 39, 110):
-            # Se debe inicar SOLO SI los valores indicados en el documento son con iva incluido
-            dte["Encabezado"]["IdDoc"]["MntBruto"] = 1
-
-        if self.dte_type_id.code is '110':
-            dte["Encabezado"]["OtraMoneda"] = {
-                'TpoMoneda': 'PESO CL',
-                'TpoCambio': self.exchange_rate,
-                'MntExeOtrMnda': round(self.amount_total * self.exchange_rate, 2),
-                'MntTotOtrMnda': round(self.amount_total * self.exchange_rate, 2)
-            }
-            dte["Encabezado"]["Totales"] = {
-                'TpoMoneda': 'DOLAR USA',
-                'MntExe': self.amount_total,
-                'MntTotal': self.amount_total
-            }
-
-        # EL CAMPO RUT DE FACTURACIÓN, debe corresponder al RUT de la Empresa
-        dte["Encabezado"]["Emisor"] = {"RUTEmisor": self.company_id.invoice_rut.replace(".", "")}
-
-        # EL CAMPO VAT o NIF Del Partner, debe corresponder al RUT , si es empresa extranjera debe ser 55555555-5
-        dte["Encabezado"]["Receptor"] = {"RUTRecep": self.partner_id.invoice_rut.replace(".", ""),
-                                         "RznSocRecep": self.partner_id.name,
-                                         "DirRecep": self.partner_id.street + ' ' + self.partner_id.city,
-                                         "CmnaRecep": self.partner_id.city,
-                                         "GiroRecep": self.partner_activity_id.name}
-
-        dte["Encabezado"]["IdDoc"]["TermPagoGlosa"] = self.comment or ''
-        dte["Encabezado"]["IdDoc"]["Folio"] = '0'
-        dte["Encabezado"]["IdDoc"]["FchEmis"] = str(date.today())
-        dte["Detalle"] = []
-        for line in self.invoice_line_ids:
-            # El Portal Calculos los Subtotales
-            ld = {'NmbItem': line.product_id.name,
-                  'DscItem': '',
-                  'QtyItem': round(line.quantity, 6),
-                  'PrcItem': round(line.price_unit, 4)
-                  }
-            if line.product_id.default_code:
-                ld['CdgItem'] = {"TpoCodigo": "INT1",
-                                 "VlrCodigo": line.product_id.default_code}
-            if line.discount:
-                ld['DescuentoPct'] = round(line.discount, 2)
-            dte["Detalle"].append(ld)
-        referencias = []
-        for reference in self.references:
-            ref = {'TpoDocRef': reference.document_type_reference_id.code or 'SET',
-                   'FolioRef': reference.folio_reference,
-                   'FchRef': reference.document_date.__str__(),
-                   'RazonRef': reference.reason}
-            if reference.code_reference:
-                ref['CodRef'] = reference.code_reference
-            referencias.append(ref)
-        if referencias:
-            dte['Referencia'] = referencias
-        # raise models.ValidationError(json.dumps(dte))
-        self.send_dte(json.dumps(dte))
-
-    
-
-    #eliminar
-    def send_dte(self, dte):
-        url = self.company_id.dte_url
-        rut_emisor = self.company_id.invoice_rut.replace(".", "").split("-")[0]
-        hash = self.company_id.dte_hash
-        auth = requests.auth.HTTPBasicAuth(hash, 'X')
-        ssl_check = False
-        # Api para Generar DTE
-        apidte = '/dte/documentos/gendte?getXML=true&getPDF=true&getTED=png'
-        emitir = requests.post(url + '/api' + apidte, dte, auth=auth, verify=ssl_check)
-        if emitir.status_code != 200:
-            raise Exception('Error al Temporal: ' + emitir.json())
-        data = emitir.json()
-        self.dte_folio = data.get('folio', None)
-        self.dte_xml = data.get("xml", None)
-        self.dte_pdf = data.get('pdf', None)
-        self.ted = data.get("ted", None)
-        fecha = data.get("fecha", None)
-        total = data.get("total", None)
-        self.pdf_url = "%s/dte/dte_emitidos/pdf/%s/%s/0/%s/%s/%s" % (
-            url, self.dte_type_id.code, self.dte_folio, rut_emisor, fecha, total)
-
-    #new
-
+ 
     #@api.onchange('type')
     #not worked
     @api.multi
@@ -360,6 +252,8 @@ class AccountInvoice(models.Model):
             if self.currency_id.name != "CLP":
                 raise models.ValidationError('El Tipo {} debe tener moneda CLP'.format(self.dte_type_id.name))
 
+        raise models.ValidationError('{} {}  -  {} {}'.format(item.invoice_line_tax_ids[0],self.invoice_line_ids[0],item.invoice_line_tax_ids[1],self.invoice_line_ids[1]))
+
         for item in self.invoice_line_ids:
             for tax_line in item.invoice_line_tax_ids:
                 if (tax_line.id == 6 or tax_line.id == None) and (item.exempt != "7"):
@@ -422,7 +316,7 @@ class AccountInvoice(models.Model):
                             product_price = item.price_unit  * (1 + tax.amount / 100)
                             amount_subtotal = item.price_subtotal * (1 + tax.amount / 100)
                 
-                netAmount += int(item.price_subtotal)
+                netAmount += int(amount_subtotal)
                 productLines.append(
                     {
                         "LineNumber": str(lineNumber),
