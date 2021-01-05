183aafa63a3accd169d90ce9004556e75f870608
dimabe-odoo <desarrolloti@dimabe.cl>
2021-01-05 12:33:58 -0300

    Test

--- dimabe_billing_rut/models/account_invoice.py
+++ dimabe_billing_rut/models/account_invoice.py
@@ -349,9 +349,7 @@ class AccountInvoice(models.Model):
                     amount_subtotal = item.price_subtotal
                 elif self.dte_type_id.code == "39": #Boleta Elecronica
                     for tax in item.invoice_line_tax_ids:
-                        raise models.ValidationError('taxt_id: {}  price: {}  price iva: {}'.format(tax.id,item.price_unit,product_price))
-
-                        if tax.id == 1 or tax.id == 2: 
+                        if tax.id == 1 or tax.id == 2 or tax.id == 4: 
                             product_price = item.price_unit  * (1 + tax.amount / 100)
                             amount_subtotal = self.roundclp(item.price_subtotal * (1 + tax.amount / 100))
                                 
