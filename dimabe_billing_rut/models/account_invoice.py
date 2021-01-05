312455917c63721b1ce894c097efddab9b7c6c83
dimabe-odoo <desarrolloti@dimabe.cl>
2021-01-05 11:22:57 -0300

    Test

--- dimabe_billing_rut/models/account_invoice.py
+++ dimabe_billing_rut/models/account_invoice.py
@@ -291,7 +291,7 @@ class AccountInvoice(models.Model):
     def roundclp(self, value):
         value_str = str(value)
         list_value = value_str.split('.')
-        raise models.ValidationError(int(list_value[1]))
+        raise models.ValidationError('value: {}  value_str: {}'.format(value, value_str))
         if int(list_value[1]) < 5:
             return floor(value)
         else:
