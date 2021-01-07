91ed1d83f37c9359dae979733b2cf79553e257ff
dimabe-odoo <desarrolloti@dimabe.cl>
2021-01-07 15:22:52 -0300

    Test

--- dimabe_billing_rut/models/__init__.py
+++ dimabe_billing_rut/models/__init__.py
@@ -27,3 +27,4 @@ from . import custom_receiving_country_dte
 from . import custom_sale_method
 from . import custom_type_transport
 from . import custom_export_clause
+from . import custom_package_type
