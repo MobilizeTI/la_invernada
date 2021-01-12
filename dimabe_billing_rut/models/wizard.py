56e2fbdc0156aded0d78d67774d477cb062d7a44
dimabe-odoo <desarrolloti@dimabe.cl>
2021-01-12 17:13:25 -0300

    test

--- dimabe_billing_rut/models/wizard.py
+++ dimabe_billing_rut/models/wizard.py
@@ -74,8 +74,8 @@ class WizardHrPaySlip(models.TransientModel):
             raise models.ValidationError(f'No existen datos del mes de {self.month} {self.years}')
         if indicadores.state != 'done':
             raise models.ValidationError(f'Los indicadores provicionales del mes de {indicadores.name} no se encuentran validados')
-        row = 0
-        col = 0
+        row = 2
+        col = 3
         payslips = self.env['hr.payslip'].sudo().search([('indicadores_id','=',indicadores.id)])
         for pay in payslips:
             rules = pay.struct_id.rule_ids
