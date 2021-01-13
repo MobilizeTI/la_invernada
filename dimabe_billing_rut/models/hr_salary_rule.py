907717d38a3cfae9c0bfad7bb39f74293d443624
dimabe-odoo <desarrolloti@dimabe.cl>
2021-01-13 09:16:46 -0300

    add view

--- dimabe_billing_rut/models/hr_salary_rule.py
+++ dimabe_billing_rut/models/hr_salary_rule.py
@@ -4,4 +4,4 @@ from odoo import models, fields, api
 class HrSalaryRule(models.Model):
     _inherit = 'hr.salary.rule'
 
-    show_in_book = fields.Boolean('Aparece en el libro de remuneraciones')
+    show_in_book = fields.Boolean('Aparece en el libro de remuneraciones', default=True)
