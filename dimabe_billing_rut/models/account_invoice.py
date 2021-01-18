6304a2b8766c794faba3161ad73078cbca3b1ead
dimabe-odoo <desarrolloti@dimabe.cl>
2021-01-18 17:38:00 -0300

    Test

--- dimabe_billing_rut/models/account_invoice.py
+++ dimabe_billing_rut/models/account_invoice.py
@@ -690,7 +690,8 @@ class AccountInvoice(models.Model):
                                 'order_name': self.order_to_add_ids.name,
                                 'quantity_to_invoice': str(item.qty_delivered - item.qty_invoiced),
                                 'dispatch': self.stock_picking_ids.name,
-                                'stock_picking_id': self.stock_picking_ids.id
+                                'stock_picking_id': self.stock_picking_ids.id,
+                                'quantity': item.qty_delivered - item.qty_invoiced
                             })
                         valid = False
         else:
