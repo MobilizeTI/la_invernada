f548fef1ed2ee49a94e2ad50e32c3208b8e3df99
dimabe-odoo <desarrolloti@dimabe.cl>
2021-01-18 13:05:32 -0300

    Test

--- dimabe_billing_rut/models/account_invoice.py
+++ dimabe_billing_rut/models/account_invoice.py
@@ -684,8 +684,9 @@ class AccountInvoice(models.Model):
     def write(self, vals):
         res = super(AccountInvoice, self).write(vals)
         order_list = []
-        raise models.ValidationError('{} {}'.format(self.invoice_line_ids[0],self.invoice_line_ids[1]))
+        raise models.ValidationError('{}'.format(len(self.invoice_line_ids))
         for item in self.invoice_line_ids:
+            
             if item.order_id:
                 order_list.append(item.order_id)
         
