b533fa40a0cb13382b3106460d0c574e125759c4
dimabe-odoo <desarrolloti@dimabe.cl>
2021-01-18 08:49:22 -0300

    Test

---
+++ dimabe_billing_rut/models/custom_orders_in_invoice.py
@@ -0,0 +1,20 @@
+from odoo import models, fields, api
+
+class CustomOrdersInInvoice(models.Model):
+    _name = 'custom.orders.in.invoice'
+
+    order_id = fields.Char(string= 'Pedido', required=True)
+
+    product_id = fields.Char(string= 'Producto', required=True)
+
+    price = fields.Char(string= 'Precio', required=True)
+
+    quantity = fields.Char(string= 'Cantidad', required=True)
+
+    invoice_id = fields.Many2one(
+        'account.invoice',
+        ondelete='cascade',
+        index=True,
+        copy=False,
+        string="Pedidos",
+    )
