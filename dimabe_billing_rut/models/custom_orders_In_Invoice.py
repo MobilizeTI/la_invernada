8a1b8ece31338675dd74f81457b7bcc7a270af57
dimabe-odoo <desarrolloti@dimabe.cl>
2021-01-20 17:30:33 -0300

    Agregar pedidos a Linea factura

--- dimabe_billing_rut/models/custom_orders_In_Invoice.py
+++ dimabe_billing_rut/models/custom_orders_In_Invoice.py
@@ -3,21 +3,21 @@ from odoo import models, fields, api
 class CustomOrdersInInvoice(models.Model):
     _name = 'custom.orders.in.invoice'
 
-    stock_picking_id = fields.Integer(string= 'Despacho Id', required=True)
+    stock_picking_id = fields.Integer(string="Despacho Id", required=True)
 
-    stock_picking_name = fields.Char(string= 'Despacho', required=True)
+    stock_picking_name = fields.Char(string="Despacho", required=True)
 
-    order_id = fields.Integer(string= 'Pedido Id', required=True)
+    order_id = fields.Integer(string="Pedido Id", required=True)
 
-    order_name = fields.Char(string= 'Pedido', required=True)
+    order_name = fields.Char(string="Pedido", required=True)
 
-    product_id = fields.Integer(string= 'Producto Id', required=True)
+    product_id = fields.Integer(string="Producto Id", required=True)
 
-    product_name = fields.Char(string= 'Producto', required=True)
+    product_name = fields.Char(string="Producto", required=True)
 
-    price = fields.Char(string= 'Precio', required=True)
+    price = fields.Char(string="Precio", required=True)
 
-    quantity = fields.Char(string= 'Cantidad', required=True)
+    quantity = fields.Char(string="Cantidad", required=True)
 
     invoice_id = fields.Many2one(
         'account.invoice',
