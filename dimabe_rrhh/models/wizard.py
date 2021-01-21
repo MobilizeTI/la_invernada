f17161546fdc4e0bde74c2c0516055d9d4bf14c9
dimabe-odoo <desarrolloti@dimabe.cl>
2021-01-21 18:10:57 -0300

    test

--- dimabe_rrhh/models/wizard.py
+++ dimabe_rrhh/models/wizard.py
@@ -166,13 +166,13 @@ class WizardHrPaySlip(models.TransientModel):
         worksheet.write(3, 3, 'DEBE', format_title)
         worksheet.write(3, 4, 'HABER', format_title)
         line = self.env['hr.payslip.line'].search([('slip_id','=',payslips.mapped('id'))])
-        row = 0
-        col = 0
+        row = 5
+        col = 1
         rule = self.env['hr.salary.rule'].sudo().search([('show_in_central','=',True)])
         for data in rule:
             line = self.env['hr.payslip.line'].sudo().search([('slip_id','in',payslips.mapped('id')),('salary_rule_id','=',data.id)])
             total = sum(line.mapped("total"))
-            worksheet.write(0,0,total)
+            worksheet.write(row,col,total)
             row += 1
             col +=1
 
