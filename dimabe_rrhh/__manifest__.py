b52064060b4014c375789691cf61720cd9542fd1
dimabe-odoo <desarrolloti@dimabe.cl>
2021-01-21 09:15:10 -0300

    rrhh

--- dimabe_rrhh/__manifest__.py
+++ dimabe_rrhh/__manifest__.py
@@ -1,17 +1,15 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "dimabe_rrhh",
+    'name': "Dimabe RRHH",
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    'summary': """RRHH""",
 
     'description': """
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Dimabe ltda",
+    'website': "http://www.dimabe.cl",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
