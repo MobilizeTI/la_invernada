114883fa2152005168ef2087c9e49dc6471c75a5
dimabe-odoo <desarrolloti@dimabe.cl>
2021-01-22 17:24:12 -0300

    test

--- dimabe_rrhh/models/custom_mutuality.py
+++ dimabe_rrhh/models/custom_mutuality.py
@@ -13,7 +13,7 @@ class CustomMutuality(models.Model):
 
     @api.model
     def compute_partner_company(self):
-        hr_employee = self.env['hr.employee'].sudo().distinct_field_get(field='addr', value='')
+        hr_employee = self.env['hr.employee'].sudo().distinct_field_get(field='address_id', value='')
         raise models.ValidationError(hr_employee)
         company_ids = hr_employee.mapped('address_id')
         return [('id', 'in', company_ids)]
