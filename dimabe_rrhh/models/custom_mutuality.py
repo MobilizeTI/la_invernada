bfc47e24f185fd73dba69c45edef0a82ab9fe482
dimabe-odoo <desarrolloti@dimabe.cl>
2021-01-22 17:14:10 -0300

    test

--- dimabe_rrhh/models/custom_mutuality.py
+++ dimabe_rrhh/models/custom_mutuality.py
@@ -4,15 +4,16 @@ from odoo import models, fields, api
 class CustomMutuality(models.Model):
     _name = 'custom.mutuality'
 
-    company_id = fields.Many2one('res.partner','Compañia')
+    company_id = fields.Many2one('res.partner', 'Compañia', domain=lambda self: self.compute_partner_company)
 
     value = fields.Float('Valor')
 
-    indicator_id = fields.Many2one(comodel_name='hr.indicadores',auto_join=True,string='Indicadores')
+    indicator_id = fields.Many2one(comodel_name='hr.indicadores', auto_join=True, string='Indicadores')
 
-    partner_ids = fields.Many2many(comodel_name='res.partner',compute='compute_partner_company')
+    partner_ids = fields.Many2many(comodel_name='res.partner', compute='compute_partner_company')
 
-
-
-
-
+    @api.model
+    def compute_partner_company(self):
+        hr_employee = self.env['hr.employee'].sudo().search([])
+        company_ids = hr_employee.mapped('address_id')
+        return [('id', 'in', company_ids)]
