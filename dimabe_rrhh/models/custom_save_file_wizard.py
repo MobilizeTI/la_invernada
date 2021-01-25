9b107e06e943ccaebafe51fb16455f09e5493207
dimabe-odoo <desarrolloti@dimabe.cl>
2021-01-25 14:40:23 -0300

    Nuevo generar archivo previred

--- dimabe_rrhh/models/custom_save_file_wizard.py
+++ dimabe_rrhh/models/custom_save_file_wizard.py
@@ -4,3 +4,5 @@ class CustomSaveFileWizard(models.TransientModel):
     _name = 'custom.save.file.wizard'
 
     file = fields.Binary('Archivo', readonly=True)
+
+    file_name = fields.Char('Nombre del archivo', readonly=True)
