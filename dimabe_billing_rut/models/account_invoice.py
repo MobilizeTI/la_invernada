8becf9e157721bf4428e7852b3912169c94caad9
dimabe-odoo <desarrolloti@dimabe.cl>
2021-01-28 11:06:25 -0300

    Other Validation in Add products

--- dimabe_billing_rut/models/account_invoice.py
+++ dimabe_billing_rut/models/account_invoice.py
@@ -784,8 +784,8 @@ class AccountInvoice(models.Model):
     @api.multi  
     def add_products_by_order(self):
         if self.stock_picking_ids and self.order_to_add_ids:
-            if self.stock_picking_ids.sale_id != self.order_to_add_id:
-                raise models.ValidationError('El Desapcho {} {} no pertenece al pedido {} {}'.format(self.stock_picking_ids.sale_id,self.stock_picking_ids.name,self.order_to_add_id,self.order_to_add_ids.name))
+            if self.stock_picking_ids.sale_id.id != self.order_to_add_id:
+                raise models.ValidationError('El Desapcho {} no pertenece al pedido {}'.format(self.stock_picking_ids.name,self.order_to_add_ids.name))
     
             product_ids = self.env['sale.order.line'].search([('order_id','=',self.order_to_add_ids.id)])
             stock_picking_line = self.env['stock.move.line'].search([('picking_id','=',self.stock_picking_ids.id)])
