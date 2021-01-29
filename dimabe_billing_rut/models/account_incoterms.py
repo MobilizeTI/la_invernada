bd8ef44a1992f7ad382df39cd3832b40ef8cabac
dimabe-odoo <desarrolloti@dimabe.cl>
2021-01-29 12:47:11 -0300

    change incoterms from export clause

--- dimabe_billing_rut/models/account_incoterms.py
+++ dimabe_billing_rut/models/account_incoterms.py
@@ -2,6 +2,6 @@ from odoo import models, fields
 
 class AccountIncoterms(models.Model):
     _inherit = 'account.incoterms'
-    _rec_name = 'sii_code'
+    _rec_name = 'code'
 
     sii_code = fields.Char(string="Código SII")
