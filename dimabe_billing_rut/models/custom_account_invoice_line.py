1a23f3e7313dc30e5c29ebaa13456b5abefba22d
dimabe-odoo <desarrolloti@dimabe.cl>
2021-01-29 10:02:05 -0300

    create custom account invoice line

--- dimabe_billing_rut/models/custom_account_invoice_line.py
+++ dimabe_billing_rut/models/custom_account_invoice_line.py
@@ -3,21 +3,19 @@ from odoo import models, fields, api
 class CustomAccountInvoiceLine(models.Model):
     _name = "custom.account.invoice.line"
 
-    invoice_id = fields.many2one('account.invoice',string="Factura")
+    #invoice_id = fields.Many2one('account.invoice',string="Factura")
 
-    product_id = fields.Many2one('product.product',string="Producto")
+    #product_id = fields.Many2one('product.product',string="Producto")
 
-    #name = fields.Char(string="Descripción")
-
-    account_id = fields.Many2one('account.account', string="Cuenta")
+    #account_id = fields.Many2one('account.account', string="Cuenta")
 
     quantity = fields.Float(string="Cantidad")
 
-    uom_id = fields.Many2one('uom.uom',string="Unidad de medida")
+    #uom_id = fields.Many2one('uom.uom',string="Unidad de medida")
 
     price_unit = fields.Float(string="Precio")
 
-    invoice_tax_line_ids = fields.Many2many('account.tax', string="Impuestos")
+    #invoice_tax_line_ids = fields.Many2many('account.tax', string="Impuestos")
 
     exempt = fields.Selection([
             ('1', 'No afecto o exento de IVA'),
