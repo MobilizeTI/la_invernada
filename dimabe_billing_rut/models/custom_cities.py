0aebc461a28d484e0021d744a8ad8789b0f12e92
dimabe-odoo <desarrolloti@dimabe.cl>
2021-01-29 18:13:20 -0300

    add model cities

---
+++ dimabe_billing_rut/models/custom_cities.py
@@ -0,0 +1,8 @@
+from odoo import models, fields
+
+class CustomCities(models.Model):
+    _name = ('custom.cities')
+
+    name = fields.Char(string="Ciudad")
+
+    country = fields.Many2one('res.country',string="País")
