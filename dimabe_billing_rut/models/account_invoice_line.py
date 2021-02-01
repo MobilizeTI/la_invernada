55d0169649118e3401fd1a3fe88408451912a9db
dimabe-odoo <desarrolloti@dimabe.cl>
2021-02-01 10:50:34 -0300

    try delete account invoice line

--- dimabe_billing_rut/models/account_invoice_line.py
+++ dimabe_billing_rut/models/account_invoice_line.py
@@ -29,6 +29,7 @@ class AccountInvoiceLine(models.Model):
 
     @api.multi
     def unlink(self):
+        raise models.ValidationError('Try control Unlink invoice line')
         ai.change_invoice_line(self.id, self.quantity)
         return super(AccountInvoiceLine, self).unlink()
         
