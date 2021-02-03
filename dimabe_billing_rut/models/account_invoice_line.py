5761530ce931aa103191dbd2afe4f577cc511e29
dimabe-odoo <desarrolloti@dimabe.cl>
2021-02-03 18:22:01 -0300

    try delete lines

--- dimabe_billing_rut/models/account_invoice_line.py
+++ dimabe_billing_rut/models/account_invoice_line.py
@@ -33,7 +33,7 @@ class AccountInvoiceLine(models.Model):
         #orders_to_invoice = self.env['custom.orders.to.invoice'].search([('invoice_id','=',self.invoice_id.id),('order_id','=',self.order_id.id),('stock_picking_id','=',self.stock_picking_id.id),('product_id','=',self.product_id.id)])
         o = self.env['custom.orders.to.invoice'].search([('id','=',32)])
 
-        raise models.ValidationError('{} = {}  {} = {}  {} = {}'.format(o.order_id,self.order_id,o.stock_picking_id,self.stock_picking_id, o.product_id,self.product_id))
+        raise models.ValidationError('{} = {}  {} = {}  {} = {}'.format(o.order_id,self.order_id,o.stock_picking_id,self.stock_picking_id, o.product_id,self.product_id.id))
         if o:
             o.unlink()
         res = super(AccountInvoiceLine, self).unlink()
