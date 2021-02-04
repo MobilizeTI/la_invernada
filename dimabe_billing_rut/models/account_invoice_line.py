904f502c5adf09b82ab6dcf48a3e6c24ddb2fa2d
dimabe-odoo <desarrolloti@dimabe.cl>
2021-02-04 12:54:39 -0300

    Calculate Values For Exchange

--- dimabe_billing_rut/models/account_invoice_line.py
+++ dimabe_billing_rut/models/account_invoice_line.py
@@ -34,19 +34,5 @@ class AccountInvoiceLine(models.Model):
         #self._onchange_invoice_line()
         return res
 
-    @api.multi
-    def create(self, vals):
-        invoice_id = self.env['account.invoice'].search([('id','=',self.invoice_id.id)])
-        if self.invoice_id.currency_id.name == "CLP":
-            if self.env.user.company_id.id == 1 and self.invoice_id.dte_type_id.code != "110":
-                self.write({
-                    'price_unit': self.roundclp(self.amount_untaxed * self.roundclp(self.exchange_rate)),
-                })
-
-            elif self.env.user.company_id.id == 3:
-                self.write({
-                    'price_unit': self.roundclp(self.amount_untaxed * self.roundclp(self.exchange_rate)),
-                })
-        res = super(AccountInvoiceLine, self).create()
-        return res
+    
 
