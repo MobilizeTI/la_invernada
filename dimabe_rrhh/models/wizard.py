0b02eee9bebf89eb6f0bb3fc64716d932d0989dd
dimabe-odoo <desarrolloti@dimabe.cl>
2021-02-09 09:31:42 -0300

    verify afc

--- dimabe_rrhh/models/wizard.py
+++ dimabe_rrhh/models/wizard.py
@@ -493,9 +493,9 @@ class WizardHrPaySlip(models.TransientModel):
     def verify_quotation_afc(self,TOTIM,indicadores,contract):
         totimp = float(TOTIM)
         if contract.type_id.name == 'Plazo Fijo':
-            return round(TOTIM * indicadores.contrato_plazo_fijo_empleador)
+            return round(totimp * indicadores.contrato_plazo_fijo_empleador)
         elif contract.type_id.name == 'Plazo Indefinido':
-            return round(TOTIM * indicadores.contrato_plazo_indefinido_empleador)
+            return round(totimp * indicadores.contrato_plazo_indefinido_empleador)
         else:
             return 0
 
