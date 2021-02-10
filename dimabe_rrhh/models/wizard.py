a09878e24924811c25cd868b474aada59ceff01f
Leonardo Norambuena <leonardo.norambuenam@gmail.com>
2021-02-10 16:54:23 -0300

    debug libro remuneraciones

--- dimabe_rrhh/models/wizard.py
+++ dimabe_rrhh/models/wizard.py
@@ -87,9 +87,9 @@ class WizardHrPaySlip(models.TransientModel):
         row = 0
         col = 0
         payslips = self.env['hr.payslip'].sudo().search(
-            [('indicadores_id', '=', indicadores.id), ('state', 'in', ['done', 'draft']),('employee_id.address_id.company_id.id','=',self.company_id.id)])
-        company_ids = payslips.mapped('employee_id.address_id.company_id.id')
-        raise models.ValidationError(self.company_id.id)
+            [('indicadores_id', '=', indicadores.id), ('state', 'in', ['done', 'draft']),('employee_id.address_id.id','=',self.company_id.id)])
+        company_ids = payslips.mapped('employee_id.address_id.id')
+        raise models.ValidationError(company_ids)
         totals = self.env['hr.payslip.line'].sudo().search([('slip_id', 'in', payslips.mapped('id'))]).filtered(
             lambda a: a.total > 0)
 
