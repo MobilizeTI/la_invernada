44d6535fffa0fa2a85baf37c9a665bb1928b97bc
dimabe-odoo <desarrolloti@dimabe.cl>
2021-02-10 15:39:30 -0300

    modify condition avoid 0

--- dimabe_rrhh/models/wizard.py
+++ dimabe_rrhh/models/wizard.py
@@ -88,7 +88,7 @@ class WizardHrPaySlip(models.TransientModel):
         payslips = self.env['hr.payslip'].sudo().search(
             [('indicadores_id', '=', indicadores.id), ('state', 'in', ['done', 'draft'])])
         totals = self.env['hr.payslip.line'].sudo().search([('slip_id', 'in', payslips.mapped('id'))]).filtered(
-            lambda a: a.total > 0 or a.total and a.slip_id.employee_id.address_id.id == self.company_id.id)
+            lambda a: a.total > 0 and a.total and a.slip_id.employee_id.address_id.id == self.company_id.id)
 
         totals_result = []
         for pay in payslips:
