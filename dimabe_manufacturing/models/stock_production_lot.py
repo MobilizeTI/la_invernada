415539c3074db9ba4d9c2bfac54a8630d9c7bc91
dimabe-odoo <desarrolloti@dimabe.cl>
2021-02-11 11:34:35 -0300

    modify add_reserved pallet and serial

--- dimabe_manufacturing/models/stock_production_lot.py
+++ dimabe_manufacturing/models/stock_production_lot.py
@@ -661,8 +661,9 @@ class StockProductionLot(models.Model):
             picking.move_line_ids_without_package.filtered(lambda a: a.product_id.id == self.product_id.id).write({
                 'product_uom_qty':sum(self.stock_production_lot_serial_ids.filtered(lambda a: a.reserved_to_stock_picking_id).mapped('display_weight'))
             })
-        picking.dispatch_line_ids.filtered(lambda a: a.product_id == self.product_id).write({
-            'real_dispatch_qty':sum(self.stock_production_lot_serial_ids.filtered(lambda a: a.to_add).mapped('display_weight'))
+        dispatch_line =picking.dispatch_line_ids.filtered(lambda a: a.product_id == self.product_id)
+        dispatch_line.write({
+            'real_dispatch_qty':dispatch_line.real_dispatch_qty + sum(self.stock_production_lot_serial_ids.filtered(lambda a: a.to_add).mapped('display_weight'))
         })
         self.stock_production_lot_serial_ids.filtered(lambda a: a.to_add).write({
             'to_add': False
@@ -692,8 +693,9 @@ class StockProductionLot(models.Model):
             picking.move_line_ids_without_package.filtered(lambda a: a.product_id.id == self.product_id.id and a.lot_id.id == self.id).write({
                 'product_uom_qty': sum(self.pallet_ids.filtered(lambda a: a.reserved_to_stock_picking_id).mapped('lot_serial_ids').mapped('display_weight'))
             })
-        picking.dispatch_line_ids.filtered(lambda a: a.product_id == self.product_id).write({
-            'real_dispatch_qty': sum(self.pallet_ids.filtered(lambda a: a.add_picking).mapped('lot_serial_ids').mapped('display_weight'))
+        dispatch_line = picking.dispatch_line_ids.filtered(lambda a: a.product_id.id == self.product_id.id)
+        dispatch_line.write({
+            'real_dispatch_qty':dispatch_line.real_dispatch_qty + sum(self.pallet_ids.filtered(lambda a: a.add_picking).mapped('lot_serial_ids').mapped('display_weight'))
         })
         for pallet in self.pallet_ids.filtered(lambda a:a.add_picking):
             pallet.lot_serial_ids.write({
