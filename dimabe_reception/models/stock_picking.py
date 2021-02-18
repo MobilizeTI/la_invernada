861c82daa491c4e34b56cbd9e9d8b7005f7a2033
dimabe-odoo <desarrolloti@dimabe.cl>
2021-02-18 15:28:08 -0300

    fix error se modifica funcionalidad de multiple despachos

--- dimabe_reception/models/stock_picking.py
+++ dimabe_reception/models/stock_picking.py
@@ -438,7 +438,7 @@ class StockPicking(models.Model):
                         })
             for item in self.dispatch_line_ids:
                 if item.dispatch_id.state != 'done':
-                    item.dispatch_id.button_validate()
+                    item.dispatch_id.action_done()
                 else:
                     continue
         else:
