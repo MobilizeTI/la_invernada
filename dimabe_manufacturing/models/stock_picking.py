6fb8f65d15407f0a1d72e93c5500f3e077637b6b
dimabe-odoo <desarrolloti@dimabe.cl>
2021-02-19 11:51:37 -0300

    raise para debug

--- dimabe_manufacturing/models/stock_picking.py
+++ dimabe_manufacturing/models/stock_picking.py
@@ -209,9 +209,8 @@ class StockPicking(models.Model):
             qty_move = sum(lot.stock_production_lot_serial_ids.filtered(
                 lambda a: a.reserved_to_stock_picking_id.id == self.id).mapped('display_weight'))
             quant = self.env['stock.quant'].search([('lot_id', '=', lot.id), ('location_id.usage', '=', 'internal')])
-            raise models.ValidationError(sum(lot.stock_production_lot_serial_ids.filtered(lambda
-                                                                                          x: x.reserved_to_stock_picking_id and x.reserved_to_stock_picking_id.state != 'done' and not x.consumed).mapped(
-                    'display_weight')))
+            raise models.ValidationError(sum(lot.stock_production_lot_serial_ids.filtered(
+                    lambda x: not x.reserved_to_stock_picking_id and not x.consumed).mapped('display_weight')))
             quant.write({
                 'reserved_quantity': sum(lot.stock_production_lot_serial_ids.filtered(lambda
                                                                                           x: x.reserved_to_stock_picking_id and x.reserved_to_stock_picking_id.state != 'done' and not x.consumed).mapped(
