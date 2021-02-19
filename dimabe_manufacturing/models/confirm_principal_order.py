3b763d2fd24110fa6b9b59e1590629430fad0e6f
dimabe-odoo <desarrolloti@dimabe.cl>
2021-02-19 13:54:49 -0300

    raise para debug

--- dimabe_manufacturing/models/confirm_principal_order.py
+++ dimabe_manufacturing/models/confirm_principal_order.py
@@ -15,7 +15,7 @@ class ConfirmPrincipalOrde(models.TransientModel):
         self.picking_id.dispatch_line_ids.filtered(lambda a: a.sale_id.id == self.sale_id.id).write({
             'is_select':True
         })
-        report = self.env.ref('dimabe_export_order.action_packing_list').render_qweb_pdf(self.picking_id.filtered(lambda a: a.is_select).dispatch_id.id)
+        report = self.env.ref('dimabe_export_order.action_packing_list').render_qweb_pdf(self.picking_id.dispatch_line_ids.filtered(lambda a: a.is_select).dispatch_id.id)
         for item in self.picking_id.dispatch_line_ids:
             item.dispatch_id.write({
                 'packing_list_file':base64.b64encode(report[0])
