70427995c6e67782f014aa1a7e63bd56fd28122b
dimabe-odoo <desarrolloti@dimabe.cl>
2021-02-24 09:39:05 -0300

    modificacion el reservas

--- dimabe_manufacturing/models/confirm_principal_order.py
+++ dimabe_manufacturing/models/confirm_principal_order.py
@@ -36,7 +36,7 @@ class ConfirmPrincipalOrde(models.TransientModel):
 
     def process_data(self):
         for item in self.custom_dispatch_line_ids:
-            item.dispatch_id.clean_reserved(item.dispatch_id.id)
+            item.dispatch_id.clean_reserved(item.dispatch_id)
             self.env['stock.move.line'].create({
                 'move_id': item.dispatch_id.move_ids_without_package.filtered(
                     lambda m: m.product_id.id == item.product_id.id).id,
