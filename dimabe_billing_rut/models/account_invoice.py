9fb29c835442a4030747c3a8ec71c180305ede3e
dimabe-odoo <desarrolloti@dimabe.cl>
2021-02-26 13:19:39 -0300

    test

--- dimabe_billing_rut/models/account_invoice.py
+++ dimabe_billing_rut/models/account_invoice.py
@@ -992,7 +992,7 @@ class AccountInvoice(models.Model):
                 'departure_date': self.departure_date,
                 'arrival_date': self.arrival_date,
                 'customs_department': self.custom_department,
-                'transport': self.transport_to_port.name
+                #'transport': self.transport_to_port.name
             })
 
         return res
