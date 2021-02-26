dc4cb572f267541aab050db8122f80b9e045ee2e
dimabe-odoo <desarrolloti@dimabe.cl>
2021-02-26 09:33:51 -0300

    AddColumnQtyInSaleOrder

--- dimabe_export_order/models/sale_order.py
+++ dimabe_export_order/models/sale_order.py
@@ -7,8 +7,21 @@ class SaleOrder(models.Model):
 
     contract_number = fields.Char('Contrato')
 
-    ship_ids = fields.Many2many('custom.ship', compute="compute_ships")
+    ship_ids = fields.Many2many('custom.ship', string="Nave",compute="compute_ships")
+
+    ordered_quantity = fields.Float(string="Cantidad Pedida", compute="compute_ordered_quantity")
+
+    delivered_quantity = fields.Float(string="Cantidad Entregada", compute="compute_delivered_quantity")
 
     def compute_ships(self):
         for item in self:
             item.ship_ids = item.picking_ids.mapped('ship') 
+    
+    def compute_ordered_quantity(self):
+        for item in self:
+            item.ordered_quantity = item.order_line[0].product_uom_qty
+
+    def compute_delivered_quantity(self):
+        for item in self:
+            item.ordered_quantity = item.order_line[0].qty_delivered
+
