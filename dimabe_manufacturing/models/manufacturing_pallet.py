662161607557b0371598ccd1107e393d4fe9408d
dimabe-odoo <desarrolloti@dimabe.cl>
2021-02-26 18:10:00 -0300

    modifyPackingList

--- dimabe_manufacturing/models/manufacturing_pallet.py
+++ dimabe_manufacturing/models/manufacturing_pallet.py
@@ -132,7 +132,7 @@ class ManufacturingPallet(models.Model):
         for item in self:
             if 'picking_id' in self.env.context.keys():
                 picking_id = self.env.context['picking_id']
-                item.total_reserved_serial = self.total_reserved_reserved(picking_id)
+                item.total_reserved_serial = self.get_total_reserved_reserved(picking_id)
 
     def get_total_reserved_reserved(self,picking_id):
         return len(self.lot_serial_ids.filtered(lambda a: a.reserved_to_stock_picking_id.id == picking_id))
