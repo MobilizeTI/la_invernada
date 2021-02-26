e6a17cefff35dc5ee9851cd1bb445268f0246a2a
dimabe-odoo <desarrolloti@dimabe.cl>
2021-02-26 11:53:46 -0300

    FixErrorUpdate

--- dimabe_manufacturing/models/confirm_principal_order.py
+++ dimabe_manufacturing/models/confirm_principal_order.py
@@ -52,7 +52,8 @@ class ConfirmPrincipalOrde(models.TransientModel):
             item.dispatch_id.clean_reserved(item.dispatch_id)
             for line in self.picking_id.move_line_ids_without_package.filtered(
                     lambda a: a.product_id.id == item.product_id.id):
-                models.ValidationError(f'{item.dispatch_id.name} {item.dispatch_id.id} {item.product_id.id}')
+                if item.dispatch_id.id == self.picking_id.id:
+                    continue
                 line.write({
                     'picking_id': item.dispatch_id.id,
                     'move_id': item.dispatch_id.move_ids_without_package.filtered(
