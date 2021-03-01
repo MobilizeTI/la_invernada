3a814a82fd1b9e2ecfe3df73850242a2bd9c4d34
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-01 18:23:00 -0300

    AddValidationInDispatchGuide

--- dimabe_export_order/models/sale_order.py
+++ dimabe_export_order/models/sale_order.py
@@ -19,7 +19,7 @@ class SaleOrder(models.Model):
 
     shipping_number = fields.Integer(string="Número Embarque", compute="compute_shipping_number")
 
-    partner_id = fields.Many2one('res.partner',string='Cliente')
+    partner_id = fields.Many2one('res.partner')
 
     def compute_ships(self):
         for item in self:
