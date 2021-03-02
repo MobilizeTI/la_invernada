4d9678b928f698d4e168c6530b31ef47d9d94b7e
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-02 09:22:01 -0300

    ReestructureFieldsInModules

--- dimabe_export_order/models/account_invoice.py
+++ dimabe_export_order/models/account_invoice.py
@@ -7,6 +7,39 @@ import json
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
+    #To Intructuives
+
+    temperature = fields.Char('Temperatura')
+
+    ventilation = fields.Char('Ventilación')
+
+    humidity = fields.Char('Humedad')
+
+    withdrawal_deposit = fields.Many2one('custom.withdrawal.deposits',string="Depósito Retiro")
+
+    freight_payment_term = fields.Many2one('custom.freight.payment.term',string="Termino de Pago Flete")
+
+    safe_type = fields.Many2one('custom.safe.type',string="Tipo de Seguro")
+
+    stacking = fields.Char(string="Stacking")
+
+    cut_off = fields.Char(string="Cut Off")
+
+    dus_second_send = fields.Char(string="D.U.S 2DO.ENVÍO")
+
+    bill_of_lading = fields.Char(string="Conocimiento de Embarque")
+
+    phytosanitary_certificate = fields.Char(string="Certificado Fitosanitario")
+
+    origin_certificate = fields.Char(string="Certificado Origen")
+
+    plant = fields.Many2one('res.partner', string="Planta")
+
+    quality_type = fields.Many2one('custom.quality.type',string="Calidad")
+
+    consolidation = fields.Char(string="Consolidación")
+
+    total_container = fields.Char(string="Total de Contenedores")
 
     notify_ids = fields.Many2many(
             'res.partner',
