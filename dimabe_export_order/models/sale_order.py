db5727e782f93c8ba382e54a1bea0680c55dc97e
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-08 15:49:15 -0300

    Change View Sale Orden in Other Company

--- dimabe_export_order/models/sale_order.py
+++ dimabe_export_order/models/sale_order.py
@@ -8,42 +8,48 @@ class SaleOrder(models.Model):
 
     contract_number = fields.Char('Contrato')
 
-    ship_ids = fields.Many2many('custom.ship', string="Nave", compute="compute_ships")
+    ship_ids = fields.Many2many('custom.ship', string="Nave", compute="_compute_ships")
 
-    ordered_quantity = fields.Float(string="Cantidad Pedida", compute="compute_ordered_quantity")
+    ordered_quantity = fields.Float(string="Cantidad Pedida", compute="_compute_ordered_quantity")
 
-    delivered_quantity = fields.Float(string="Cantidad Entregada", compute="compute_delivered_quantity")
+    delivered_quantity = fields.Float(string="Cantidad Entregada", compute="_compute_delivered_quantity")
 
-    unit_price = fields.Float(string="Precio Unitario", compute="compute_unit_price")
+    unit_price = fields.Float(string="Precio Unitario", compute="_compute_unit_price")
 
-    departure_date = fields.Datetime(string="Fecha de Zarpe", compute="compute_departure_date")
+    departure_date = fields.Datetime(string="Fecha de Zarpe", compute="_compute_departure_date")
 
-    shipping_number = fields.Char(string="Número Embarque", compute="compute_shipping_number")
+    shipping_number = fields.Char(string="Número Embarque", compute="_compute_shipping_number")
 
     partner_id = fields.Many2one('res.partner', "Cliente", readonly=False)
 
-    def compute_ships(self):
+    company_id = fields.Integer(compute="_compute_company_id")
+
+    def _compute_company_id(self):
+        for item in self:
+            item.company_id = self.env.user.company_id.id
+
+    def _compute_ships(self):
         for item in self:
             item.ship_ids = item.picking_ids.mapped('ship')
 
-    def compute_ordered_quantity(self):
+    def _compute_ordered_quantity(self):
         for item in self:
             item.ordered_quantity = item.order_line[0].product_uom_qty
 
-    def compute_delivered_quantity(self):
+    def _compute_delivered_quantity(self):
         for item in self:
             item.delivered_quantity = item.order_line[0].qty_delivered
 
-    def compute_unit_price(self):
+    def _compute_unit_price(self):
         for item in self:
             item.unit_price = item.order_line[0].price_unit
 
-    def compute_departure_date(self):
+    def _compute_departure_date(self):
         #valid if one dispatch of sale order havent date
         for item in self:
             item.departure_date = item.picking_ids[0].departure_date
 
-    def compute_shipping_number(self):
+    def _compute_shipping_number(self):
         for item in self:
             shipping_number = ""
             for picking in item.picking_ids:
