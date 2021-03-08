9d0fe10832b472928ac6e0b2c29eaaae00587ef1
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-08 12:50:26 -0300

    se corrige error en generacion de libro

--- dimabe_billing_rut/models/account_invoice_xlsx.py
+++ dimabe_billing_rut/models/account_invoice_xlsx.py
@@ -424,7 +424,8 @@ class AccountInvoiceXlsx(models.Model):
         another_taxes = inv.mapped('invoice_line_ids').filtered(
             lambda a: 'IVA Crédito' not in a.invoice_line_tax_ids.mapped(
                 'name') or 'IVA Crédito' not in a.invoice_line_tax_ids.mapped(
-                'name') or 'Exento' not in a.invoice_line_tax_ids.mapped('name'))
+                'name') or 'Exento' not in a.invoice_line_tax_ids.mapped(
+                'name') or len(a.invoice_line_tax_ids) != 0)
         sheet.write('L{}'.format(str(row)), round(sum(another_taxes.mapped('price_subtotal'))))
         sheet.write('M{}'.format(str(row)), round(inv.amount_total_signed), formats['number'])
         return sheet
