157bc9e4c6c3921cf5a86962f6a67b15dac34eb7
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-08 12:01:29 -0300

    se corrige error en generacion de libro

--- dimabe_manufacturing/models/update_stock_quant.py
+++ dimabe_manufacturing/models/update_stock_quant.py
@@ -12,27 +12,29 @@ class UpdateStockQuant(models.TransientModel):
 
     @api.multi
     def update(self):
-        lot = self.env['stock.production.lot'].search([('product_id.id', '=',self.product_id.id)])
-        if lot.stock_production_lot_serial_ids.filtered(lambda a: not a.consumed):
-            quant = self.env['stock.quant'].sudo().search(
-                [('lot_id', '=', lot.id), ('location_id.usage', '=', 'internal'), ('location_id', '=', self.picking_id.location_id.id)])
+        lots = self.env['stock.production.lot'].search([('product_id.id', '=',self.product_id.id)])
+        for lot in lots:
+            if lot.stock_production_lot_serial_ids.filtered(lambda a: not a.consumed):
+                quant = self.env['stock.quant'].sudo().search(
+                    [('lot_id', '=', lot.id), ('location_id.usage', '=', 'internal'),
+                     ('location_id', '=', self.picking_id.location_id.id)])
 
-            if quant:
-                quant.write({
-                    'reserved_quantity': sum(lot.stock_production_lot_serial_ids.filtered(lambda
-                                                                                              x: x.reserved_to_stock_picking_id and x.reserved_to_stock_picking_id.state != 'done' and not x.consumed).mapped(
-                        'display_weight')),
-                    'quantity': sum(lot.stock_production_lot_serial_ids.filtered(
-                        lambda x: not x.reserved_to_stock_picking_id and not x.consumed).mapped('display_weight'))
-                })
-            else:
-                self.env['stock.quant'].sudo().create({
-                    'lot_id': lot.id,
-                    'product_id': lot.product_id.id,
-                    'reserved_quantity': sum(lot.stock_production_lot_serial_ids.filtered(lambda
-                                                                                              x: x.reserved_to_stock_picking_id and x.reserved_to_stock_picking_id.state != 'done' and not x.consumed).mapped(
-                        'display_weight')),
-                    'quantity': sum(lot.stock_production_lot_serial_ids.filtered(
-                        lambda x: not x.reserved_to_stock_picking_id and not x.consumed).mapped('display_weight')),
-                    'location_id': self.picking_id.location_id.id
-                })
+                if quant:
+                    quant.write({
+                        'reserved_quantity': sum(lot.stock_production_lot_serial_ids.filtered(lambda
+                                                                                                  x: x.reserved_to_stock_picking_id and x.reserved_to_stock_picking_id.state != 'done' and not x.consumed).mapped(
+                            'display_weight')),
+                        'quantity': sum(lot.stock_production_lot_serial_ids.filtered(
+                            lambda x: not x.reserved_to_stock_picking_id and not x.consumed).mapped('display_weight'))
+                    })
+                else:
+                    self.env['stock.quant'].sudo().create({
+                        'lot_id': lot.id,
+                        'product_id': lot.product_id.id,
+                        'reserved_quantity': sum(lot.stock_production_lot_serial_ids.filtered(lambda
+                                                                                                  x: x.reserved_to_stock_picking_id and x.reserved_to_stock_picking_id.state != 'done' and not x.consumed).mapped(
+                            'display_weight')),
+                        'quantity': sum(lot.stock_production_lot_serial_ids.filtered(
+                            lambda x: not x.reserved_to_stock_picking_id and not x.consumed).mapped('display_weight')),
+                        'location_id': self.picking_id.location_id.id
+                    })
