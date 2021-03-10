5958f2fb3d39df7e285c8380b0bbc7580361a017
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-10 18:09:00 -0300

    add totals

--- dimabe_billing_rut/models/account_invoice_xlsx.py
+++ dimabe_billing_rut/models/account_invoice_xlsx.py
@@ -43,7 +43,6 @@ class AccountInvoiceXlsx(models.Model):
                 })
             for wk in array_worksheet:
                 sheet = wk['worksheet']
-                formats = self.set_formats(workbook)
                 region = self.env['region.address'].search([('id', '=', 1)])
                 titles = ['Cod.SII', 'Folio', 'Cor.Interno', 'Fecha', 'RUT', 'Nombre', '#', 'EXENTO', 'NETO', 'IVA',
                           'IVA NO RECUPERABLE']
