5b7094af92bd599604ad06a9e0391439ab32079d
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-11 19:08:02 -0300

    test

--- dimabe_export_order/models/custom_customer_orders_xls.py
+++ dimabe_export_order/models/custom_customer_orders_xls.py
@@ -54,6 +54,7 @@ class CustomCustomerOrdersXls(models.TransientModel):
                 stock_picking_ids = self.env['stock.picking'].sudo().search([('sale_id','=',order.id)])
                 for stock in stock_picking_ids:
                     invoice_line = self.env['account.invoice.line'].sudo().search([('stock_picking_id','=',stock.id)])
+                    raise models.ValidationError(invoice_line.invoice_id)
                     account_invoice = self.env['account.invoice'].sudo().search([('id','=',invoice_line[0].invoice_id)])
                     #N° Embarque
                     sheet.write(row, col, stock.shipping_number if stock.shipping_number else '') 
