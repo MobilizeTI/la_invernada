aae245944bdbaf0167a1e5beca671ef860478f80
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-12 17:31:37 -0300

    try dispacth guide with multiple dispatch

--- dimabe_billing_rut/models/stock_picking.py
+++ dimabe_billing_rut/models/stock_picking.py
@@ -221,7 +221,7 @@ class StockPicking(models.Model):
             haveExempt = False
             
             price_unit =  item.sale_id.mapped('order_line').filtered(lambda a : a.product_id.id == item.product_id.id).price_unit
-            amount = item.real_dispatch_qty if self.is_multiple_dispatch else item.quantity_done * price_unit
+            amount = item.real_dispatch_qty * price_unit if self.is_multiple_dispatch else item.quantity_done * price_unit
             tax_ids = self.sale_id.mapped('order_line').filtered(lambda a : a.product_id.id == item.product_id.id).tax_id
             amount_tax = 0
             for tax in tax_ids:
