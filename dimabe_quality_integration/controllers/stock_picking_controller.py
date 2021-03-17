94324eb7ade3bf9fd5a2bc4061ad584742f5434d
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-17 11:48:04 -0300

    test tima zone in integration

--- dimabe_quality_integration/controllers/stock_picking_controller.py
+++ dimabe_quality_integration/controllers/stock_picking_controller.py
@@ -33,7 +33,7 @@ class StockPickingController(http.Controller):
                                     'VarietyName': res.move_ids_without_package[0].product_id.get_variety(),
                                     'LotNumber': res.name,
                                     'DispatchGuideNumber': res.guide_number,
-                                    'ReceptionDate': res.scheduled_date or res.write_date,
+                                    'ReceptionDate': time_to_tz_naive(res.scheduled_date, pytz.utc, pytz.timezone("America/Santiago")) or time_to_tz_naive(res.write_date, pytz.utc, pytz.timezone("America/Santiago")),
                                     'ReceptionKgs': kgs if kgs > 0 else res.production_net_weight,
                                     'ContainerType': res.get_canning_move().product_id.display_name,
                                     'ContainerWeightAverage': res.avg_unitary_weight,
@@ -144,3 +144,5 @@ class StockPickingController(http.Controller):
         return data
 
 
+    def time_to_tz_naive(t, tz_in, tz_out):
+        return tz_in.localize(datetime.combine(datetime.today(), t)).astimezone(tz_out).time()
