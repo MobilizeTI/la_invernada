110aba0e505c370d4cc9e508a488ade8105f99cc
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-18 10:46:31 -0300

    last view custom notify

--- dimabe_export_order/models/custom_notify.py
+++ dimabe_export_order/models/custom_notify.py
@@ -8,4 +8,14 @@ class CustomNotify(models.Model):
 
     partner_name = fields.Char(string="Cliente", related='partner_id.name')
 
+    partner_identifier_type = fields.Char(string="Tipo de Notificador", related='partner_id.client_identifier_id.name')
+
+    partner_identifier_value = fields.Char(string="Valor Notificador", related='partner_id.client_identifier_value')
+
+    partner_contact = fields.Char(string="Contacto", related='partner_id.child_ids[0].name')
+
+    partner_contact_phone = fields.Char(string="Contacto", related='partner_id.child_ids[0].phone')
+    
+    partner_contact_mobile = fields.Char(string="Contacto", related='partner_id.child_ids[0].mobile')
+
     position = fields.Integer("Posición",nullable=True)
