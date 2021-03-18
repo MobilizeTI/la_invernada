06c180e1f841d2960bd95fc92af2a5db5d6b7ba4
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-18 10:10:18 -0300

    add name in custom notify

--- dimabe_export_order/models/custom_notify.py
+++ dimabe_export_order/models/custom_notify.py
@@ -4,6 +4,8 @@ from odoo import models, fields
 class CustomNotify(models.Model):
     _name = 'custom.notify'
 
-    partner_id = fields.Many2one('res.partner', string='Cliente', required=True, domain=[('customer', '=', True)])
+    partner_id = fields.Many2one('res.partner', domain=[('customer', '=', True)], string='Cliente', required=True)
+
+    partner_name = fields.Char(string="Cliente", related='partner_id.name')
 
     position = fields.Integer("Posición",nullable=True)
