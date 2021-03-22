158cdb32c0725a8a6fee8338149f7cdda386da55
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-22 10:56:57 -0300

    add buttons

--- dimabe_manufacturing/models/process_report_xlsx.py
+++ dimabe_manufacturing/models/process_report_xlsx.py
@@ -113,9 +113,9 @@ class ProcessReport(models.TransientModel):
                 col = 0
             row = 0
             for out_serial in process.summary_out_serial_ids:
-                sheet.write(row, col, out_serial.reserved_to_production_id.name, text_format)
+                sheet.write(row, col, out_serial.production_id.name, text_format)
                 col += 1
-                sheet.write(row, col, out_serial.reserved_to_production_id.sale_order_id.name, text_format)
+                sheet.write(row, col, out_serial.production_id.sale_order_id.name, text_format)
                 col += 1
                 sheet.write(row, col, out_serial.packaging_date.strftime('%d-%m-%Y'), text_format)
                 col += 1
