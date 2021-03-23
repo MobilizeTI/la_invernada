55cd902c2eb803e67d719f2a4882a07456c3a145
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-23 13:32:39 -0300

    test

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -487,8 +487,7 @@ class MrpWorkorder(models.Model):
         self.process_serial(serial=self.confirmed_serial)
 
     def _on_barcode_scanned(self,barcode):
-        res = super(MrpWorkorder, self)._on_barcode_scanned(barcode)
-        raise models.ValidationError(res)
+        self.process_serial(serial=barcode)
 
     def process_serial(self, serial):
         serial = self.env['stock.production.lot.serial'].search([('serial_number', '=', serial)])
