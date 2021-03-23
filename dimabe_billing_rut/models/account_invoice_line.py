f009a5ba320776aef86a28ec543346472b896048
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-23 16:09:51 -0300

    fix roundclp

--- dimabe_billing_rut/models/account_invoice_line.py
+++ dimabe_billing_rut/models/account_invoice_line.py
@@ -52,7 +52,7 @@ class AccountInvoiceLine(models.Model):
             if self.env.user.company_id.id == 1:
                 invoice_id = self.env['account.invoice'].search([('id','=',vals['invoice_id'])])
                 if invoice_id.currency_id.name=="CLP":
-                    vals.update(price_unit = (vals['price_unit'] * self.roundclp(invoice_id.exchange_rate)))
+                    vals.update(price_unit = (vals['price_unit'] * float('{:.2f}'.format(invoice_id.exchange_rate))))
             if vals.get('display_type', self.default_get(['display_type'])['display_type']):
                 vals.update(price_unit=0, account_id=False, quantity=0)
         return super(AccountInvoiceLine, self).create(vals_list)
