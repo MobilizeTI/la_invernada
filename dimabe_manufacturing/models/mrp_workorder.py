2ffef0451fa40c9a3250310c2d6dd2e8653c76e4
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-23 12:16:35 -0300

    test

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -3,7 +3,8 @@ from odoo.addons import decimal_precision as dp
 
 
 class MrpWorkorder(models.Model):
-    _inherit = ['mrp.workorder']
+    _name = 'mrp.workorder'
+    _inherit = ['mrp.workorder','barcodes.barcode_events_mixin']
 
     show_manual_input = fields.Boolean(
         'Digitar Serie Manualmente'
