0af453e2a98cdaf562f6f02902a14fd5b68f0001
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-25 15:12:40 -0300

    test

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -505,7 +505,6 @@ class MrpWorkorder(models.Model):
         })
         return res
 
-    @api.onchange('confirmed_serial')
     def confirmed_keyboard(self):
         self.process_serial(self.confirmed_serial)
 
