3e6b9b0960f68d7e97492c21ff2b40ecae3d6d7f
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-26 18:07:30 -0300

    test

--- dimabe_manufacturing/models/stock_production_lot.py
+++ dimabe_manufacturing/models/stock_production_lot.py
@@ -278,8 +278,6 @@ class StockProductionLot(models.Model):
         for item in self:
             if item.stock_production_lot_serial_ids.mapped('production_id')[0].stock_picking_id.arrival_port.country_id:
                 item.destiny_country_id = item.stock_production_lot_serial_ids.mapped('production_id')[0].stock_picking_id.arrival_port.country_id
-            else:
-                item.destiny_country_id = item.stock_production_lot_serial_ids.mapped('production_id')[0].stock_picking_id.shipping_id.arrival_port.country_id
 
     @api.multi
     def compute_measure(self):
