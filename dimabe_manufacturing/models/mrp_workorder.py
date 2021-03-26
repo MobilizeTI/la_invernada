dbf0733e8aee1e335c9d241285526e1f17269b12
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-26 13:20:16 -0300

    test

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -451,7 +451,7 @@ class MrpWorkorder(models.Model):
 
     @api.onchange('confirmed_serial')
     def confirmed_keyboard(self):
-        self.process_serial(self.confirmed_serial)
+        self.process_serial(self=self._origin,serial_number=self.confirmed_serial)
 
     def process_serial(self, serial_number):
         serial_number = serial_number.strip()
