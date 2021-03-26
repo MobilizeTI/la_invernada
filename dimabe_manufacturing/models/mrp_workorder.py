c0786e4487df4130cbca8b9bfc73dfba1a7d99b0
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-26 12:47:06 -0300

    test

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -392,9 +392,6 @@ class MrpWorkorder(models.Model):
             'views': [
                 [self.env.ref('dimabe_manufacturing.mrp_workorder_process_view').id, 'form']],
             'res_id': self.id,
-            'context': {'current_id': self.id,
-                        'default_production_id':self.production_id.id,
-                        'default_in_weight': sum(self.potential_serial_planned_ids.mapped('real_weight'))}
         }
 
     def do_finish(self):
