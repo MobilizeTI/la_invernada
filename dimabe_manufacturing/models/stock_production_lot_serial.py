aca97c50df346849a1ae69be039f1b78156fee02
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-29 12:39:56 -0300

    Fix Error Producer

--- dimabe_manufacturing/models/stock_production_lot_serial.py
+++ dimabe_manufacturing/models/stock_production_lot_serial.py
@@ -330,7 +330,7 @@ class StockProductionLotSerial(models.Model):
                 lot.write({
                     'producer_id':values_list['producer_id']
                 })
-            workorder = self.env['mrp.workorder'].search([('production_id','=',values_list['production_id'])])
+            workorder = self.env['mrp.workorder'].search([('production_id.id','=',values_list['production_id'])])
             workorder.write({
                 'out_weight': sum(lot.stock_production_lot_serial_ids.mapped('display_weight'))
             })
