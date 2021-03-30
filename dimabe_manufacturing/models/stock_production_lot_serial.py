c6fe60e1b0bc8978070375efe4f5b9788ed685ef
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-30 20:34:14 -0300

    Fix Report

--- dimabe_manufacturing/models/stock_production_lot_serial.py
+++ dimabe_manufacturing/models/stock_production_lot_serial.py
@@ -413,10 +413,13 @@ class StockProductionLotSerial(models.Model):
             )
         group = self.env['res.groups'].search([('name', '=', 'Limpiar')])
         user_logon = self.env.user
-
-        if user_logon not in group.users:
-            raise models.ValidationError("Opcion no disponible con sus permisos de usuario")
-        return super(StockProductionLotSerial, self).unlink()
+        lot = self.env['stock.production.lot'].search([('id', '=', self.stock_production_lot_id.id)])
+        if self.production_id:
+            production = self.env['mrp.production'].search([('id', '=', self.production_id.id)])
+        res = super(StockProductionLotSerial, self).unlink()
+        lot.update_kg(lot.id)
+        lot.update_stock_quant(production.location_dest_id.id)
+        return res
 
     @api.multi
     def delete(self):
