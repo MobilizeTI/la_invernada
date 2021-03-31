c7262129d926fc966074ef1ee9eb2b1959ff1ee9
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-31 17:17:06 -0300

    allow_currency_conversion to any company

--- dimabe_billing_rut/models/account_invoice_line.py
+++ dimabe_billing_rut/models/account_invoice_line.py
@@ -49,10 +49,9 @@ class AccountInvoiceLine(models.Model):
     @api.model_create_multi
     def create(self, vals_list):
         for vals in vals_list:
-            if self.env.user.company_id.id == 1:
-                invoice_id = self.env['account.invoice'].search([('id','=',vals['invoice_id'])])
-                if invoice_id.currency_id.name=="CLP" and invoice_id.allow_currency_conversion:
-                    vals.update(price_unit = self.roundclp(vals['price_unit'] * float('{:.2f}'.format(invoice_id.exchange_rate))))
+            invoice_id = self.env['account.invoice'].search([('id','=',vals['invoice_id'])])
+            if invoice_id.currency_id.name=="CLP" and invoice_id.allow_currency_conversion:
+                vals.update(price_unit = self.roundclp(vals['price_unit'] * float('{:.2f}'.format(invoice_id.exchange_rate))))
             if vals.get('display_type', self.default_get(['display_type'])['display_type']):
                 vals.update(price_unit=0, account_id=False, quantity=0)
         return super(AccountInvoiceLine, self).create(vals_list)
