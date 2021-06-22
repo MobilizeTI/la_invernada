0db8babf4fbe43a0914a65319f6090568f71dcf3
dimabe-odoo <desarrolloti@dimabe.cl>
2021-06-22 18:15:21 -0400

    pap 22/06/2021

--- dimabe_manufacturing/models/__init__.py
+++ dimabe_manufacturing/models/__init__.py
@@ -32,6 +32,6 @@ from . import confirm_principal_order
 from . import update_stock_quant
 from . import stock_report_xlsx
 from . import process_report_xlsx
-from . import generate_label_wizard
+#from . import generate_label_wizard
 #from . import mrp_eco
 from . import change_date_lot
