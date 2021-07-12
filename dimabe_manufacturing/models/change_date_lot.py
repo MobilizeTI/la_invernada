d96862f60ed6334fb3ae2bd63c9d698095d7df8e
dimabe-odoo <desarrolloti@dimabe.cl>
2021-07-12 16:20:07 -0400

    fix

--- dimabe_manufacturing/models/change_date_lot.py
+++ dimabe_manufacturing/models/change_date_lot.py
@@ -1,10 +1,12 @@
+import datetime
+
 from odoo import fields, models, api
 
 
 class ChangeDateLot(models.TransientModel):
     _name = 'change.date.lot'
 
-    lot_id = fields.Many2one('stock.production.lot',string='Lote')
+    lot_id = fields.Many2one('stock.production.lot', string='Lote')
 
     packaging_date_new = fields.Date(string='Fecha de Envasado')
 
@@ -14,11 +16,13 @@ class ChangeDateLot(models.TransientModel):
 
     best_before_date_new = fields.Date(string='Fecha de Consumir Preferentemente antes de')
 
-
     def change_pack(self):
         for item in self:
             item.lot_id.stock_production_lot_serial_ids.write({
                 'packaging_date': item.packaging_date_new
+            })
+            item.lot_id.pallet_ids.write({
+                'create_date': datetime.datetime.combine(item.packaging_date_new, datetime.datetime.min.time())
             })
             item.lot_id.write({
                 'change_packaging': False
