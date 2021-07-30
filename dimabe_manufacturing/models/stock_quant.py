5e990e28c2a2b2435b8609378d8f0a20fe4a8780
dimabe-odoo <desarrolloti@dimabe.cl>
2021-07-30 18:27:38 -0400

    fix error create negativate qaunt falta validacion

--- dimabe_manufacturing/models/stock_quant.py
+++ dimabe_manufacturing/models/stock_quant.py
@@ -45,57 +45,38 @@ class StockQuant(models.Model):
                               )
             ).mapped('display_weight'))
 
-    # def verify_negative_quant(self):
-    #     for item in self:
-    #         quants = self.env['stock.quant'].search([('quantity', '<', 0), ('location_id.usage', '=', 'internal')])
-    #         if quants:
-    #             for quant in quants:
-    #                 try:
-    #                     quant.unlink()
-    #                 except:
-    #                     query = 'DELETE FROM stock_quant where id = {}'.format(quant)
-    #                     cr = self._cr
-    #                     cr.execute(query)
-    #
-    # @api.model
-    # def create(self, values):
-    #     if isinstance(values, list):
-    #         for value in values:
-    #             if 'quantity' in value.keys() and 'location_id' in value.keys():
-    #                 location = self.env['stock.location'].search([('id', '=', value['location_id'])])
-    #                 if value['quantity'] < 0 and location.usage == 'internal':
-    #                     values.remove(value)
-    #     else:
-    #         if 'quantity' in values.keys() and 'location_id' in values.keys():
-    #             location = self.env['stock.location'].search([('id', '=', values['location_id'])])
-    #             if values['quantity'] < 0 and location.usage == 'internal':
-    #                 return self.env['stock.quant']
-    #     return super(StockQuant, self).create(values)
-    #
-    # @api.model
-    # def write(self, values):
-    #     for item in self:
-    #         if isinstance(values, list):
-    #             for value in values:
-    #                 if 'quantity' in value.keys() and 'location_id' in value.keys():
-    #                     location = self.env['stock.location'].search([('id', '=', value['location_id'])])
-    #                     if value['quantity'] < 0 and location.usage == 'internal':
-    #                         item.unlink()
-    #         else:
-    #             if 'quantity' in values.keys() and 'location_id' in values.keys():
-    #                 location = self.env['stock.location'].search([('id', '=', values['location_id'])])
-    #                 if values['quantity'] < 0 and location.usage == 'internal':
-    #                     item.unlink()
-    #     return super(StockQuant, self).write(values)
+    def verify_negative_quant(self):
+        quants = self.env['stock.quant'].search([('quantity', '<', 0), ('location_id.usage', '=', 'internal')])
+        if quants:
+            for quant in quants:
+                try:
+                    quant.unlink()
+                except:
+                    query = 'DELETE FROM stock_quant where id = {}'.format(quant.id)
+                    cr = self._cr
+                    cr.execute(query)
 
     @api.model
     def _update_reserved_quantity(self, product_id, location_id, quantity, lot_id=None, package_id=None, owner_id=None,
                                   strict=False):
         try:
             if self.lot_id:
-                return self.lot_id.update_stock_quant_production(location_id=location_id)
+                self.verify_negative_quant()
+
+                self.lot_id.update_stock_quant_production(location_id=location_id)
+                return
             else:
+                self.lot_id.update_stock_quant_production(location_id=location_id)
+                self.verify_negative_quant()
+
                 return super(StockQuant, self)._update_reserved_quantity(product_id, location_id, quantity, lot_id,
                                                                          package_id, owner_id, strict)
         except UserError:
-            return self.lot_id.update_stock_quant_production(location_id=location_id)
+            if product_id.tracking == 'lot':
+                self.lot_id.update_stock_quant_production(location_id=location_id)
+                self.verify_negative_quant()
+                return
+            res = super(StockQuant, self)._update_reserved_quantity(product_id, location_id, quantity, lot_id,
+                                                                         package_id, owner_id, strict)
+            self.verify_negative_quant()
+            return res
