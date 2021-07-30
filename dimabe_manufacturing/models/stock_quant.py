83345243277b91230444a16ddbe276c44d4c4d04
dimabe-odoo <desarrolloti@dimabe.cl>
2021-07-30 14:06:27 -0400

    commit corrections

--- dimabe_manufacturing/models/stock_quant.py
+++ dimabe_manufacturing/models/stock_quant.py
@@ -45,6 +45,49 @@ class StockQuant(models.Model):
                               )
             ).mapped('display_weight'))
 
+    # def verify_negative_quant(self):
+    #     for item in self:
+    #         quants = self.env['stock.quant'].search([('quantity', '<', 0), ('location_id.usage', '=', 'internal')])
+    #         if quants:
+    #             for quant in quants:
+    #                 try:
+    #                     quant.unlink()
+    #                 except:
+    #                     query = 'DELETE FROM stock_quant where id = {}'.format(quant)
+    #                     cr = self._cr
+    #                     cr.execute(query)
+    #
+    # @api.model
+    # def create(self, values):
+    #     if isinstance(values, list):
+    #         for value in values:
+    #             if 'quantity' in value.keys() and 'location_id' in value.keys():
+    #                 location = self.env['stock.location'].search([('id', '=', value['location_id'])])
+    #                 if value['quantity'] < 0 and location.usage == 'internal':
+    #                     values.remove(value)
+    #     else:
+    #         if 'quantity' in values.keys() and 'location_id' in values.keys():
+    #             location = self.env['stock.location'].search([('id', '=', values['location_id'])])
+    #             if values['quantity'] < 0 and location.usage == 'internal':
+    #                 return self.env['stock.quant']
+    #     return super(StockQuant, self).create(values)
+    #
+    # @api.model
+    # def write(self, values):
+    #     for item in self:
+    #         if isinstance(values, list):
+    #             for value in values:
+    #                 if 'quantity' in value.keys() and 'location_id' in value.keys():
+    #                     location = self.env['stock.location'].search([('id', '=', value['location_id'])])
+    #                     if value['quantity'] < 0 and location.usage == 'internal':
+    #                         item.unlink()
+    #         else:
+    #             if 'quantity' in values.keys() and 'location_id' in values.keys():
+    #                 location = self.env['stock.location'].search([('id', '=', values['location_id'])])
+    #                 if values['quantity'] < 0 and location.usage == 'internal':
+    #                     item.unlink()
+    #     return super(StockQuant, self).write(values)
+
     @api.model
     def _update_reserved_quantity(self, product_id, location_id, quantity, lot_id=None, package_id=None, owner_id=None,
                                   strict=False):
@@ -53,6 +96,6 @@ class StockQuant(models.Model):
                 return self.lot_id.update_stock_quant_production(location_id=location_id)
             else:
                 return super(StockQuant, self)._update_reserved_quantity(product_id, location_id, quantity, lot_id,
-                                                                     package_id, owner_id, strict)
+                                                                         package_id, owner_id, strict)
         except UserError:
             return self.lot_id.update_stock_quant_production(location_id=location_id)
