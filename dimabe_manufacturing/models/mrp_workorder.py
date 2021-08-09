8763d451899e9d70716532c396d5621fac1a9f6a
dimabe-odoo <desarrolloti@dimabe.cl>
2021-08-09 17:40:19 -0400

    fix error

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -512,7 +512,6 @@ class MrpWorkorder(models.Model):
 
     def process_serial(self, serial_number):
         lots = self.potential_serial_planned_ids
-        weigths = lots.mapped('display_weight')
         if not isinstance(self.id, int):
             self = self._origin
         serial_number = serial_number.strip()
