4f3b1a75312ae728771cd9c2c6d3f59ca8c2250e
Felipe Eduardo Angulo Achurra <felipe@pudutechnology.cl>
2021-08-10 09:31:47 -0400

    fix

--- mblz_la_invernada/models/model.py
+++ mblz_la_invernada/models/model.py
@@ -21,7 +21,7 @@ class AccountInvoice(models.Model):
                 obj_name = line[0]
                 obj = self.pool.get(obj_name)
                 if obj:
-                    sql = "delete from %s where create_date <= %s" % obj._table, datetime.datetime.strptime('2020-12-31 23:59:59', '%Y-%m-%d %H:%M:%S')
+                    sql = "delete from {} where create_date <= {}".format(obj._table, datetime.datetime.strptime('2020-12-31 23:59:59', '%Y-%m-%d %H:%M:%S'))
                     self._cr.execute(sql)
         except Exception as e:
             raise Warning(e)
