c6a4ef28f774aca3530d69428b68044ddbdae340
Felipe Eduardo Angulo Achurra <felipe@pudutechnology.cl>
2021-08-10 10:05:06 -0400

    stock

--- mblz_la_invernada/models/model.py
+++ mblz_la_invernada/models/model.py
@@ -13,17 +13,34 @@ class AccountInvoice(models.Model):
     @api.multi
     def remove_account_move_lines(self):
         to_removes = [
-            ['account.voucher.line', ],
-            ['account.voucher', ],
-            ['account.bank.statement', ],
-            ['account.bank.statement.line', ],
-            ['account.payment', ],
-            ['account.analytic.line', ],
-            ['account.invoice.line', ],
-            ['account.invoice', ],
+            # ['account.voucher.line', ],
+            # ['account.voucher', ],
+            # ['account.bank.statement', ],
+            # ['account.bank.statement.line', ],
+            # ['account.payment', ],
+            # ['account.analytic.line', ],
+            # ['account.invoice.line', ],
+            # ['account.invoice', ],
             # ['account.partial.reconcile', ],
-            ['account.move.line', ],
-            ['account.move', ],
+            # ['account.move.line', ],
+            # ['account.move', ],
+
+            ##### stock ######
+            ['stock.quant', ],
+            ['stock.quant.package', ],
+            ['stock.quant.move.rel', ],
+            ['stock.move.line', ],
+            ['stock.move', ],
+            ['stock.pack.operation', ],
+            ['stock.picking', ],
+            ['stock.scrap', ],
+            ['stock.inventory.line', ],
+            ['stock.inventory', ],
+            ['stock.production.lot', ],
+            ['stock.fixed.putaway.strat', ],
+            ['make.procurement', ],
+            ['procurement.order', ],
+            ['procurement.group', ],
         ]
         try:
             for line in to_removes:
