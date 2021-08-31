1a9816825a9584bc2dea1982ff870375929ec2ed
Felipe Angulo <felipe@mobilize.cl>
2021-08-31 16:44:19 -0400

    chnages

--- mblz_la_invernada/models/__init__.py
+++ mblz_la_invernada/models/__init__.py
@@ -1,2 +1,2 @@
 from . import model
-from . import account_general_ledger_cl
+from . import aglc
--- mblz_la_invernada/models/aglc.py
+++ mblz_la_invernada/models/aglc.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields, api, _, _lt, fields
+from odoo import models, fields, api, _
 from odoo.tools.misc import format_date, DEFAULT_SERVER_DATE_FORMAT
 from datetime import timedelta
 import logging
 _logger = logging.getLogger('TEST GENERAL LEDGER')
 
 
-class AccountGeneralLedgerReportCl(models.AbstractModel):
+class ReportAccountGeneralLedger(models.AbstractModel):
     _name = "account.general.ledger_cl"
     _description = "Libro Mayor Chile"
-    _inherit = "account.report"
+    _inherit = "account.general.ledger"
     
 
     filter_date = {'mode': 'range', 'filter': 'this_month'}
