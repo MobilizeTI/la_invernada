0a7b7229c9dd6f51775eda5c7ac5eb87a99b6862
Felipe Angulo <felipe@mobilize.cl>
2021-08-31 10:31:43 -0400

    fix

---
+++ dimabe_billing_rut/models/account_general_ledger.py
@@ -0,0 +1,23 @@
+from odoo import models, fields, api, _
+from odoo.tools.misc import format_date, DEFAULT_SERVER_DATE_FORMAT
+from datetime import timedelta
+
+
+class AccountGeneralLedgerReport(models.AbstractModel):
+    _inherit = 'account.general.ledger'
+
+    @api.model
+    def _get_columns_name(self, options):
+        columns_names = [
+            {'name': ''},
+            {'name': _('Date'), 'class': 'date'},
+            {'name': _('Communication')},
+            {'name': _('Cuenta Analítica')},
+            {'name': _('Partner')},
+            {'name': _('Debit'), 'class': 'number'},
+            {'name': _('Credit'), 'class': 'number'},
+            {'name': _('Balance'), 'class': 'number'}
+        ]
+        if self.user_has_groups('base.group_multi_currency'):
+            columns_names.insert(4, {'name': _('Currency'), 'class': 'number'})
+        return columns_names
