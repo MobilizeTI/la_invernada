443b94a411bda701a156b8e8885a9e2824cfd215
Felipe Angulo <felipe@mobilize.cl>
2021-08-31 18:22:21 -0400

    fix

--- mblz_la_invernada/models/account_general_ledger.py
+++ mblz_la_invernada/models/account_general_ledger.py
@@ -9,7 +9,8 @@ _logger = logging.getLogger('TEST PURCHASE')
 
 
 class AccountGeneralLedgerReport(models.AbstractModel):
-    _inherit = "account.general.ledger"
+    _inherit = ["account.general.ledger", "account.report"]
+    # _name = "account.general.ledger.cl"
 
     @api.model
     def _get_columns_name(self, options):
@@ -21,34 +22,7 @@ class AccountGeneralLedgerReport(models.AbstractModel):
     
     @api.model
     def _get_lines(self, options, line_id=None):
-        # offset = int(options.get('lines_offset', 0))
-        # remaining = int(options.get('lines_remaining', 0))
-        # balance_progress = float(options.get('lines_progress', 0))
-
-        # if offset > 0:
-        #     # Case a line is expanded using the load more.
-        #     return self._load_more_lines(options, line_id, offset, remaining, balance_progress)
-        # else:
-        #     # Case the whole report is loaded or a line is expanded for the first time.
-        #     return self._get_general_ledger_lines(options, line_id=line_id)
-        
         return super(AccountGeneralLedgerReport, self)._get_lines(options, line_id=None)
-    
-    # @api.model
-    # def _get_columns_name(self, options):
-    #     columns_names = [
-    #         {'name': 'Cuentas'},
-    #         {'name': _('Date'), 'class': 'date'},
-    #         {'name': _('Communication')},
-    #         {'name': _('Analítica')},
-    #         {'name': _('Partner')},
-    #         {'name': _('Debit'), 'class': 'number'},
-    #         {'name': _('Credit'), 'class': 'number'},
-    #         {'name': _('Balance'), 'class': 'number'}
-    #     ]
-    #     if self.user_has_groups('base.group_multi_currency'):
-    #         columns_names.insert(4, {'name': _('Currency'), 'class': 'number'})
-    #     return columns_names
     
     @api.model
     def _get_report_name(self):
@@ -59,4 +33,5 @@ class AccountGeneralLedgerReport(models.AbstractModel):
         res = super(AccountGeneralLedgerReport, self)._get_general_ledger_lines(options, line_id)
         _logger.info('!!!!!')
         return res
+    
         
