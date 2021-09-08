9c2bc8780379002b1acccb08d09ea28e1ce85424
Felipe Angulo <felipe@mobilize.cl>
2021-09-07 21:24:03 -0300

    log

--- l10n_cl_reports/models/account_eightcolumns_report.py
+++ l10n_cl_reports/models/account_eightcolumns_report.py
@@ -30,6 +30,7 @@ class CL8ColumnsReport(models.AbstractModel):
         options['date']['mode'] = 'range'
         options['date']['filter'] = 'custom'
         options['date']['date'] = options['date']['date_to']
+        _logger.info('LOG:  --->>> options get options {}'.format(options))
         # if self._context.get('financial_report_line_values'):
         #     options['financial_report_line_values'] = self.env.context['financial_report_line_values']
 
