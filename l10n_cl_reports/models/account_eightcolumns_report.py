7277652346ccd50f963670e6c2d70f8c0247a6a0
Felipe Angulo <felipe@mobilize.cl>
2021-09-08 08:50:42 -0300

    test356

--- l10n_cl_reports/models/account_eightcolumns_report.py
+++ l10n_cl_reports/models/account_eightcolumns_report.py
@@ -30,6 +30,7 @@ class CL8ColumnsReport(models.AbstractModel):
         # a context loss, but keeping the options).
         options['date']['mode'] = 'range'
         options['date']['filter'] = 'custom'
+        options['date']['date_from'] = ''
         # options['date']['date'] = options['date']['date_to']
         _logger.info('LOG:  --->>> options get options {}'.format(options))
         # if self._context.get('financial_report_line_values'):
