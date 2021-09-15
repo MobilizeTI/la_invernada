596914b3faec4b8ffa360469f68884ca17247020
Felipe Angulo <felipe@mobilize.cl>
2021-09-15 18:13:19 -0300

    test

--- l10n_cl_reports/models/account_eightcolumns_report.py
+++ l10n_cl_reports/models/account_eightcolumns_report.py
@@ -42,6 +42,25 @@ class CL8ColumnsReport(models.AbstractModel):
     def _prepare_query(self, options):
         tables, where_clause, where_params = self._query_get(options)
 
+        # sql_query = """
+        #     SELECT aa.id, aa.code, aa.name,
+        #            SUM(account_move_line.debit) AS debe,
+        #            SUM(account_move_line.credit) AS haber,
+        #            GREATEST(SUM(account_move_line.balance), 0) AS deudor,
+        #            GREATEST(SUM(-account_move_line.balance), 0) AS acreedor,
+        #            SUM(CASE aa.internal_group WHEN 'asset' THEN account_move_line.balance ELSE 0 END) AS activo,
+        #            SUM(CASE aa.internal_group WHEN 'equity' THEN -account_move_line.balance ELSE 0 END) +
+        #            SUM(CASE aa.internal_group WHEN 'liability' THEN -account_move_line.balance ELSE 0 END) AS pasivo,
+        #            SUM(CASE aa.internal_group WHEN 'expense' THEN account_move_line.balance ELSE 0 END) AS perdida,
+        #            SUM(CASE aa.internal_group WHEN 'income' THEN -account_move_line.balance ELSE 0 END) AS ganancia
+        #     FROM account_account AS aa, """ + tables + """
+        #     WHERE """ + where_clause + """
+        #     AND aa.id = account_move_line.account_id
+        #     GROUP BY aa.id, aa.code, aa.name
+        #     ORDER BY aa.code            
+        # """
+        _logger.info('Log. --ZZ tables {}'.format(tables))
+        _logger.info('Log. --ZZ where {}'.format(where_clause))
         sql_query = """
             SELECT aa.id, aa.code, aa.name,
                    SUM(account_move_line.debit) AS debe,
@@ -270,4 +289,18 @@ class CL8ColumnsReport(models.AbstractModel):
 
     
     
-  
+#   SELECT aa.id, aa.code, aa.name,
+#                    SUM(account_move_line.debit) AS debe,
+#                    SUM(account_move_line.credit) AS haber,
+#                    GREATEST(SUM(account_move_line.balance), 0) AS deudor,
+#                    GREATEST(SUM(-account_move_line.balance), 0) AS acreedor,
+#                    SUM(CASE aa.internal_group WHEN 'asset' THEN account_move_line.balance ELSE 0 END) AS activo,
+#                    SUM(CASE aa.internal_group WHEN 'equity' THEN -account_move_line.balance ELSE 0 END) +
+#                    SUM(CASE aa.internal_group WHEN 'liability' THEN -account_move_line.balance ELSE 0 END) AS pasivo,
+#                    SUM(CASE aa.internal_group WHEN 'expense' THEN account_move_line.balance ELSE 0 END) AS perdida,
+#                    SUM(CASE aa.internal_group WHEN 'income' THEN -account_move_line.balance ELSE 0 END) AS ganancia
+#             FROM account_account AS aa ,"account_move_line"
+#             WHERE (((("account_move_line"."company_id" = 3))  AND  ("account_move_line"."date" <= '2021-01-31'))  AND  (("account_move_line"."date" >= '2021-01-01'))) AND ("account_move_line"."company_id" IS NULL   OR  ("account_move_line"."company_id" in (3)))
+#             AND aa.id = account_move_line.account_id
+#             GROUP BY aa.id, aa.code, aa.name
+#             ORDER BY aa.code
