1fa727c7c0699e9d0acd2e970648db2d7c403855
Felipe Angulo <felipe@mobilize.cl>
2021-09-16 15:10:20 -0300

    erro no lines

--- mblz_la_invernada/report/report_account_move_line_diary_parser.py
+++ mblz_la_invernada/report/report_account_move_line_diary_parser.py
@@ -11,9 +11,13 @@ class DiaryAccountMoveLineReport(models.AbstractModel):
     def _get_report_values(self, docids, data=None):
         if not data.get('form'):
             raise UserError(_("El contenido del reporte esta vacio, El reporte no puede imprimirse."))
+        
+
 
         report = self.env['ir.actions.report']._get_report_from_name('mblz_la_invernada.report_diary_account_move_pdf')
         lines = self.get_move_lines(data['form']['date'], data['form']['company_get_id'][0])
+        if not lines:
+            raise UserError(_("No hay Apuntes para este día."))
         report_data = {            
             'doc_ids': lines[0].get('move'),
             'doc_model': report.model,
