27cb180b626b29340f56d9f31bfd810d904869ed
Felipe Angulo <felipe@mobilize.cl>
2021-09-16 12:22:22 -0300

    fixes

--- mblz_la_invernada/wizard/wizard_diary_account_move_line_report.py
+++ mblz_la_invernada/wizard/wizard_diary_account_move_line_report.py
@@ -13,11 +13,11 @@ class WizardDiaryAccountMoveLine(models.TransientModel):
 
     book_file = fields.Binary("Libro Diario")
     company_get_id = fields.Many2one('res.company', 'Compañía')
-    book_report_name = fields.Char("Libro Diario")
+    # book_report_name = fields.Char("Libro Diario")
     date = fields.Date('Fecha')
 
     def generate_diary_book_pdf(self):
-        _logger.info('LOG  ..>><<< OKKKKK')
+        # _logger.info('LOG  ..>><<< OKKKKK')
         self.ensure_one()
         [data] = self.read()
         # data['move_ids'] = self.env.context.get('active_ids', [])
@@ -29,5 +29,5 @@ class WizardDiaryAccountMoveLine(models.TransientModel):
             'form': data
         }
         # return self.env.ref('dimabe_billing_rut.honorarios_book_pdf_report').report_action(invoices, data=datas)
-        return self.env.ref('mblz_la_invernada.diary_book_pdf_report').report_action(lines, data=datas)
+        # return self.env.ref('mblz_la_invernada.diary_book_pdf_report').report_action(lines, data=datas)
         return self.env.ref('l10n_cl_balance.action_report_financial').report_action(self, data=datas, config=False)
