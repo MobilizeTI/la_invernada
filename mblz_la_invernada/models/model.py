a308355037995eabd8c70cf4d63450ee3e0f2063
Felipe Angulo <felipe@mobilize.cl>
2021-09-22 14:58:41 -0300

    ok

--- mblz_la_invernada/models/model.py
+++ mblz_la_invernada/models/model.py
@@ -10,5 +10,7 @@ _logger = logging.getLogger(__name__)
 class AccountInvoice(models.Model):
     _inherit = 'account.move'
 
-    def format_amount(self, amount):
+    def format_amount(self, amount, usd=False):
+        if usd:
+            return'$ {:,.3f}'.format(round(amount)).replace(",",".")
         return '$ {:,.0f}'.format(round(amount)).replace(",",".")
