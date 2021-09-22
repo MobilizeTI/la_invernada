e3ddab0c1324070734c54bb055db6cf978f055e7
Felipe Angulo <felipe@mobilize.cl>
2021-09-22 16:20:34 -0300

    css fix

--- l10n_cl_reports/models/account_eightcolumns_report.py
+++ l10n_cl_reports/models/account_eightcolumns_report.py
@@ -194,6 +194,20 @@ class CL8ColumnsReport(models.AbstractModel):
                 subtotals[key] += line[key]
         return subtotals
 
+    #     subtotal_line = OrderedDict(
+    # [
+    #     ('debe', 10994414133.02), 
+    #     ('haber', 10994414133.019999), 
+    #     ('deudor', 2039458103.12), 
+    #     ('acreedor', 2039458103.12), 
+    #     ('activo', 233192700.75), 
+    #     ('pasivo', 857581742.9), 
+    #     ('perdida', 1.494.374.878,37), 
+    #     ('ganancia', 869.985.836,22)])
+
+# exercise_resullt = {'pasivo': -624389042.1499999, 'perdida': 624389042.1499999, 'ganancia': 0}
+
+
     def _calculate_exercise_result(self, subtotal_line):
         exercise_result = {'pasivo': 0, 'perdida': 0, 'ganancia': 0}
         _logger.info('LOG: -->>> subtotal_line {}'.format(subtotal_line))
@@ -310,42 +324,5 @@ class CL8ColumnsReport(models.AbstractModel):
             return [('move_id.state', '=', 'posted')]
         else:
             return [('move_id.state', '!=', 'cancel')]
-    
+ 
 
-
-
-            
-
-    
-    
-#   SELECT aa.id, aa.code, aa.name,
-#                    SUM(account_move_line.debit) AS debe,
-#                    SUM(account_move_line.credit) AS haber,
-#                    GREATEST(SUM(account_move_line.balance), 0) AS deudor,
-#                    GREATEST(SUM(-account_move_line.balance), 0) AS acreedor,
-#                    SUM(CASE aa.internal_group WHEN 'asset' THEN account_move_line.balance ELSE 0 END) AS activo,
-#                    SUM(CASE aa.internal_group WHEN 'equity' THEN -account_move_line.balance ELSE 0 END) +
-#                    SUM(CASE aa.internal_group WHEN 'liability' THEN -account_move_line.balance ELSE 0 END) AS pasivo,
-#                    SUM(CASE aa.internal_group WHEN 'expense' THEN account_move_line.balance ELSE 0 END) AS perdida,
-#                    SUM(CASE aa.internal_group WHEN 'income' THEN -account_move_line.balance ELSE 0 END) AS ganancia
-#             FROM account_account AS aa, "account_move" as "account_move_line__move_id","account_move_line"
-#             WHERE (((("account_move_line__move_id"."state" != 'cancel')  AND("account_move_line"."company_id" = 3))  AND  ("account_move_line"."date" <= '2021-01-31'))  AND  (("account_move_line"."date" >= '2021-01-01') OR  ("account_move_line__move_id"."state" = 'posted'))) AND ("account_move_line"."company_id" IS NULL   OR  ("account_move_line"."company_id" in (3)))
-#             AND aa.id = account_move_line.account_id
-#             GROUP BY aa.id, aa.code, aa.name
-#             ORDER BY aa.code
-
-# SELECT aa.id, aa.code, aa.name,
-#                    SUM(account_move_line.debit) AS debe,
-#                    SUM(account_move_line.credit) AS haber,
-#                    GREATEST(SUM(account_move_line.balance), 0) AS deudor,
-#                    GREATEST(SUM(-account_move_line.balance), 0) AS acreedor,
-#                    SUM(CASE aa.internal_group WHEN 'asset' THEN account_move_line.balance ELSE 0 END) AS activo,
-#                    SUM(CASE aa.internal_group WHEN 'equity' THEN -account_move_line.balance ELSE 0 END) +
-#                    SUM(CASE aa.internal_group WHEN 'liability' THEN -account_move_line.balance ELSE 0 END) AS pasivo,
-#                    SUM(CASE aa.internal_group WHEN 'expense' THEN account_move_line.balance ELSE 0 END) AS perdida,
-#                    SUM(CASE aa.internal_group WHEN 'income' THEN -account_move_line.balance ELSE 0 END) AS ganancia
-#             FROM account_account AS aa, "account_move" as "account_move_line__move_id","account_move_line"
-#             WHERE ("account_move_line"."move_id"="account_move_line__move_id"."id") AND (((("account_move_line__move_id"."state" != %s)  AND  ("account_move_line"."company_id" = %s))  AND  ("account_move_line"."date" <= %s))  AND  (("account_move_line"."date" >= %s)  OR  ("account_move_line__move_id"."state" = %s))) AND ("account_move_line"."company_id" IS NULL   OR  ("account_move_line"."company_id" in (%s)))
-#             AND aa.id = account_move_line.account_id
-#             GROUP BY aa.id, aa.code, aa.name
-#             ORDER BY aa.code
