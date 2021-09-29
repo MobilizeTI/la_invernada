a5905f1abfa302bf490e3fe98a15009b89f1ae85
olanoit <olanoit@gmail.com>
2021-09-29 13:46:42 -0500

    fix: cambios el reporte de libro diario

--- mblz_la_invernada/wizard/wizard_diary_account_move_line_report.py
+++ mblz_la_invernada/wizard/wizard_diary_account_move_line_report.py
@@ -1,11 +1,15 @@
 import base64
-from datetime import date
+from datetime import date, datetime
+from dateutil.relativedelta import relativedelta
+
 import string
 import xlsxwriter
 from odoo import fields, models, api
 from collections import Counter
 import logging
+
 _logger = logging.getLogger('TEST report =======')
+
 
 class WizardDiaryAccountMoveLine(models.TransientModel):
     _name = 'account.move.line.diary'
@@ -14,8 +18,11 @@ class WizardDiaryAccountMoveLine(models.TransientModel):
     book_file = fields.Binary("Libro Diario")
     company_get_id = fields.Many2one('res.company', 'Compañía')
     # book_report_name = fields.Char("Libro Diario")
-    date_from = fields.Date('Desde')
-    date_to = fields.Date('Hasta')
+    date_from = fields.Date('Desde',
+                            default=lambda self: fields.Date.to_string(date.today().replace(day=1)),
+                            required=True)
+    date_to = fields.Date("Hasta", default=lambda self: fields.Date.to_string(
+        (datetime.now() + relativedelta(months=+1, day=1, days=-1)).date()), required=True)
 
     def generate_diary_book_pdf(self):
         self.ensure_one()
@@ -26,7 +33,7 @@ class WizardDiaryAccountMoveLine(models.TransientModel):
             'form': data
         }
         return self.env.ref('mblz_la_invernada.diary_book_pdf_report').report_action(self, data=datas, config=False)
-    
+
     @api.multi
     def generate_diary_book(self):
         file_name = 'diario.xlsx'
@@ -40,7 +47,7 @@ class WizardDiaryAccountMoveLine(models.TransientModel):
             company_name = ''
             begin = 0
             end = 0
-            
+
             for com in companies:
                 worksheet = workbook.add_worksheet(com.display_name)
                 array_worksheet.append({
@@ -49,7 +56,8 @@ class WizardDiaryAccountMoveLine(models.TransientModel):
             for wk in array_worksheet:
                 sheet = wk['worksheet']
                 region = self.env['region.address'].search([('id', '=', 1)])
-                titles = ['Fecha', 'Diario', 'Cuenta', 'Cód.Cuenta', 'Analítica', 'Ref', 'Empresa', 'Débito', 'Crédito', 'Divisa', 'Match']
+                titles = ['Fecha', 'Diario', 'Cuenta', 'Cód.Cuenta', 'Analítica', 'Ref', 'Empresa', 'Débito', 'Crédito',
+                          'Divisa', 'Match']
                 sheet.merge_range(0, 0, 0, 2, self.company_get_id.display_name, formats['title'])
                 sheet.merge_range(1, 0, 1, 2, self.company_get_id.invoice_rut, formats['title'])
                 sheet.merge_range(2, 0, 2, 2,
@@ -64,7 +72,7 @@ class WizardDiaryAccountMoveLine(models.TransientModel):
                 sheet.merge_range(8, 3, 8, 6, 'Moneda : Peso Chileno', formats['title'])
                 row = 10
                 col = 0
-                
+
                 for title in titles:
                     sheet.write(row, col, title, formats['title'])
                     col += 1
@@ -78,10 +86,17 @@ class WizardDiaryAccountMoveLine(models.TransientModel):
                 row = data_invoice['row']
                 count_invoice += data_invoice['count_invoice']
 
+                # Totales
+                sheet.write(row + 2, col + 6, 'Total General', formats['title'])
+                sheet.write(row + 2, col + 7, data_invoice['total_general_debit'], formats['total'])
+                sheet.write(row + 2, col + 8, data_invoice['total_general_credit'], formats['total'])
+
         workbook.close()
         with open(file_name, "rb") as file:
             file_base64 = base64.b64encode(file.read())
-        file_name = 'Libro Diario {} Desde {} Hasta {}.xlsx'.format(self.company_get_id[0].name, self.date_from.strftime("%d/%m/%Y"), self.date_to.strftime("%d/%m/%Y"))
+        file_name = 'Libro Diario {} Desde {} Hasta {}.xlsx'.format(self.company_get_id[0].name,
+                                                                    self.date_from.strftime("%d/%m/%Y"),
+                                                                    self.date_to.strftime("%d/%m/%Y"))
         attachment_id = self.env['ir.attachment'].sudo().create({
             'name': file_name,
             'datas_fname': file_name,
@@ -94,15 +109,15 @@ class WizardDiaryAccountMoveLine(models.TransientModel):
             'target': 'current',
         }
         return action
-    
+
     def get_move_lines(self, date_from, date_to, company_id):
         domain = [
             ('date', '>=', date_from),
             ('date', '<=', date_to),
             ('company_id.id', '=', company_id)
-            ]
+        ]
         res = []
-        
+
         lines = self.env['account.move.line'].sudo().search(domain, order='date asc')
 
         # moves = list(set([line.move_id for line in lines]))
@@ -117,9 +132,9 @@ class WizardDiaryAccountMoveLine(models.TransientModel):
                 'move': move,
                 'lines': move_lines
             })
-        
+
         return res
-    
+
     def set_formats(self, workbook):
         merge_format_string = workbook.add_format({
             'border': 0,
@@ -158,27 +173,33 @@ class WizardDiaryAccountMoveLine(models.TransientModel):
             'total': merge_format_total,
             'text_total': merge_format_total_text
         }
-    
+
     def set_data_for_excel(self, sheet, row, invoices, titles, formats):
+        total_general_debit, total_general_credit = 0.0, 0.0
         for inv in invoices:
             col = 0
             data = self.set_data_invoice(sheet, col, row, inv, invoices, titles, formats)
             sheet = data['sheet']
             row = data['row']
             col = data['col']
+            total_general_debit += data['total_debit']
+            total_general_credit += data['total_credit']
             row += 1
+
         count_invoice = len(invoices)
         col = 0
         return {
-            'sheet': sheet, 
-            'row': row, 
+            'sheet': sheet,
+            'row': row,
             # 'total': {
             #     'total' : sum(invoices.mapped('amount_total')),
             #     'net' : sum(invoices.mapped('amount_untaxed')),
             #     'tax': sum(invoices.mapped('amount_tax')),
             #     'exempt': exempt_sum,
             # },
-            'count_invoice': count_invoice
+            'count_invoice': count_invoice,
+            'total_general_debit': total_general_debit,
+            'total_general_credit': total_general_credit,
         }
 
     def set_data_invoice(self, sheet, col, row, inv, invoices, titles, formats):
@@ -191,66 +212,63 @@ class WizardDiaryAccountMoveLine(models.TransientModel):
         for line in inv['lines']:
             total_debit += line.debit
             total_credit += line.credit
-            #Fecha
-            sheet.write(row, col, line.date.strftime('%Y-%m-%d'), formats['string'])  
+            # Fecha
+            sheet.write(row, col, line.date.strftime('%Y-%m-%d'), formats['string'])
             col += 1
-            #Diario
+            # Diario
             sheet.write(row, col, line.journal_id.name, formats['string'])
             width = len(line.journal_id.name)
             col += 1
             sheet.set_column(col, row, width)
-            
-            #Cuenta (Nombre)
+
+            # Cuenta (Nombre)
             sheet.write(row, col, line.account_id.name, formats['string'])
             width = len(line.account_id.name)
             col += 1
             sheet.set_column(col, row, width)
-            
-            #Cuenta (Código)
+
+            # Cuenta (Código)
             sheet.write(row, col, line.account_id.code, formats['string'])
             col += 1
-            #Cuenta Analítica
+            # Cuenta Analítica
             if line.analytic_account_id:
                 sheet.write(row, col, line.analytic_account_id.name, formats['string'])
                 width = len(line.analytic_account_id.name)
             col += 1
             sheet.set_column(col, row, width)
-            
-            #Referencia
+
+            # Referencia
             # sheet.write(row, col, line.ref, formats['string'])
-            #width = len(line.ref)
+            # width = len(line.ref)
             col += 1
-            #sheet.set_column(col, row, width)
-            
-            #Partner
+            # sheet.set_column(col, row, width)
+
+            # Partner
             if line.partner_id:
                 sheet.write(row, col, line.partner_id.name, formats['string'])
                 width = len(line.partner_id.name)
             col += 1
             sheet.set_column(col, row, width)
-            #Débito
+            # Débito
             sheet.write(row, col, line.debit, formats['number'])
             col += 1
-            #Crédito
+            # Crédito
             sheet.write(row, col, line.credit, formats['number'])
             col += 1
-            #Divisa
+            # Divisa
             if line.amount_currency:
                 sheet.write(row, col, line.amount_currency, formats['number'])
             col += 1
-            #Match
+            # Match
             if line.reconciled:
                 sheet.write(row, col, line.full_reconcile_id.name, formats['string'])
             col = col - 10
             row += 1
-        #Totales
+        # Totales
         sheet.write(row, col + 6, 'Totales', formats['title'])
         sheet.write(row, col + 7, total_debit, formats['total'])
         sheet.write(row, col + 8, total_credit, formats['total'])
-        
-        line_out = {'sheet': sheet, 'row': row, 'col': col}
+
+        line_out = {'sheet': sheet, 'row': row, 'col': col, 'total_debit': total_debit, 'total_credit': total_credit}
 
         return line_out
-
-
-       
