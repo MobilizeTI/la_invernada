127668fb020d28f217e7ba1d856e5f84a4e16f98
Felipe Angulo <felipe@mobilize.cl>
2021-11-18 12:57:44 -0300

    test inicial balance

--- l10n_cl_reports/models/account_eightcolumns_report.py
+++ l10n_cl_reports/models/account_eightcolumns_report.py
@@ -29,7 +29,7 @@ class CL8ColumnsReport(models.AbstractModel):
     def _get_columns_name(self, options):
         columns = [
             {'name': _("Cuenta")},
-            # {'name': _("Saldo Inicial"), 'class': 'number'},
+            {'name': _("Saldo Inicial"), 'class': 'number'},
             {'name': _("Debe"), 'class': 'number'},
             {'name': _("Haber"), 'class': 'number'},
             {'name': _("Deudor"), 'class': 'number'},
@@ -45,28 +45,6 @@ class CL8ColumnsReport(models.AbstractModel):
     def _prepare_query(self, options):
         tables, where_clause, where_params = self._query_get(options)
 
-        sql_query = """
-            SELECT aa.id, aa.code, aa.name,
-                   SUM(account_move_line.debit) AS debe,
-                   SUM(account_move_line.credit) AS haber,
-                   GREATEST(SUM(account_move_line.balance), 0) AS deudor,
-                   GREATEST(SUM(-account_move_line.balance), 0) AS acreedor,
-                   SUM(CASE aa.internal_group WHEN 'asset' THEN account_move_line.balance ELSE 0 END) AS activo,
-                   SUM(CASE aa.internal_group WHEN 'equity' THEN -account_move_line.balance ELSE 0 END) +
-                   SUM(CASE aa.internal_group WHEN 'liability' THEN -account_move_line.balance ELSE 0 END) AS pasivo,
-                   SUM(CASE aa.internal_group WHEN 'expense' THEN account_move_line.balance ELSE 0 END) AS perdida,
-                   SUM(CASE aa.internal_group WHEN 'income' THEN -account_move_line.balance ELSE 0 END) AS ganancia
-            FROM account_account AS aa, """ + tables + """
-            WHERE """ + where_clause + """
-            AND aa.id = account_move_line.account_id
-            GROUP BY aa.id, aa.code, aa.name
-            ORDER BY aa.code            
-        """
-        # _logger.info('Log. --ZZ tables {}'.format(tables))
-        # _logger.info('Log. --ZZ where_params {}'.format(where_params))
-        # tables = ''
-        # where_clause_ = where_clause.replace('("account_move_line"."move_id"="account_move_line__move_id"."id") AND ', '')
-        # where_clause__ = where_clause_.replace('("account_move_line__move_id"."state" != {})'.format("'cancel'"), '')
         # sql_query = """
         #     SELECT aa.id, aa.code, aa.name,
         #            SUM(account_move_line.debit) AS debe,
@@ -78,13 +56,12 @@ class CL8ColumnsReport(models.AbstractModel):
         #            SUM(CASE aa.internal_group WHEN 'liability' THEN -account_move_line.balance ELSE 0 END) AS pasivo,
         #            SUM(CASE aa.internal_group WHEN 'expense' THEN account_move_line.balance ELSE 0 END) AS perdida,
         #            SUM(CASE aa.internal_group WHEN 'income' THEN -account_move_line.balance ELSE 0 END) AS ganancia
-        #     FROM account_account AS aa, "account_move_line" """ + tables + """
+        #     FROM account_account AS aa, """ + tables + """
         #     WHERE """ + where_clause + """
         #     AND aa.id = account_move_line.account_id
         #     GROUP BY aa.id, aa.code, aa.name
         #     ORDER BY aa.code            
         # """
-        # _logger.info('LOG: ....>>> sql {}'.format(sql_query))
         sql_query = """
             select aa.id, aa.code, aa.name, 
                 SUM(account_move_line.debit) AS debe,
@@ -330,32 +307,6 @@ class CL8ColumnsReport(models.AbstractModel):
             params = [tuple(user_types.ids)] + where_params + [tuple(user_types.ids)] + where_params + [tuple(user_types.ids)] + where_params + [tuple(user_types.ids)]
         return sql, params
     
-    # @api.model
-    # def _get_lines(self, options, line_id=None):
-    #     context = self.env.context
-    #     company_id = context.get('company_id') or self.env.user.company_id
-    #     grouped_accounts = {}
-    #     initial_balances = {}
-    #     comparison_table = [options.get('date')]
-    #     comparison_table += options.get('comparison') and options['comparison'].get('periods') or []
-
-    #     #get the balance of accounts for each period
-    #     period_number = 0
-    #     for period in reversed(comparison_table):
-    #         res = self.with_context(date_from_aml=period['date_from'], date_to=period['date_to'], date_from=period['date_from'] and company_id.compute_fiscalyear_dates(fields.Date.from_string(period['date_from']))['date_from'] or None)._group_by_account_id(options, line_id)  # Aml go back to the beginning of the user chosen range but the amount on the account line should go back to either the beginning of the fy or the beginning of times depending on the account
-    #         if period_number == 0:
-    #             initial_balances = dict([(k, res[k]['initial_bal']['balance']) for k in res])
-    #         for account in res:
-    #             if account not in grouped_accounts:
-    #                 grouped_accounts[account] = [{'balance': 0, 'debit': 0, 'credit': 0} for p in comparison_table]
-    #             grouped_accounts[account][period_number]['balance'] = res[account]['balance'] - res[account]['initial_bal']['balance']
-    #             grouped_accounts[account][period_number]['debit'] = res[account]['debit'] - res[account]['initial_bal']['debit']
-    #             grouped_accounts[account][period_number]['credit'] = res[account]['credit'] - res[account]['initial_bal']['credit']
-    #         period_number += 1
-
-    #     #build the report
-    #     lines = self._post_process(grouped_accounts, initial_balances, options, comparison_table)
-    #     return lines
 
     @api.model
     def _get_lines(self, options, line_id=None):
@@ -437,7 +388,7 @@ class CL8ColumnsReport(models.AbstractModel):
                 'unfoldable': False,
                 'columns': [
                     {'name': values} for values in [
-                        # self.format_value(init_account_balance),
+                        self.format_value(init_account_balance),
                         self.format_value(line['debe']),
                         self.format_value(line['haber']),
                         self.format_value(line['deudor']),
@@ -497,7 +448,7 @@ class CL8ColumnsReport(models.AbstractModel):
 
     def _calculate_subtotals(self, lines):
         subtotals = OrderedDict([
-            # ('balance_inicial', 0),
+            ('balance_inicial', 0),
             ('debe', 0), ('haber', 0),
             ('deudor', 0), ('acreedor', 0),
             ('activo', 0), ('pasivo', 0),
@@ -519,7 +470,7 @@ class CL8ColumnsReport(models.AbstractModel):
 
     def _calculate_totals(self, subtotal_line, exercise_result_line):
         totals = OrderedDict([
-            # ('balance_inicial', subtotal_line['balance_inicial']),
+            ('balance_inicial', subtotal_line['balance_inicial']),
             ('debe', subtotal_line['debe']), ('haber', subtotal_line['haber']),
             ('deudor', subtotal_line['deudor']), ('acreedor', subtotal_line['acreedor']),
             ('activo', subtotal_line['activo'] + exercise_result_line['activo']), ('pasivo', subtotal_line['pasivo'] + exercise_result_line['pasivo']),
@@ -532,11 +483,8 @@ class CL8ColumnsReport(models.AbstractModel):
     def _query_get(self, options, domain=None):
         domain = self._get_options_domain(options) + (domain or [])
         self.env['account.move.line'].check_access_rights('read')
-        # _logger.info('LOG: ----ZZ domain {}'.format(domain))
 
         query = self.env['account.move.line']._where_calc(domain)
-
-        # Wrap the query with 'company_id IN (...)' to avoid bypassing company access rights.
         self.env['account.move.line']._apply_ir_rules(query)
 
         return query.get_sql()   
