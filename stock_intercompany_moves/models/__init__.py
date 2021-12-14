9bce69f3cd24a79df0ae2f08d36cd044fff397af
Felipe Angulo <felipe@mobilize.cl>
2021-12-14 11:27:42 -0300

    [FIX] remove modules via upload

--- stock_intercompany_moves/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import product_product
-from . import stock_location
-from . import stock_picking
-from . import stock_picking_type
-from . import stock_quant
--- stock_requisitions/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import product_template
-from . import stock_move
-from . import stock_picking_type
-from . import stock_picking
