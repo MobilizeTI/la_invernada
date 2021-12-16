0739dc0fdd131ae1117b2b7fdc0812dc9ed98624
Felipe Angulo <felipe@mobilize.cl>
2021-12-16 15:42:38 -0300

    [TEST] write method in res partner

--- mblz_fe/l10n_cl_fe/models/res_partner.py
+++ mblz_fe/l10n_cl_fe/models/res_partner.py
@@ -2,8 +2,8 @@ import json
 import logging
 import re
 
-from odoo import api, fields, models, tools
-from odoo.exceptions import UserError
+from odoo import api, fields, models, tools, SUPERUSER_ID
+from odoo.exceptions import UserError, ValidationError
 from odoo.tools.translate import _
 
 _logger = logging.getLogger(__name__)
@@ -78,6 +78,43 @@ class ResPartner(models.Model):
             d = self.document_number.replace(".", "").split("-")
             rut = str(int(d[0])) + "-" + d[1]
         return rut
+    
+    @api.multi
+    def write(self, vals):
+        if vals.get('active') is False:
+            for partner in self:
+                if partner.active and partner.user_ids:
+                    raise ValidationError(_('You cannot archive a contact linked to an internal user.'))
+        # res.partner must only allow to set the company_id of a partner if it
+        # is the same as the company of all users that inherit from this partner
+        # (this is to allow the code from res_users to write to the partner!) or
+        # if setting the company_id to False (this is compatible with any user
+        # company)
+        if vals.get('website'):
+            vals['website'] = self._clean_website(vals['website'])
+        if vals.get('parent_id'):
+            vals['company_name'] = False
+        if vals.get('company_id'):
+            company = self.env['res.company'].browse(vals['company_id'])
+            for partner in self:
+                if partner.user_ids:
+                    companies = set(user.company_id for user in partner.user_ids)
+                    if len(companies) > 1 or company not in companies:
+                        raise UserError(
+                            ("The selected company is not compatible with the companies of the related user(s)"))
+        tools.image_resize_images(vals, sizes={'image': (1024, None)})
+
+        result = True
+        # To write in SUPERUSER on field is_company and avoid access rights problems.
+        if 'is_company' in vals and self.user_has_groups('base.group_partner_manager') and not self.env.uid == SUPERUSER_ID:
+            result = super(ResPartner, self.sudo()).write({'is_company': vals.get('is_company')})
+            del vals['is_company']
+        result = result and super(ResPartner, self).write(vals)
+        for partner in self:
+            # if any(u.has_group('base.group_user') for u in partner.user_ids if u != self.env.user):
+            #     self.env['res.users'].check_access_rights('write')
+            partner._fields_sync(vals)
+        return result
 
     # def write(self, vals):
     #     result = super(ResPartner, self).write(vals)
